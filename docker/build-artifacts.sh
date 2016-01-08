116d1c33784800e7a16f789dd2bf58f6c823f21c
Andrew Jorgensen <ajorgensen@twitter.com>
2016-01-08 17:47:20 -0500

    Move bazelrc to tools/docker/bazelrc

--- docker/build-artifacts.sh
+++ docker/build-artifacts.sh
@@ -13,6 +13,9 @@ cleanup() {
   if [ -f $SRC_TAR ]; then
     echo "Cleaning up generate source tarball"
     rm -rf $SRC_TAR
+
+    echo "Removing bazelrc"
+    rm $DOCKER_DIR/bazelrc
   fi
 }
 
@@ -23,28 +26,60 @@ generate_source() {
   tar --exclude-from=$DOCKER_DIR/.tarignore -C $PROJECT_DIR -czf $SRC_TAR .
 }
 
+verify_dockerfile_exists() {
+  if [ ! -f $1 ]; then
+    echo "The Dockerfiler $1 does not exist"
+    exit 1
+  fi
+}
+
+verify_source_exists() {
+  if [ ! -f $1 ]; then
+    echo "The source provided $1 does not exist"
+    exit 1
+  fi
+}
+
+dockerfile_path_for_platform() {
+  echo "$DOCKER_DIR/Dockerfile.$1"
+}
+
+copy_bazel_rc() {
+  cp $PROJECT_DIR/tools/docker/bazelrc $DOCKER_DIR/bazelrc
+}
+
 run_build() {
   PLATFORM=$1
   HERON_VERSION=$2
-  SOURCE_TARBALL=$(realpath $3)
   OUTPUT_DIRECTORY=$(realpath $4)
+  DOCKER_FILE=$(dockerfile_path_for_platform $PLATFORM)
+
+  verify_dockerfile_exists $DOCKER_FILE
+  copy_bazel_rc
+
+  if [ -z "$3" ]; then
+    generate_source
+    SOURCE_TARBALL=$SRC_TAR
+  else
+    SOURCE_TARBALL=$(realpath $3)
+  fi
+  verify_source_exists $SOURCE_TARBALL
 
   echo "Building heron-compiler container"
-  docker build -t heron-compiler -f $DOCKER_DIR/Dockerfile.$PLATFORM $DOCKER_DIR
+  docker build -t heron-compiler -f $DOCKER_FILE $DOCKER_DIR
 
   echo "Running build in container"
   docker run \
     --rm \
     -e HERON_VERSION=$HERON_VERSION \
-    -v "$SRC_TAR:/src.tar.gz:ro" \
+    -v "$SOURCE_TARBALL:/src.tar.gz:ro" \
     -v "$OUTPUT_DIRECTORY:/dist" \
     -it "heron-compiler" /compile.sh
 }
 
 case $# in
   3)
-    generate_source
-    run_build $1 $2 $SRC_TAR $3
+    run_build $1 $2 "" $3
     ;;
 
   4)
