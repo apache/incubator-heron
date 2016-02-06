2897efee3158e4beb2877f7dd188ef71aa60cb5c
Karthik Ramasamy <kramasamy@twitter.com>
2016-02-05 17:17:02 -0800

    added generic yaml config parsing

---
+++ heron/config/tests/java/com/twitter/heron/config/ConfigReaderTest.java
@@ -0,0 +1,28 @@
+package com.twitter.heron.config;
+
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConfigReaderTest {
+  private static final Logger LOG = Logger.getLogger(ConfigReaderTest.class.getName());
+
+  @Test
+  public void testLoadFile() throws Exception {
+    ConfigReader configReader = ConfigReader.class.newInstance();
+
+    String file = Paths.get(System.getenv("JAVA_RUNFILES"), Constants.TEST_DATA_PATH, "defaults.conf").toString();
+    Map props = configReader.loadFile(file);
+
+    Assert.assertEquals("role", props.get(Constants.ROLE_KEY));
+    Assert.assertEquals("environ", props.get(Constants.ENVIRON_KEY));
+    Assert.assertEquals("com.twitter.heron.scheduler.aurora.AuroraLauncher", props.get(Constants.LAUNCHER_CLASS_KEY)); 
+
+    Assert.assertNull(props.get(Constants.USER_KEY));
+  }
+}
