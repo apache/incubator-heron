cf652cdb06d8c4cf9607178994053986dc039f80
Karthik Ramasamy <kramasamy@twitter.com>
2016-02-11 21:57:27 -0800

    modularized all the uploaders

--- heron/spi/src/java/com/twitter/heron/spi/common/Context.java
+++ heron/spi/src/java/com/twitter/heron/spi/common/Context.java
@@ -1,4 +1,4 @@
-package com.twitter.heron.spi.common.context;
+package com.twitter.heron.spi.common;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -9,8 +9,22 @@ public class Context {
   public static class Builder {
     private final Map<String, Object> keyValues = new HashMap<String, Object>();
 
-    public Builder setKeyValue(String key, Object value) {
+    public Builder set(String key, Object value) {
       this.keyValues.put(key, value);
+      return this;
+    }
+
+    public Builder setStringValue(String key, String value) {
+      this.keyValues.put(key, value);
+      return this;
+    }
+
+    public Builder setBooleanValue(String key, boolean value) {
+      if (value == true)
+        this.keyValues.put(key, "yes");
+      else
+        this.keyValues.put(key, "no");
+
       return this;
     }
 
@@ -19,16 +33,37 @@ public class Context {
     }
   }
 
-  public Context(Builder) {
-    this.keyValues.putAll(Builder.keyValues);
+  public Context(Builder build) {
+    this.keyValues.putAll(build.keyValues);
   }
 
-  public append(Builder) {
-    this.keyValues.putAll(Builder.keyValues);
+  public void append(Builder build) {
+    this.keyValues.putAll(build.keyValues);
   }
 
-  public Object getProperty(String key) {
-    return config.get(key);
+  public Object get(String key) {
+    return keyValues.get(key);
   }
 
+  public String getStringValue(String key) {
+    return (String) keyValues.get(key);
+  }
+
+  public String getStringValue(String key, String defaultValue) {
+    String value = getStringValue(key);
+    return value != null ? value : defaultValue;
+  }
+
+  public boolean getBooleanValue(String key) {
+    String value = getStringValue(key);
+    return value == "yes" ? true : false;
+  }
+
+  public boolean getBooleanValue(String key, boolean defaultValue) {
+    String value = (String) get(key);
+    if (value != null)
+      return value == "yes" ? true : false;
+
+    return defaultValue;
+  }
 }
