da24a49f0203f5b8d13b09e07dfc356881695ecf
Vikas Kedigehalli <vikasr@twitter.com>
2016-02-15 12:44:11 -0800

    NullStateManager cleanup

--- heron/spi/src/java/com/twitter/heron/spi/statemgr/NullStateManager.java
+++ heron/spi/src/java/com/twitter/heron/spi/statemgr/NullStateManager.java
@@ -11,15 +11,13 @@ import com.twitter.heron.proto.system.ExecutionEnvironment;
 import com.twitter.heron.proto.system.PhysicalPlans;
 import com.twitter.heron.proto.tmaster.TopologyMaster;
 
-import com.twitter.heron.spi.statemgr.IStateManager;
-import com.twitter.heron.spi.statemgr.WatchCallback;
-
+@SuppressWarnings("unchecked")
 public class NullStateManager implements IStateManager {
-  public SettableFuture<Boolean> brightFuture = SettableFuture.create();
+  public SettableFuture nullFuture = SettableFuture.create();
 
   @Override
   public void initialize(Map<Object, Object> conf) {
-    brightFuture.set(true);
+    nullFuture.set(null);
   }
 
   @Override
@@ -34,76 +32,81 @@ public class NullStateManager implements IStateManager {
 
   @Override
   public ListenableFuture<Boolean> setTMasterLocation(TopologyMaster.TMasterLocation location, String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
-  public ListenableFuture<Boolean> setExecutionState(ExecutionEnvironment.ExecutionState executionState, String topologyName) {
-    return brightFuture;
+  public ListenableFuture<Boolean> setExecutionState(ExecutionEnvironment.ExecutionState executionState, String
+      topologyName) {
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> setTopology(TopologyAPI.Topology topology, String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> setPhysicalPlan(PhysicalPlans.PhysicalPlan physicalPlan, String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> setSchedulerLocation(Scheduler.SchedulerLocation location, String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> deleteTMasterLocation(String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> deleteExecutionState(String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> deleteTopology(String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> deletePhysicalPlan(String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<Boolean> deleteSchedulerLocation(String topologyName) {
-    return brightFuture;
+    return nullFuture;
   }
 
   @Override
-  public ListenableFuture<TopologyMaster.TMasterLocation> getTMasterLocation(WatchCallback watcher, String topologyName) {
-    return null;
+  public ListenableFuture<TopologyMaster.TMasterLocation> getTMasterLocation(WatchCallback watcher, String
+      topologyName) {
+
+    return nullFuture;
   }
 
   @Override
-  public ListenableFuture<Scheduler.SchedulerLocation> getSchedulerLocation(WatchCallback watcher, String topologyName) {
-    return null;
+  public ListenableFuture<Scheduler.SchedulerLocation> getSchedulerLocation(WatchCallback watcher, String
+      topologyName) {
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<TopologyAPI.Topology> getTopology(WatchCallback watcher, String topologyName) {
-    return null;
+    return nullFuture;
   }
 
   @Override
-  public ListenableFuture<ExecutionEnvironment.ExecutionState> getExecutionState(WatchCallback watcher, String topologyName) {
-    return null;
+  public ListenableFuture<ExecutionEnvironment.ExecutionState> getExecutionState(WatchCallback watcher, String
+      topologyName) {
+    return nullFuture;
   }
 
   @Override
   public ListenableFuture<PhysicalPlans.PhysicalPlan> getPhysicalPlan(WatchCallback watcher, String topologyName) {
-    return null;
+    return nullFuture;
   }
 }
