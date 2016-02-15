2c902fe59f7bd81ce71ec659f8f4e5c0716a9e37
Karthik Ramasamy <kramasamy@twitter.com>
2016-02-15 11:00:26 -0800

    initial version of config loading

--- heron/newscheduler/src/java/com/twitter/heron/scheduler/ClusterConfig.java
+++ heron/newscheduler/src/java/com/twitter/heron/scheduler/ClusterConfig.java
@@ -15,33 +15,45 @@ import com.twitter.heron.spi.common.Context;
 import com.twitter.heron.common.config.ClusterConfigReader;
 
 public final class ClusterConfig {
-  public static Context loadClusterConfig(String cluster, String configPath) {
+  protected static Context loadClusterConfig(String cluster, String configPath) {
     Map config = ClusterConfigReader.load(cluster, configPath, Defaults.Files.CLUSTER_YAML);
     return Context.newBuilder().putAll(config).build();
   }
 
-  public static Context loadDefaultsConfig(String cluster, String configPath) {
+  protected static Context loadDefaultsConfig(String cluster, String configPath) {
     Map config = ClusterConfigReader.load(cluster, configPath, Defaults.Files.DEFAULTS_YAML);
     return Context.newBuilder().putAll(config).build();
   }
 
-  public static Context loadPackingConfig(String cluster, String configPath) {
+  protected static Context loadPackingConfig(String cluster, String configPath) {
     Map config = ClusterConfigReader.load(cluster, configPath, Defaults.Files.PACKING_YAML);
     return Context.newBuilder().putAll(config).build();
   }
 
-  public static Context loadSchedulerConfig(String cluster, String configPath) {
+  protected static Context loadSchedulerConfig(String cluster, String configPath) {
     Map config = ClusterConfigReader.load(cluster, configPath, Defaults.Files.SCHEDULER_YAML);
     return Context.newBuilder().putAll(config).build();
   }
 
-  public static Context loadStateManagerConfig(String cluster, String configPath) {
+  protected static Context loadStateManagerConfig(String cluster, String configPath) {
     Map config = ClusterConfigReader.load(cluster, configPath, Defaults.Files.STATEMGR_YAML);
     return Context.newBuilder().putAll(config).build();
   }
 
-  public static Context loadUploaderConfig(String cluster, String configPath) {
+  protected static Context loadUploaderConfig(String cluster, String configPath) {
     Map config = ClusterConfigReader.load(cluster, configPath, Defaults.Files.UPLOADER_YAML);
     return Context.newBuilder().putAll(config).build();
+  }
+  
+  public static Context loadConfig(String cluster, String configPath) {
+    Context.Builder cb = Context.newBuilder()
+        .putAll(loadClusterConfig(cluster, configPath))
+        .putAll(loadDefaultsConfig(cluster, configPath))
+        .putAll(loadPackingConfig(cluster, configPath))
+        .putAll(loadSchedulerConfig(cluster, configPath))
+        .putAll(loadStateManagerConfig(cluster, configPath))
+        .putAll(loadUploaderConfig(cluster, configPath));
+
+    return cb.build();
   }
 }
--- heron/newscheduler/src/java/com/twitter/heron/scheduler/ClusterDefaults.java
+++ heron/newscheduler/src/java/com/twitter/heron/scheduler/ClusterDefaults.java
@@ -17,38 +17,38 @@ import com.twitter.heron.common.config.ClusterConfigReader;
 public final class ClusterDefaults {
 
   protected static Context getDefaultBinaries() {
-     Context binContext = Context.newBuilder()
+     Context.Builder cb = Context.newBuilder()
          .put(Keys.Config.EXECUTOR_BINARY, Defaults.Config.EXECUTOR_BINARY)
          .put(Keys.Config.STMGR_BINARY, Defaults.Config.STMGR_BINARY)
          .put(Keys.Config.TMASTER_BINARY, Defaults.Config.TMASTER_BINARY)
-         .put(Keys.Config.SHELL_BINARY, Defaults.Config.SHELL_BINARY)
-         .build();
-     return binContext;
+         .put(Keys.Config.SHELL_BINARY, Defaults.Config.SHELL_BINARY);
+
+     return cb.build();
   }
 
   protected static Context getDefaultJars() {
-     Context jarContext = Context.newBuilder()
-         .put(Keys.Config.SCHEDULER_JAR, Defaults.Config.SCHEDULER_JAR)
-         .build();
-     return jarContext;
+     Context.Builder cb = Context.newBuilder()
+         .put(Keys.Config.SCHEDULER_JAR, Defaults.Config.SCHEDULER_JAR);
+
+     return cb.build();
   }
 
   protected static Context getDefaultResources() {
-     Context resContext = Context.newBuilder()
+     Context.Builder cb = Context.newBuilder()
          .put(Keys.Config.STMGR_RAM, Long.valueOf(Defaults.Config.STMGR_RAM))
          .put(Keys.Config.INSTANCE_CPU, Long.valueOf(Defaults.Config.INSTANCE_CPU))
          .put(Keys.Config.INSTANCE_RAM, Long.valueOf(Defaults.Config.INSTANCE_RAM))
-         .put(Keys.Config.INSTANCE_DISK, Long.valueOf(Defaults.Config.INSTANCE_DISK))
-         .build();
-     return resContext;
+         .put(Keys.Config.INSTANCE_DISK, Long.valueOf(Defaults.Config.INSTANCE_DISK));
+
+     return cb.build();
   }
      
   public static Context getDefaults() {
-     Context context = Context.newBuilder()
+     Context.Builder cb = Context.newBuilder()
          .putAll(getDefaultBinaries())
          .putAll(getDefaultJars())
-         .putAll(getDefaultResources())
-         .build();
-     return context;
+         .putAll(getDefaultResources());
+
+     return cb.build();
   }
 }
