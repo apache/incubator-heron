4c667bd81e6242357ecae9acb1379d6ed82e3731
kramasamy <karthikz@Karthiks-iMac.lan>
2016-02-27 22:17:52 -0800

    default uploader and statemgr path is changed ~/.herondata. This is to separate the software from the data

--- heron/uploaders/src/java/com/twitter/heron/uploader/localfs/LocalFileSystemDefaults.java
+++ heron/uploaders/src/java/com/twitter/heron/uploader/localfs/LocalFileSystemDefaults.java
@@ -1,5 +1,5 @@
 package com.twitter.heron.uploader.localfs;
 
 public class LocalFileSystemDefaults {
-  public static final String FILE_SYSTEM_DIRECTORY = "${HOME}/.heron/repository/${CLUSTER}/${ROLE}/${TOPOLOGY}";
+  public static final String FILE_SYSTEM_DIRECTORY = "${HOME}/.herondata/repository/${CLUSTER}/${ROLE}/${TOPOLOGY}";
 }
