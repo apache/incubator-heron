526e9cab7cdb3ccb0062078578163a1977a4f211
Karthik Ramasamy <kramasamy@twitter.com>
2016-03-09 11:53:40 -0800

    added centos release self extracting package

--- docker/compile.sh
+++ docker/compile.sh
@@ -15,9 +15,16 @@ bazel clean
 
 echo "Creating release packages"
 bazel build --config=$TARGET_PLATFORM release:packages
+bazel build --config=$TARGET_PLATFORM release/packages:heron-client-install.sh
 
-echo "Moving release files to /dist"
+echo "Moving release tar files to /dist"
 for file in ./bazel-bin/release/*.tar.gz; do
   filename=$(basename $file)
   cp $file /dist/${filename/.tar/-$HERON_VERSION.tar}
+done
+
+echo "Moving release self extracting binaries to /dist"
+for file in ./bazel-bin/release/*.sh; do
+  filename=$(basename $file)
+  cp $file /dist/${filename/.sh/-$HERON_VERSION.sh}
 done
