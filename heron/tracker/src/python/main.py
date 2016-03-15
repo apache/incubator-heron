1fe2082cf6603c7e2546375d5146ecd9ee407163
Karthik Ramasamy <kramasamy@twitter.com>
2016-03-14 18:05:35 -0700

    added packaging of heron tools conf to self heron tools tar ball

--- heron/tracker/src/python/main.py
+++ heron/tracker/src/python/main.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import argparse
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 from tornado.escape import json_encode, utf8
 from tornado.options import define, options
 
+from heron.tracker.src.python import utils
 from heron.tracker.src.python import handlers
 from heron.tracker.src.python import log
 from heron.tracker.src.python.log import Log as LOG
@@ -39,15 +41,104 @@ class Application(tornado.web.Application):
       (r"/topologies/histo", handlers.MemoryHistogramHandler, {"tracker":tracker}),
       (r"(.*)", handlers.DefaultHandler),
     ]
+
     settings = dict(
-      static_path = os.path.dirname(__file__),
       debug = True,
-      serve_traceback = True
+      serve_traceback = True,
+      static_path = os.path.dirname(__file__)
     )
     tornado.web.Application.__init__(self, tornadoHandlers, **settings)
 
+
+class _HelpAction(argparse._HelpAction):
+  def __call__(self, parser, namespace, values, option_string=None):
+    parser.print_help()
+
+    # retrieve subparsers from parser
+    subparsers_actions = [
+      action for action in parser._actions
+      if isinstance(action, argparse._SubParsersAction)]
+
+    # there will probably only be one subparser_action,
+    # but better save than sorry
+    for subparsers_action in subparsers_actions:
+      # get all subparsers and print help
+      for choice, subparser in subparsers_action.choices.items():
+        print("Subparser '{}'".format(choice))
+        print(subparser.format_help())
+
+    parser.exit()
+
+class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
+  def _format_action(self, action):
+    parts = super(argparse.RawDescriptionHelpFormatter, self)._format_action(action)
+    if action.nargs == argparse.PARSER:
+      parts = "\n".join(parts.split("\n")[1:])
+    return parts
+
+def add_titles(parser):
+  parser._positionals.title = "Required arguments"
+  parser._optionals.title = "Optional arguments"
+  return parser
+
+def add_config(parser):
+
+  # the default config file path
+  default_config_file = os.path.join(
+      utils.get_heron_tracker_conf_dir(), "localfilestateconf.yaml")
+
+  parser.add_argument(
+      '--config-file',
+      metavar='(a string; path to config file; default: "' + default_config_file + '")',
+      default=default_config_file)
+
+  parser.add_argument(
+      '--port',
+      metavar='(an integer; port to listen; default: 8888)',
+      type = int, 
+      default=8888)
+
+  return parser
+
+def create_parsers():
+  parser = argparse.ArgumentParser(
+      epilog = 'For detailed documentation, go to http://go/heron',
+      usage = "%(prog)s [options] <command>",
+      formatter_class=SubcommandHelpFormatter,
+      add_help = False)
+
+  parser = add_titles(parser)
+  parser = add_config(parser)
+
+  ya_parser = argparse.ArgumentParser(parents = [parser], add_help = False)
+  subparsers = ya_parser.add_subparsers(
+      title = "Available commands",
+      # metavar = 'help          Prints help')
+      metavar = '<command> [command-options]')
+
+  help_parser = subparsers.add_parser(
+      'help',
+      help='Prints help',
+      add_help = False)
+
+  help_parser.set_defaults(help=True)
+  return (parser, ya_parser)
+
 def main():
   log.configure(log.logging.DEBUG)
+
+  # create the parser and parse the arguments
+  # (parser, help_parser) = create_parsers()
+  
+  # (args, remaining) = parser.parse_known_args()
+
+  # if remaining:
+  #   yaargs = help_parser.parse_args(args = remaining, namespace=args)
+  #   print yaargs
+  # else:
+  #   print args
+  # sys.exit(0) 
+
   options.parse_command_line()
   port = options.port
   LOG.info("Running on port: " + str(port))
