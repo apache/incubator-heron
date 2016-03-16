1814308fb3b08158a06c84cdc392cf8228bedb73
Karthik Ramasamy <kramasamy@twitter.com>
2016-03-16 13:29:14 -0700

    added feedback from Chris - consts moved to consts.py and renamed variables meaningfully

---
+++ heron/ui/src/python/consts.py
@@ -0,0 +1,5 @@
+# default parameter - port for the web to ui to listen on
+DEFAULT_PORT = 8889
+
+# default parameter - url to connect to heron tracker
+DEFAULT_TRACKER_URL = "http://localhost:8888"
