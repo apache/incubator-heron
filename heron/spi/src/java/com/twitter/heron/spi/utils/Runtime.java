456a655fa734da9a75b7deb65cbe2070cc860235
Maosong Fu <mfu@twitter.com>
2016-03-22 17:07:51 -0700

    Refactor RuntimeManagerRunnable and add AuroraRuntimeManager and bug fixing 1. Refactor RuntimeManager to determine whether "no_scheduler_rest_enpoint" basing on config rather than    checking whether a location is reachable 2. Fix bugs in RuntimeManager, which currently will by mistake return early when no_scheduler_rest_endpoint,    without invoking post* logic, including states cleaning in killRequest. 3. Refactor the RuntimeManager to close resources, i.e. connection.disconnect(), in a try-finally style,    to avoid spreading the close in a lot of places, which is bug prone. 4. Add AuroraRuntimeManager 5. Add more utils methods in AuroraUtils.java

--- heron/spi/src/java/com/twitter/heron/spi/utils/Runtime.java
+++ heron/spi/src/java/com/twitter/heron/spi/utils/Runtime.java
@@ -1,7 +1,18 @@
 package com.twitter.heron.spi.utils;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
 import com.twitter.heron.api.generated.TopologyAPI;
+import com.twitter.heron.proto.tmaster.TopologyMaster;
 import com.twitter.heron.spi.common.Config;
+import com.twitter.heron.spi.common.HttpUtils;
 import com.twitter.heron.spi.common.Keys;
 import com.twitter.heron.spi.packing.IPacking;
 import com.twitter.heron.spi.scheduler.ILauncher;
@@ -9,6 +20,7 @@ import com.twitter.heron.spi.scheduler.IRuntimeManager;
 import com.twitter.heron.spi.statemgr.SchedulerStateManagerAdaptor;
 
 public class Runtime {
+  private static final Logger LOG = Logger.getLogger(Runtime.class.getName());
 
   public static String topologyId(Config runtime) {
     return runtime.getStringValue(Keys.topologyId());
@@ -68,5 +80,61 @@ public class Runtime {
 
   public static Long numContainers(Config runtime) {
     return runtime.getLongValue(Keys.numContainers());
+  }
+
+  /**
+   * Communicate with TMaster with command
+   *
+   * @param command the command requested to TMaster, activate or deactivate.
+   * @return true if the requested command is processed successfully by tmaster
+   */
+  public static boolean sendToTMaster(String command, String topologyName,
+                                      SchedulerStateManagerAdaptor stateManager) {
+    // fetch the TMasterLocation for the topology
+    LOG.info("Fetching TMaster location for topology: " + topologyName);
+    ListenableFuture<TopologyMaster.TMasterLocation> locationFuture =
+        stateManager.getTMasterLocation(null, topologyName);
+
+    TopologyMaster.TMasterLocation location =
+        NetworkUtils.awaitResult(locationFuture, 5, TimeUnit.SECONDS);
+    LOG.info("Fetched TMaster location for topology: " + topologyName);
+
+    // for the url request to be sent to TMaster
+    String endpoint = String.format("http://%s:%d/%s?topologyid=%s",
+        location.getHost(), location.getControllerPort(), command, location.getTopologyId());
+    LOG.info("HTTP URL for TMaster: " + endpoint);
+
+    // create a URL connection
+    HttpURLConnection connection = null;
+    try {
+      connection = (HttpURLConnection) new URL(endpoint).openConnection();
+    } catch (IOException e) {
+      LOG.log(Level.SEVERE, "Failed to get a HTTP connection to TMaster: ", e);
+      return false;
+    }
+    LOG.info("Successfully opened HTTP connection to TMaster");
+
+    // now sent the http request
+    HttpUtils.sendHttpGetRequest(connection);
+    LOG.info("Sent the HTTP payload to TMaster");
+
+    boolean success = false;
+    // get the response and check if it is successful
+    try {
+      int responseCode = connection.getResponseCode();
+      if (responseCode == HttpURLConnection.HTTP_OK) {
+        LOG.info("Successfully got a HTTP response from TMaster for " + command);
+        success = true;
+      } else {
+        LOG.info(String.format("Non OK HTTP response %d from TMaster for command %s",
+            responseCode, command));
+      }
+    } catch (IOException e) {
+      LOG.log(Level.SEVERE, "Failed to receive HTTP response from TMaster for " + command + " :", e);
+    } finally {
+      connection.disconnect();
+    }
+
+    return success;
   }
 }
