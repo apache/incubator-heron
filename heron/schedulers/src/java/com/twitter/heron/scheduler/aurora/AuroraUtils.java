a49db2ab41ae165469d8372f8f08776846040178
Maosong Fu <mfu@twitter.com>
2016-03-22 17:07:51 -0700

    Refactor RuntimeManagerRunnable and add AuroraRuntimeManager and bug fixing 1. Refactor RuntimeManager to determine whether "no_scheduler_rest_enpoint" basing on config rather than    checking whether a location is reachable 2. Fix bugs in RuntimeManager, which currently will by mistake return early when no_scheduler_rest_endpoint,    without invoking post* logic, including states cleaning in killRequest. 3. Refactor the RuntimeManager to close resources, i.e. connection.disconnect(), in a try-finally style,    to avoid spreading the close in a lot of places, which is bug prone. 4. Add AuroraRuntimeManager 5. Add more utils methods in AuroraUtils.java

--- heron/schedulers/src/java/com/twitter/heron/scheduler/aurora/AuroraUtils.java
+++ heron/schedulers/src/java/com/twitter/heron/scheduler/aurora/AuroraUtils.java
@@ -7,29 +7,83 @@ import java.util.logging.Logger;
 
 import com.twitter.heron.spi.common.ShellUtils;
 
+/**
+ * This file defines Utils methods used by Aurora
+ */
 public class AuroraUtils {
   private static final Logger LOG = Logger.getLogger(AuroraUtils.class.getName());
 
+  // Create an aurora job
   public static boolean createAuroraJob(String jobName, String cluster, String role, String env,
-                                        String auroraFilename, Map<String, String> bindings) {
+                                        String auroraFilename, Map<String, String> bindings,
+                                        boolean isVerbose) {
     ArrayList<String> auroraCmd = new ArrayList<>(Arrays.asList(
-        "aurora", "job", "create", "--verbose",
-        "--wait-until", "RUNNING"));
+        "aurora", "job", "create", "--wait-until", "RUNNING"));
 
     for (Map.Entry<String, String> binding : bindings.entrySet()) {
       auroraCmd.add("--bind");
       auroraCmd.add(String.format("%s=%s", binding.getKey(), binding.getValue()));
     }
 
-    auroraCmd.add(String.format("%s/%s/%s/%s",
-        cluster,
-        role,
-        env,
-        jobName));
+    String jobSpec = String.format("%s/%s/%s/%s", cluster, role, env, jobName);
+    auroraCmd.add(jobSpec);
     auroraCmd.add(auroraFilename);
 
-    String[] cmdline = auroraCmd.toArray(new String[auroraCmd.size()]);
-    LOG.info("cmdline=" + Arrays.toString(cmdline));
+    if (isVerbose) {
+      auroraCmd.add("--verbose");
+    }
+
+    String[] cmdline = constructCmdline(auroraCmd);
     return 0 == ShellUtils.runProcess(true, cmdline, new StringBuilder(), new StringBuilder());
+  }
+
+  // Kill an aurora job
+  public static boolean killAuroraJob(String jobName, String cluster, String role, String env,
+                                      boolean isVerbose) {
+    ArrayList<String> auroraCmd = new ArrayList<>(Arrays.asList(
+        "aurora", "job", "killall"));
+    String jobSpec = String.format("%s/%s/%s/%s", cluster, role, env, jobName);
+    auroraCmd.add(jobSpec);
+
+    appendAuroraCommandOptions(auroraCmd, isVerbose);
+
+    String[] cmdline = constructCmdline(auroraCmd);
+    return 0 == ShellUtils.runProcess(isVerbose, cmdline, new StringBuilder(), new StringBuilder());
+  }
+
+  // Restart an aurora job
+  public static boolean restartAuroraJob(String jobName, String cluster, String role, String env,
+                                         int containerId, boolean isVerbose) {
+    ArrayList<String> auroraCmd = new ArrayList<>(Arrays.asList(
+        "aurora", "job", "restart"));
+    String jobSpec = String.format("%s/%s/%s/%s", cluster, role, env, jobName);
+    if (containerId != -1) {
+      jobSpec = String.format("%s/%s", jobSpec, "" + containerId);
+    }
+    auroraCmd.add(jobSpec);
+
+    appendAuroraCommandOptions(auroraCmd, isVerbose);
+
+    String[] cmdline = constructCmdline(auroraCmd);
+    return 0 == ShellUtils.runProcess(isVerbose, cmdline, new StringBuilder(), new StringBuilder());
+  }
+
+  // Static method to append verbose and batching options if needed
+  public static void appendAuroraCommandOptions(ArrayList<String> auroraCmd,
+                                                boolean isVerbose) {
+    if (isVerbose) {
+      auroraCmd.add("--verbose");
+    }
+    auroraCmd.add("--no-batching");
+
+    return;
+  }
+
+  // Convert command from ArrayList<String> to String[] format
+  // Log the command to execute
+  public static String[] constructCmdline(ArrayList<String> command) {
+    String[] cmdline = command.toArray(new String[command.size()]);
+    LOG.info("cmdline=" + Arrays.toString(cmdline));
+    return cmdline;
   }
 }
