5c02d6bda0b66a43883b7fc25dcc4c488ae2c81b
Sailesh Mittal <smittal@twitter.com>
2016-03-24 14:17:45 -0700

    Fix typo in pidhandler.py

--- heron/tracker/src/python/handlers/pidhandler.py
+++ heron/tracker/src/python/handlers/pidhandler.py
@@ -54,7 +54,7 @@ class PidHandler(BaseHandler):
       environ = self.get_argument_environ()
       topology_name = self.get_argument_topology()
       instance = self.get_argument_instance()
-      topology_info = self.tracker.getTopologyInfo(topology, cluster, environ)
+      topology_info = self.tracker.getTopologyInfo(topology_name, cluster, environ)
       result = yield getInstancePid(topology_info, instance)
       self.write_success_response(result)
     except Exception as e:
