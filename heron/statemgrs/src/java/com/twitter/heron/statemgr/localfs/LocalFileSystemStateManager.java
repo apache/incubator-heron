796e4560b40860e22fe601649e8fcadee8865e46
Maosong Fu <mfu@twitter.com>
2016-04-07 17:30:18 -0700

    Provide utils method to reduce duplicated codes in LocalFileSystemStateManager

--- heron/statemgrs/src/java/com/twitter/heron/statemgr/localfs/LocalFileSystemStateManager.java
+++ heron/statemgrs/src/java/com/twitter/heron/statemgr/localfs/LocalFileSystemStateManager.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
 
 import com.twitter.heron.api.generated.TopologyAPI;
 import com.twitter.heron.common.basics.FileUtils;
@@ -38,9 +39,9 @@ public class LocalFileSystemStateManager extends FileSystemStateManager {
 
   protected boolean initTree() {
     // Make necessary directories
-    LOG.info("Topologies directory: " + getTopologyDir()); 
+    LOG.info("Topologies directory: " + getTopologyDir());
     LOG.info("Tmaster location directory: " + getTMasterLocationDir());
-    LOG.info("Physical plan directory: " + getPhysicalPlanDir()); 
+    LOG.info("Physical plan directory: " + getPhysicalPlanDir());
     LOG.info("Execution state directory: " + getExecutionStateDir());
     LOG.info("Scheduler location directory: " + getSchedulerLocationDir());
 
@@ -55,204 +56,114 @@ public class LocalFileSystemStateManager extends FileSystemStateManager {
     return false;
   }
 
-  @Override
-  public ListenableFuture<Boolean> setExecutionState(
-      ExecutionEnvironment.ExecutionState executionState, String topologyName) {
+  protected ListenableFuture<Boolean> setData(String path, byte[] data) {
     SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.writeToFile(getExecutionStatePath(topologyName),
-        executionState.toByteArray());
+    boolean ret = FileUtils.writeToFile(path, data);
     future.set(ret);
 
     return future;
   }
 
-
-  @Override
-  public ListenableFuture<Boolean> setTMasterLocation(TopologyMaster.TMasterLocation location, String topologyName) {
+  protected ListenableFuture<Boolean> deleteData(String path) {
     SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.writeToFile(getTMasterLocationPath(topologyName),
-        location.toByteArray());
+    boolean ret = FileUtils.deleteFile(path);
     future.set(ret);
 
     return future;
+  }
+
+  protected <M extends Message> ListenableFuture<M> getData(String path, M.Builder builder) {
+    SettableFuture<M> future = SettableFuture.create();
+    byte[] data = FileUtils.readFromFile(path);
+    if (data.length == 0) {
+      future.set(null);
+      return future;
+    }
+
+    try {
+      builder.mergeFrom(data);
+      future.set((M) builder.build());
+    } catch (InvalidProtocolBufferException e) {
+      future.setException(new RuntimeException("Could not parse " + M.Builder.class, e));
+    }
+
+    return future;
+  }
+
+  @Override
+  public ListenableFuture<Boolean> setExecutionState(
+      ExecutionEnvironment.ExecutionState executionState, String topologyName) {
+    return setData(getExecutionStatePath(topologyName), executionState.toByteArray());
+  }
+
+  @Override
+  public ListenableFuture<Boolean> setTMasterLocation(TopologyMaster.TMasterLocation location, String topologyName) {
+    return setData(getTMasterLocationPath(topologyName), location.toByteArray());
   }
 
   @Override
   public ListenableFuture<Boolean> setTopology(TopologyAPI.Topology topology, String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.writeToFile(getTopologyPath(topologyName), topology.toByteArray());
-    future.set(ret);
-
-    return future;
+    return setData(getTopologyPath(topologyName), topology.toByteArray());
   }
 
   @Override
   public ListenableFuture<Boolean> setPhysicalPlan(PhysicalPlans.PhysicalPlan physicalPlan, String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.writeToFile(getPhysicalPlanPath(topologyName), physicalPlan.toByteArray());
-    future.set(ret);
-
-    return future;
+    return setData(getPhysicalPlanPath(topologyName), physicalPlan.toByteArray());
   }
 
   @Override
   public ListenableFuture<Boolean> setSchedulerLocation(Scheduler.SchedulerLocation location, String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.writeToFile(getSchedulerLocationPath(topologyName),
-        location.toByteArray());
-    future.set(ret);
-
-    return future;
+    return setData(getSchedulerLocationPath(topologyName), location.toByteArray());
   }
 
   @Override
   public ListenableFuture<Boolean> deleteTMasterLocation(String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.deleteFile(getTMasterLocationPath(topologyName));
-    future.set(ret);
-
-    return future;
+    return deleteData(getTMasterLocationPath(topologyName));
   }
 
   @Override
   public ListenableFuture<Boolean> deleteSchedulerLocation(String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.deleteFile(getSchedulerLocationPath(topologyName));
-    future.set(ret);
-
-    return future;
+    return deleteData(getSchedulerLocationPath(topologyName));
   }
 
   @Override
   public ListenableFuture<Boolean> deleteExecutionState(String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.deleteFile(getExecutionStatePath(topologyName));
-    future.set(ret);
-
-    return future;
+    return deleteData(getExecutionStatePath(topologyName));
   }
 
   @Override
   public ListenableFuture<Boolean> deleteTopology(String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.deleteFile(getTopologyPath(topologyName));
-    future.set(ret);
-
-    return future;
+    return deleteData(getTopologyPath(topologyName));
   }
 
   @Override
   public ListenableFuture<Boolean> deletePhysicalPlan(String topologyName) {
-    SettableFuture<Boolean> future = SettableFuture.create();
-    boolean ret = FileUtils.deleteFile(getPhysicalPlanPath(topologyName));
-    future.set(ret);
-
-    return future;
+    return deleteData(getPhysicalPlanPath(topologyName));
   }
 
   @Override
   public ListenableFuture<Scheduler.SchedulerLocation> getSchedulerLocation(WatchCallback watcher, String topologyName) {
-    SettableFuture<Scheduler.SchedulerLocation> future = SettableFuture.create();
-    String path = getSchedulerLocationPath(topologyName);
-    byte[] data = FileUtils.readFromFile(path);
-    if(data.length == 0){
-      future.set(null);
-      return future;
-    }
-
-    Scheduler.SchedulerLocation location;
-    try {
-      location = Scheduler.SchedulerLocation.parseFrom(data);
-      future.set(location);
-    } catch (InvalidProtocolBufferException e) {
-      future.setException(new RuntimeException("Could not parse SchedulerLocation", e));
-    }
-
-    return future;
+    return getData(getSchedulerLocationPath(topologyName), Scheduler.SchedulerLocation.newBuilder());
   }
 
   @Override
   public ListenableFuture<TopologyAPI.Topology> getTopology(WatchCallback watcher, String topologyName) {
-    SettableFuture<TopologyAPI.Topology> future = SettableFuture.create();
-    String path = getTopologyPath(topologyName);
-    byte[] data = FileUtils.readFromFile(path);
-    if(data.length == 0){
-      future.set(null);
-      return future;
-    }
-
-    TopologyAPI.Topology topology;
-    try {
-      topology = TopologyAPI.Topology.parseFrom(data);
-      future.set(topology);
-    } catch (InvalidProtocolBufferException e) {
-      future.setException(new RuntimeException("Could not parse SchedulerLocation", e));
-    }
-
-    return future;
+    return getData(getTopologyPath(topologyName), TopologyAPI.Topology.newBuilder());
   }
 
   @Override
   public ListenableFuture<ExecutionEnvironment.ExecutionState> getExecutionState(WatchCallback watcher, String topologyName) {
-    SettableFuture<ExecutionEnvironment.ExecutionState> future = SettableFuture.create();
-    String path = getExecutionStatePath(topologyName);
-    byte[] data = FileUtils.readFromFile(path);
-    if(data.length == 0){
-      future.set(null);
-      return future;
-    }
-
-    ExecutionEnvironment.ExecutionState executionState;
-    try {
-      executionState = ExecutionEnvironment.ExecutionState.parseFrom(data);
-      future.set(executionState);
-    } catch (InvalidProtocolBufferException e) {
-      future.setException(new RuntimeException("Could not parse SchedulerLocation", e));
-    }
-
-    return future;
+    return getData(getExecutionStatePath(topologyName), ExecutionEnvironment.ExecutionState.newBuilder());
   }
 
   @Override
   public ListenableFuture<PhysicalPlans.PhysicalPlan> getPhysicalPlan(WatchCallback watcher, String topologyName) {
-    SettableFuture<PhysicalPlans.PhysicalPlan> future = SettableFuture.create();
-    String path = getPhysicalPlanPath(topologyName);
-    byte[] data = FileUtils.readFromFile(path);
-    if(data.length == 0){
-      future.set(null);
-      return future;
-    }
-
-    PhysicalPlans.PhysicalPlan physicalPlan;
-    try {
-      physicalPlan = PhysicalPlans.PhysicalPlan.parseFrom(data);
-      future.set(physicalPlan);
-    } catch (InvalidProtocolBufferException e) {
-      future.setException(new RuntimeException("Could not parse SchedulerLocation", e));
-    }
-
-    return future;
+    return getData(getPhysicalPlanPath(topologyName), PhysicalPlans.PhysicalPlan.newBuilder());
   }
 
   @Override
   public ListenableFuture<TopologyMaster.TMasterLocation> getTMasterLocation(WatchCallback watcher, String topologyName) {
-    SettableFuture<TopologyMaster.TMasterLocation> future = SettableFuture.create();
-    String path = getTMasterLocationPath(topologyName);
-    byte[] data = FileUtils.readFromFile(path);
-    if(data.length == 0){
-      future.set(null);
-      return future;
-    }
-
-    TopologyMaster.TMasterLocation location;
-    try {
-      location = TopologyMaster.TMasterLocation.parseFrom(data);
-      future.set(location);
-    } catch (InvalidProtocolBufferException e) {
-      future.setException(new RuntimeException("Could not parse TMasterLocation", e));
-    }
-
-    return future;
+    return getData(getTMasterLocationPath(topologyName), TopologyMaster.TMasterLocation.newBuilder());
   }
 
   @Override
