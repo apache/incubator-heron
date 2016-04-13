dbbb97d2eff99618793c3b51e4274ed865a7b1c3
Maosong Fu <maosongfu@gmail.com>
2016-04-13 14:18:49 -0700

    Make all calls in SchedulerStateManagerAdaptor synchronized (#342)
    1. Make all calls in SchedulerStateManagerAdaptor synchronized, i.e. wait for the result unless timeout
    2. Correct corresponding invocation of methods in SchedulerStateManagerAdaptor
    3. Fix some misuse of IStateManager or SchedulerStateManagerAdaptor

--- heron/spi/src/java/com/twitter/heron/spi/utils/NetworkUtils.java
+++ heron/spi/src/java/com/twitter/heron/spi/utils/NetworkUtils.java
@@ -15,27 +15,8 @@
 package com.twitter.heron.spi.utils;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ProtocolException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
-import java.net.URL;
-import java.net.UnknownHostException;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import com.google.common.util.concurrent.ListenableFuture;
-import com.sun.net.httpserver.HttpExchange;
 
 import com.twitter.heron.proto.system.Common;
 
@@ -43,23 +24,7 @@ import com.twitter.heron.proto.system.Common;
  * Utilities related to network.
  */
 public class NetworkUtils {
-  public static final String CONTENT_LENGTH = "Content-Length";
-  public static final String CONTENT_TYPE = "Content-Type";
-
   private static final Logger LOG = Logger.getLogger(NetworkUtils.class.getName());
-
-  /**
-   * Waits for ListenableFuture to terminate. Cancels on timeout
-   */
-  public static <V> V awaitResult(ListenableFuture<V> future, int time, TimeUnit unit) {
-    try {
-      return future.get(time, unit);
-    } catch (InterruptedException | TimeoutException | ExecutionException e) {
-      LOG.log(Level.SEVERE, "Exception processing future ", e);
-      future.cancel(true);
-      return null;
-    }
-  }
 
   /**
    * Get available port.
