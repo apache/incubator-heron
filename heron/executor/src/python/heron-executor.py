218ae4728158050ae28a292dfa0f7aac92d77323
Karthik Ramasamy <kramasamy@gmail.com>
2016-04-19 22:45:27 -0700

    corner cases for version checking in centos (#399)
    * fixed version checking
    
    * add /usr/bin/env python2.7 in python files that invoke main, for rest of the files, we removed it

--- heron/executor/src/python/heron-executor.py
+++ heron/executor/src/python/heron-executor.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/python2.7
+#!/usr/bin/env python2.7
 
 import atexit
 import datetime
--- heron/ui/src/python/main.py
+++ heron/ui/src/python/main.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#!/usr/bin/env python2.7
+
 import os, sys
 import argparse
 
