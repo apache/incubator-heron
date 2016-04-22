67c54520c453188a67fb0664c7fa7af8d9da9a8c
Lewis Kaneshiro <lewiskan@Lewiss-MacBook-Pro.local>
2016-04-22 08:18:03 -0700

    linkchecker formatted whitespace

--- website/linkchecker.sh
+++ website/linkchecker.sh
@@ -6,11 +6,11 @@ rm -f linkchecker-errors.csv && rm -f linkchecker-out.csv
 linkchecker public/index.html --no-warnings -F csv 
 #uses error code: on fail, write linkchecker-errors.csv for debugging
 if [[ $? == 1 ]]; then 
-	cut -sd ';' -f 1,2 linkchecker-out.csv | tr ';' ' ' | 
-    awk '{ print $2 " " $1}' | sort -u >> linkchecker-errors.csv; 
-    echo "linkchecker failed - check linkchecker-errors.csv"; 
-else 
-	echo "linkchecker passes"; 
+  cut -sd ';' -f 1,2 linkchecker-out.csv | tr ';' ' ' | \
+  awk '{ print $2 " " $1}' | sort -u >> linkchecker-errors.csv;
+  echo "linkchecker failed - check linkchecker-errors.csv"; 
+else
+  echo "linkchecker passes"; 
 fi 
 
 rm -f linkchecker-out.csv
