3b2f5e208f53507b176c44203bf54b3bf63ecd47
Vikas RK <vikasr@twitter.com>
2016-04-28 14:44:49 -0700

    fix cpp style for heron/common/src/cpp/config (#488)

--- heron/common/src/cpp/config/cluster-config-reader.cpp
+++ heron/common/src/cpp/config/cluster-config-reader.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "yaml-cpp/yaml.h"
 #include "proto/messages.h"
 
@@ -10,33 +26,27 @@
 #include "config/cluster-config-vars.h"
 #include "config/cluster-config-reader.h"
 
-namespace heron { namespace config {
+namespace heron {
+namespace config {
 
-ClusterConfigReader::ClusterConfigReader(EventLoop* eventLoop,
-                                         const sp_string& _defaults_file)
-  : YamlFileReader(eventLoop, _defaults_file)
-{
+ClusterConfigReader::ClusterConfigReader(EventLoop* eventLoop, const sp_string& _defaults_file)
+    : YamlFileReader(eventLoop, _defaults_file) {
   LoadConfig();
 }
 
-ClusterConfigReader::~ClusterConfigReader()
-{
-}
+ClusterConfigReader::~ClusterConfigReader() {}
 
-void ClusterConfigReader::FillClusterConfig(proto::api::Topology* _topology)
-{
+void ClusterConfigReader::FillClusterConfig(proto::api::Topology* _topology) {
   // Fill in the cluster config
-  for (YAML::const_iterator iter = config_.begin();
-       iter != config_.end(); ++iter) {
+  for (YAML::const_iterator iter = config_.begin(); iter != config_.end(); ++iter) {
     proto::api::Config::KeyValue* kv = _topology->mutable_topology_config()->add_kvs();
     kv->set_key(iter->first.as<sp_string>());
     kv->set_value(iter->second.as<sp_string>());
   }
 }
 
-void ClusterConfigReader::OnConfigFileLoad()
-{
+void ClusterConfigReader::OnConfigFileLoad() {
   AddIfMissing(ClusterConfigVars::CLUSTER_METRICS_INTERVAL, "60");
 }
-
-}} // end namespace
+}  // namespace config
+}  // namespace heron
--- heron/common/src/cpp/config/operational-config-vars.cpp
+++ heron/common/src/cpp/config/operational-config-vars.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "proto/messages.h"
 #include "basics/basics.h"
 #include "errors/errors.h"
@@ -6,8 +22,10 @@
 
 #include "config/operational-config-vars.h"
 
-namespace heron { namespace config {
+namespace heron {
+namespace config {
 
-const sp_string OperationalConfigVars::TOPOLOGY_RELEASE_OVERRIDES = "operational.topology.release.overrides";
-
-}} // end namespace
+const sp_string OperationalConfigVars::TOPOLOGY_RELEASE_OVERRIDES =
+    "operational.topology.release.overrides";
+}  // namespace config
+}  // namespace heron
