378cf4d042c807deb676e670acb590f7f66ceb22
Vikas RK <vikasr@twitter.com>
2016-04-29 14:04:50 -0700

    fix cpp style for heron/tmaster/src/cpp (#509)

--- heron/tmaster/src/cpp/manager/stmgrstate.h
+++ heron/tmaster/src/cpp/manager/stmgrstate.h
@@ -1,25 +1,47 @@
+/*
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef __STMANAGERSTATE_H
 #define __STMANAGERSTATE_H
 
 #include <string>
 #include <vector>
+#include "network/network.h"
+#include "proto/tmaster.pb.h"
+#include "basics/basics.h"
 
-namespace heron { namespace proto { namespace system {
-  class StMgr;
-  class StMgrStats;
-  class PhysicalPlan;
-}}}
+namespace heron {
+namespace proto {
+namespace system {
+class StMgr;
+class StMgrStats;
+class PhysicalPlan;
+}
+}
+}
 
-namespace heron { namespace tmaster {
+namespace heron {
+namespace tmaster {
 
 class TMasterServer;
 
-class StMgrState
-{
+class StMgrState {
  public:
   StMgrState(Connection* _conn, const proto::system::StMgr& _info,
-             const std::vector<proto::system::Instance*>& _instances,
-             TMasterServer* _server);
+             const std::vector<proto::system::Instance*>& _instances, TMasterServer* _server);
   virtual ~StMgrState();
 
   void UpdateWithNewStMgr(const proto::system::StMgr& _info,
@@ -44,21 +66,21 @@ class StMgrState
 
  private:
   // The last time we got a hearbeat from this stmgr
-  sp_int64                                  last_heartbeat_;
+  sp_int64 last_heartbeat_;
   // The stats that was reported last time
-  proto::system::StMgrStats*                last_stats_;
+  proto::system::StMgrStats* last_stats_;
 
   // All the instances on this stmgr
-  std::vector<proto::system::Instance*>          instances_;
+  std::vector<proto::system::Instance*> instances_;
 
   // The info about this stmgr
-  proto::system::StMgr*                     stmgr_;
+  proto::system::StMgr* stmgr_;
   // The connection used by the nodemanager to contact us
-  Connection*                               connection_;
+  Connection* connection_;
   // Our link to our TMaster
-  TMasterServer*                            server_;
+  TMasterServer* server_;
 };
-
-}} // end namespace
+}  // namespace tmaster
+}  // namespace heron
 
 #endif
