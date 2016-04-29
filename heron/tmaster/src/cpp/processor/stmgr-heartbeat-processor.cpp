d0d76879eae0aa02fbf92c9dec564c6549b6c7af
Vikas Kedigehalli <vikasr@twitter.com>
2016-04-29 12:17:02 -0700

    fix cpp style for heron/tmaster/src/cpp

--- heron/tmaster/src/cpp/processor/stmgr-heartbeat-processor.cpp
+++ heron/tmaster/src/cpp/processor/stmgr-heartbeat-processor.cpp
@@ -1,47 +1,52 @@
-#include <iostream>
+/*
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+#include "processor/stmgr-heartbeat-processor.h"
+#include <iostream>
+#include "processor/tmaster-processor.h"
+#include "manager/tmaster.h"
 #include "proto/messages.h"
 #include "basics/basics.h"
 #include "errors/errors.h"
 #include "threads/threads.h"
 #include "network/network.h"
 
-#include "manager/tmaster.h"
-#include "processor/tmaster-processor.h"
-#include "processor/stmgr-heartbeat-processor.h"
+namespace heron {
+namespace tmaster {
 
-namespace heron { namespace tmaster {
+StMgrHeartbeatProcessor::StMgrHeartbeatProcessor(REQID reqid, Connection* conn,
+                                                 proto::tmaster::StMgrHeartbeatRequest* request,
+                                                 TMaster* tmaster, Server* server)
+    : Processor(reqid, conn, request, tmaster, server) {}
 
-StMgrHeartbeatProcessor::StMgrHeartbeatProcessor(
-  REQID                                    reqid,
-  Connection*                              conn, 
-  proto::tmaster::StMgrHeartbeatRequest*   request,
-  TMaster*                                 tmaster,
-  Server*                                  server)
-  : Processor(reqid, conn, request, tmaster, server)
-{
-}
-
-StMgrHeartbeatProcessor::~StMgrHeartbeatProcessor()
-{
+StMgrHeartbeatProcessor::~StMgrHeartbeatProcessor() {
   // nothing to be done here
 }
 
-void
-StMgrHeartbeatProcessor::Start()
-{
-  proto::tmaster::StMgrHeartbeatRequest* request = 
-            static_cast<proto::tmaster::StMgrHeartbeatRequest*>(request_);
+void StMgrHeartbeatProcessor::Start() {
+  proto::tmaster::StMgrHeartbeatRequest* request =
+      static_cast<proto::tmaster::StMgrHeartbeatRequest*>(request_);
 
-  proto::system::Status* status = 
-              tmaster_->UpdateStMgrHeartbeat(GetConnection(),
-                                             request->heartbeat_time(),
-                                             request->release_stats());
-  
-  proto::tmaster::StMgrHeartbeatResponse response;              
+  proto::system::Status* status = tmaster_->UpdateStMgrHeartbeat(
+      GetConnection(), request->heartbeat_time(), request->release_stats());
+
+  proto::tmaster::StMgrHeartbeatResponse response;
   response.set_allocated_status(status);
   SendResponse(response);
   delete this;
 }
-
-}} // end namespace
+}  // namespace tmaster
+}  // namespace heron
