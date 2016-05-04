3a94b7f4f054d55f9b8f56da40a28b920fde24f3
Bill Graham <billg@twitter.com>
2016-05-04 16:14:41 -0700

    Consolidate TypeUtils in c.t.b.common.util (#549)
    * Consolidate TypeUtils in c.t.b.common.util
    
    * Handle nulls in conversion to List

--- heron/uploaders/src/java/com/twitter/heron/uploader/NullUploader.java
+++ heron/uploaders/src/java/com/twitter/heron/uploader/NullUploader.java
@@ -16,8 +16,8 @@ package com.twitter.heron.uploader;
 
 import java.net.URI;
 
+import com.twitter.heron.common.basics.TypeUtils;
 import com.twitter.heron.spi.common.Config;
-import com.twitter.heron.spi.common.Convert;
 import com.twitter.heron.spi.uploader.IUploader;
 
 public class NullUploader implements IUploader {
@@ -29,7 +29,7 @@ public class NullUploader implements IUploader {
   @Override
   public URI uploadPackage() {
     // Construct a URI from valid syntax String
-    return Convert.getURI("null://uploader:9519/w#lanfang");
+    return TypeUtils.getURI("null://uploader:9519/w#lanfang");
   }
 
   @Override
