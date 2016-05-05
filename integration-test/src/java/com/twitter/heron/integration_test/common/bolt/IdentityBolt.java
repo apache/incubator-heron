aa325e889a84d05c78ca7af6f2a7dae00d3d309f
Neng Lu <freeneng@gmail.com>
2016-05-04 20:21:20 -0700

    fix stylechecks in integration test (#557)

--- integration-test/src/java/com/twitter/heron/integration_test/common/bolt/IdentityBolt.java
+++ integration-test/src/java/com/twitter/heron/integration_test/common/bolt/IdentityBolt.java
@@ -1,3 +1,16 @@
+// Copyright 2016 Twitter. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.twitter.heron.integration_test.common.bolt;
 
 import com.twitter.heron.api.bolt.BaseBasicBolt;
@@ -8,10 +21,10 @@ import com.twitter.heron.api.tuple.Tuple;
 
 public class IdentityBolt extends BaseBasicBolt {
   private static final long serialVersionUID = 2167298598594517481L;
-  Fields _fields;
+  private Fields fields;
 
   public IdentityBolt(Fields fields) {
-    _fields = fields;
+    this.fields = fields;
   }
 
   @Override
@@ -21,6 +34,6 @@ public class IdentityBolt extends BaseBasicBolt {
 
   @Override
   public void declareOutputFields(OutputFieldsDeclarer declarer) {
-    declarer.declare(_fields);
+    declarer.declare(fields);
   }
 }
