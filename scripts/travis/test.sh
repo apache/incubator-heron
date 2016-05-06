cbea13a88d1fae6cb36cf3040aaef9352d1d0244
Neng Lu <nlu@twitter.com>
2016-05-06 12:36:35 -0700

    simplify integration test script

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -14,8 +14,7 @@ bazel --bazelrc=tools/travis-ci/bazel.rc run -- scripts/packages:heron-client-in
 python integration-test/src/python/local_test_runner/main.py
 
 # build integration test tools
-bazel --bazelrc=tools/travis-ci/bazel.rc build integration-test/src/{python,java}/...
-bazel --bazelrc=tools/travis-ci/bazel.rc build integration-test/src/java:integration-tests_deploy.jar
+bazel --bazelrc=tools/travis-ci/bazel.rc build integration-test/src/...
 
 # run integration test
 ./bazel-bin/integration-test/src/python/http_server/http-server 8080 &
@@ -23,7 +22,7 @@ http_server_id=$!
 trap "kill -9 $http_server_id" SIGINT SIGTERM EXIT
 
 ./bazel-bin/integration-test/src/python/test_runner/test-runner.pex \
-  -hc heron -tj  bazel-bin/integration-test/src/java/integration-tests_deploy.jar \
+  -hc heron -tj bazel-genfiles/integration-test/src/java/integration-tests.jar \
   -rh localhost -rp 8080\
   -tp integration-test/src/java/com/twitter/heron/integration_test/topology/ \
   -cl local -rl heron-staging -ev devel -pi file://{$HOME}/.heron/dist/heron-core.tar.gz
