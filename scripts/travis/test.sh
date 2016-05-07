9e4581f4220e7a8540226d4070ab0ec96198ceb3
Neng Lu <freeneng@gmail.com>
2016-05-06 17:07:58 -0700

    enable integration tests (#565)
    * enable integration tests

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -5,10 +5,22 @@
 set -ex
 
 # build test related jar
-bazel --bazelrc=tools/travis-ci/bazel.rc build integration-test/src/java:local-integration-tests_deploy.jar
+bazel --bazelrc=tools/travis-ci/bazel.rc build integration-test/src/...
 
 # install client
 bazel --bazelrc=tools/travis-ci/bazel.rc run -- scripts/packages:heron-client-install.sh --user
 
-# run the local integration test
+# run local integration test
 python integration-test/src/python/local_test_runner/main.py
+
+# run integration test
+./bazel-bin/integration-test/src/python/http_server/http-server 8080 &
+http_server_id=$!
+trap "kill -9 $http_server_id" SIGINT SIGTERM EXIT
+
+./bazel-bin/integration-test/src/python/test_runner/test-runner.pex \
+  -hc heron -tj bazel-genfiles/integration-test/src/java/integration-tests.jar \
+  -rh localhost -rp 8080\
+  -tp integration-test/src/java/com/twitter/heron/integration_test/topology/ \
+  -cl local -rl heron-staging -ev devel -pi file://{$HOME}/.heron/dist/heron-core.tar.gz
+
