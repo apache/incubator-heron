97a5ad74a9a953fad46a50b1c5f071326eb61392
Bill Graham <billg@twitter.com>
2016-05-11 13:34:34 -0700

    Add debugging

--- 3rdparty/pex/pex/iterator.py
+++ 3rdparty/pex/pex/iterator.py
@@ -24,7 +24,14 @@ class Iterator(IteratorInterface):
 
   def __init__(self, fetchers=None, crawler=None, follow_links=False):
     self._crawler = crawler or Crawler()
-    self._fetchers = fetchers if fetchers is not None else [PyPIFetcher()]
+    if fetchers is not None:
+      for fetcher in fetchers:
+        if not isinstance(fetcher, Fetcher):
+          raise ValueError('fetcher %s not an instance of Fetcher' % fetcher)
+        print ("Valid fetcher found %s" + fetcher)
+      self._fetchers = fetchers
+    else:
+      self._fetchers = [PyPIFetcher()]
     self.__follow_links = follow_links
 
   def _iter_requirement_urls(self, req):
