0597e5bda09e69d6bc4fba3200c89a89e18da1db
Vikas Kedigehalli <vikasr@twitter.com>
2016-05-13 12:37:15 -0700

    address comments

--- heron/common/src/python/network/packet.py
+++ heron/common/src/python/network/packet.py
@@ -9,22 +9,24 @@ class IncomingPacket:
   have a better name for this class
   """
 
-  intSizeInBytes = 4
+  INT_SIZE_IN_BYTES = 4
 
   def __init__(self, data):
     self.data = data
     self.position = 0
 
   def unpackInt(self, pos):
-    # Unpack int from bytes represented in newtork order (big-endian)
-    return struct.unpack(">I", self.data[pos: pos + self.intSizeInBytes])[0]
+    """
+    Unpack int from bytes represented in newtork order (big-endian)
+    """
+    return struct.unpack(">I", self.data[pos: pos + self.INT_SIZE_IN_BYTES])[0]
 
   def unpackTypename(self):
     """
     Get the fully qualified type name of the protobuf message
     """
     size = self.unpackInt(self.position)
-    self.position += self.intSizeInBytes
+    self.position += self.INT_SIZE_IN_BYTES
     typename = self.data[self.position: self.position + size]
     self.position += size
     return typename
@@ -42,7 +44,7 @@ class IncomingPacket:
     Deserialize the protobuf message
     """
     messageSize = self.unpackInt(self.position)
-    self.position += self.intSizeInBytes
+    self.position += self.INT_SIZE_IN_BYTES
     message.ParseFromString(self.data[self.position : self.position + messageSize])
     return message
 
@@ -52,9 +54,9 @@ def pack(reqId, message):
   <overall data size> <typename size> <typename> <reqid> <proto byte size> <serialized proto>
   """
 
-  intSizeInBytes = 4
+  INT_SIZE_IN_BYTES = 4
   typename = message.DESCRIPTOR.full_name
-  dataSize = intSizeInBytes + len(typename) + ReqId.size + intSizeInBytes + message.ByteSize()
+  dataSize = INT_SIZE_IN_BYTES + len(typename) + ReqId.size + INT_SIZE_IN_BYTES + message.ByteSize()
   data = struct.pack(">I", dataSize)
   data = data + struct.pack(">I", len(typename))
   data = data + typename
