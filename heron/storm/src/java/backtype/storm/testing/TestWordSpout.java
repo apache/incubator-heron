4aef69bb77302198628b471de9b761ceb88ad0c0
Neng Lu <nlu@twitter.com>
2016-05-19 17:49:28 -0700

    fix code style

--- heron/storm/src/java/backtype/storm/testing/TestWordSpout.java
+++ heron/storm/src/java/backtype/storm/testing/TestWordSpout.java
@@ -17,8 +17,8 @@ package backtype.storm.testing;
 import java.util.Map;
 import java.util.Random;
 
-import backtype.storm.task.TopologyContext;
 import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
