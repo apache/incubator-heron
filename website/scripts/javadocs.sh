2b0266d969f1696549505d244b85c4671e82e1b6
Luc Perkins <lucperkins@gmail.com>
2016-05-22 16:10:31 -0700

    add protobuf directories to javadocs script

--- website/scripts/javadocs.sh
+++ website/scripts/javadocs.sh
@@ -5,12 +5,14 @@
 
 HERON_ROOT_DIR=$(git rev-parse --show-toplevel)
 JAVADOC_OUTPUT_DIR=$HERON_ROOT_DIR/website/public/api
+GEN_PROTO_DIR=$HERON_ROOT_DIR/bazel-bin/heron/proto/_javac
 
 SRC_FILES=`find $HERON_ROOT_DIR -path "*/com/twitter/*" -name "*.java"`
+GEN_FILES=`find $GEN_PROTO_DIR -name "*.java"`
 
 rm -r $JAVADOC_OUTPUT_DIR
 mkdir -p $JAVADOC_OUTPUT_DIR
-javadoc -quiet -d $JAVADOC_OUTPUT_DIR $SRC_FILES
+javadoc -quiet -d $JAVADOC_OUTPUT_DIR $GEN_FILES $SRC_FILES
 
-echo Javdocs generated at $JAVADOC_OUTPUT_DIR
+echo "Javdocs generated at $JAVADOC_OUTPUT_DIR"
 exit 0
