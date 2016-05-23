f476be3debb54ced2bd255925f09372d36700bf7
Luc Perkins <lucperkins@gmail.com>
2016-05-23 10:43:56 -0700

    General website cleanup (#722)
    * re-arrange scripts and move heron logos to /src
    
    * move heron-logos to /img
    
    * add dev-assets command to Makefile
    
    * src -> assets, and change README
    
    * remove python requests install from makefile
    
    * remove warning about linkchecker-out.csv
    
    * add protobuf directories to javadocs script

---
+++ website/scripts/linkchecker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+function die {
+  echo $1 && exit 1
+}
+
+OUT_FILE=linkchecker-out.csv
+ERRORS_FILE=linkchecker-errors.csv
+FILE_TO_CHECK=public/index.html
+
+#linkchecker removes previous runs.
+which linkchecker || die 'Linkchecker must be installed to run this script. Exiting'
+rm -f $OUT_FILE $ERRORS_FILE
+
+set +e
+linkchecker $FILE_TO_CHECK --no-warnings -F csv
+STATUS=$?
+set -e
+
+#uses error code: on fail, write linkchecker-errors.csv for debugging
+if [[ $STATUS != 0 ]]; then
+  cut -sd ';' -f 1,2 $OUT_FILE | tr ';' ' ' | \
+    awk '{ print $2 " " $1}' | sort -u >> $ERRORS_FILE;
+  echo "linkchecker failed - check $ERRORS_FILE";
+  rm -f $OUT_FILE
+  exit $STATUS
+else
+  echo "linkchecker passes";
+fi
