d926dc9ae640c575059496ac5a4c35a21c4665f7
Bill Graham <billg@twitter.com>
2016-05-23 21:11:04 -0700

    adding bazel build and including more package tests

--- website/scripts/javadocs.sh
+++ website/scripts/javadocs.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# TODO: turn this back on once all the javadoc errors are fixed
-#set -e
-set -x
+set -e
 
 HERON_ROOT_DIR=$(git rev-parse --show-toplevel)
 JAVADOC_OUTPUT_DIR=$HERON_ROOT_DIR/website/public/api
 GEN_PROTO_DIR=$HERON_ROOT_DIR/bazel-bin/heron/proto/_javac
+
+(cd $HERON_ROOT_DIR && bazel build \
+  `bazel query 'kind("java_library", "heron/...")'`\
+  `bazel query 'kind("java_test", "heron/...")'` \
+  `bazel query 'kind("java_library", "integration-test/...")'`)
 
 HERON_SRC_FILES=`find $HERON_ROOT_DIR -path "*/com/twitter/*" -name "*.java"`
 BACKTYPE_SRC_FILES=`find $HERON_ROOT_DIR -path "*/backtype/storm/*" -name "*.java"`
@@ -19,7 +22,12 @@ mkdir -p $JAVADOC_OUTPUT_DIR
 EXT_JARS=`find $HERON_ROOT_DIR/bazel-out/host/genfiles/external/. -name "*\.jar" | tr '\n' ':'`
 BIN_JARS=`find $HERON_ROOT_DIR/bazel-heron/_bin/. -name "*\.jar" | tr '\n' ':'`
 GEN_JARS=`find $HERON_ROOT_DIR/bazel-genfiles/external/. -name "*\.jar" | tr '\n' ':'`
-export CLASSPATH=$EXT_JARS:$BIN_JARS:$GEN_JARS
+SCRIBE_JARS=`find $HERON_ROOT_DIR/bazel-bin/. -name "libthrift_scribe_java.jar" | tr '\n' ':'`
+PROTO_JARS=`find $HERON_ROOT_DIR/bazel-bin/heron/proto/. -name "*\.jar" | tr '\n' ':'`
+CLOSURE_CLASSES="$HERON_ROOT_DIR/bazel-bin/heron/storm/src/java/_javac/storm-compatibility-java/libstorm-compatibility-java_classes/."
+
+export CLASSPATH=$EXT_JARS:$BIN_JARS:$GEN_JARS:$SCRIBE_JARS:$PROTO_JARS:$CLOSURE_CLASSES
+
 javadoc -quiet -d $JAVADOC_OUTPUT_DIR $GEN_FILES $HERON_SRC_FILES $BACKTYPE_SRC_FILES $APACHE_SRC_FILES
 
 echo "Javdocs generated at $JAVADOC_OUTPUT_DIR"
