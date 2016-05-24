1dd0072761ab74b2246150ce42bc6aa0bf178c62
Maosong Fu <maosongfu@gmail.com>
2016-05-24 16:22:29 -0700

    Remove unneeded constants (#756)

--- heron/spi/src/java/com/twitter/heron/spi/common/Constants.java
+++ heron/spi/src/java/com/twitter/heron/spi/common/Constants.java
@@ -25,20 +25,6 @@ public final class Constants {
   public static final long GB = 1024L * 1024 * 1024;
   public static final long MB = 1024L * 1024;
 
-  public static final String TUNNEL = "tunnel";
-  public static final String ZKHOST = "zkhost";
-  public static final String ZKPORT = "zkport";
-  public static final String ZKROOT = "zkroot";
-
-  public static final String ZK_CONNECTION_STRING = "zk.connection.string";
-  public static final String ZK_CONNECTION_TIMEOUT_MS = "zk.connection.timeout.ms";
-
-  public static final String LOCALFS_STATE_MANAGER_CLASS =
-      "com.twitter.heron.statemgr.localfs.LocalFileSystemStateManager";
-
-  public static final String ZK_STATE_MANAGER_CLASS =
-      "com.twitter.heron.statemgr.zookeeper.curator.CuratorStateManager";
-
   private Constants() {
   }
 }
