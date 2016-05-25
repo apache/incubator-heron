c7539ca21e19e396fa2c0019392d853cdef275d2
Luc Perkins <lucperkins@gmail.com>
2016-05-24 21:21:14 -0700

    add sudo to make setup script

--- website/scripts/setup.sh
+++ website/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 brew list hugo || brew install hugo
 npm install
-pip uninstall requests
-pip install linkchecker pygments==2.1.3 requests==2.9.0
+sudo pip uninstall requests
+sudo pip install linkchecker pygments==2.1.3 requests==2.9.0
 (cd assets/solarized-pygment && ./setup.py install)
 rm -rf assets/solarized-pygment/{build,dist,pygments_solarized.egg-info}
