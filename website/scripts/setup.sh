726b472da3eea573a5607e9b544b60a4f89481b0
Luc Perkins <lucperkins@gmail.com>
2016-05-24 22:01:31 -0700

    re-add pip uninstall requests

--- website/scripts/setup.sh
+++ website/scripts/setup.sh
@@ -5,6 +5,7 @@ SOLARIZED_PYGMENT_DIR=assets/solarized-pygment
 brew list hugo || brew install hugo
 npm install
 
+pip uninstall requests -y
 pip install linkchecker pygments==2.1.3 requests==2.9.0
 (cd $SOLARIZED_PYGMENT_DIR && chmod +x setup.py && ./setup.py install)
 rm -rf $SOLARIZED_PYGMENT_DIR/{build,dist,pygments_solarized.egg-info}
