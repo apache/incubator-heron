8fde8341a3f8f1df41b109d1558404d0e7b6e78d
Luc Perkins <lperkins@twitter.com>
2016-05-25 17:11:00 -0700

    add overview.html file

--- website/scripts/javadocs.sh
+++ website/scripts/javadocs.sh
@@ -2,12 +2,12 @@
 #set -e TODO: figure out why this breaks CI and re-enable (https://github.com/twitter/heron/issues/766)
 
 JAVADOC=javadoc
-FLAGS="-quiet"
 
 HERON_ROOT_DIR=$(git rev-parse --show-toplevel)
 JAVADOC_OUTPUT_DIR=$HERON_ROOT_DIR/website/public/api
 JAVADOC_STATIC_OUTPUT_DIR=$HERON_ROOT_DIR/website/static
 GEN_PROTO_DIR=$HERON_ROOT_DIR/bazel-bin/heron/proto/_javac
+OVERVIEW_HTML_FILE=$HERON_ROOT_DIR/website/scripts/javadocs-overview.html
 
 (cd $HERON_ROOT_DIR && bazel build \
   `bazel query 'kind("java_library", "heron/...")'`\
@@ -26,11 +26,16 @@ BIN_JARS=`find $HERON_ROOT_DIR/bazel-heron/_bin/. -name "*\.jar" | tr '\n' ':'`
 GEN_JARS=`find $HERON_ROOT_DIR/bazel-genfiles/external/. -name "*\.jar" | tr '\n' ':'`
 SCRIBE_JARS=`find $HERON_ROOT_DIR/bazel-bin/. -name "libthrift_scribe_java.jar" | tr '\n' ':'`
 PROTO_JARS=`find $HERON_ROOT_DIR/bazel-bin/heron/proto/. -name "*\.jar" | tr '\n' ':'`
-CLOSURE_CLASSES="$HERON_ROOT_DIR/bazel-bin/heron/storm/src/java/_javac/storm-compatibility-java/libstorm-compatibility-java_classes/."
+CLOJURE_CLASSES="$HERON_ROOT_DIR/bazel-bin/heron/storm/src/java/_javac/storm-compatibility-java/libstorm-compatibility-java_classes/."
 
-export CLASSPATH=$BIN_JARS:$GEN_JARS:$SCRIBE_JARS:$PROTO_JARS:$CLOSURE_CLASSES
+export CLASSPATH=$BIN_JARS:$GEN_JARS:$SCRIBE_JARS:$PROTO_JARS:$CLOJURE_CLASSES
 
-$JAVADOC $FLAGS -d $JAVADOC_OUTPUT_DIR $GEN_FILES $HERON_SRC_FILES $BACKTYPE_SRC_FILES $APACHE_SRC_FILES
+$JAVADOC -quiet \
+  -windowtitle "Heron Java API" \
+  -doctitle "The Heron Java API" \
+  -overview $OVERVIEW_HTML_FILE \
+  -d $JAVADOC_OUTPUT_DIR $GEN_FILES $HERON_SRC_FILES $BACKTYPE_SRC_FILES $APACHE_SRC_FILES
+
 cp -rf $JAVADOC_OUTPUT_DIR $JAVADOC_STATIC_OUTPUT_DIR/api
 
 echo "Javdocs generated at $JAVADOC_OUTPUT_DIR"
