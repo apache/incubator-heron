b9c0769b2bc1ec840c7eb6106081ce5c902a51e8
Andrey Shevchenko <ashevchenko@cogniance.com>
2016-05-26 18:49:34 +0300

    Reverting Vagrant for heron-kafka as not ready yet for a commit to master

--- vagrant/submit-mesos-scheduler.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST -H "Content-Type: application/json" --data @mesos-scheduler.json http://master:8080/v2/apps
--- vagrant/submit-stack-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST -H "Content-Type: application/json" --data @stack-deploy.json http://master:8080/v2/apps
