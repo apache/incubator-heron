9ef9233f107f301b151f7fe86e4ff07cab539a9b
Bill Graham <billg@twitter.com>
2016-05-31 11:52:01 -0700

    fixes to checkstyle_all.sh

--- checkstyle_all.sh
+++ checkstyle_all.sh
@@ -8,7 +8,12 @@ set -e
 ROOT_DIR=`git rev-parse --show-toplevel`
 JARS=`find $PWD/. -name "*\.jar" | tr '\n' ":"`
 JAVA_FILES=`find $ROOT_DIR/{heron,tools,integration-test}/. -name "*.java"`
+JAVA_FILES_COUNT=`echo $JAVA_FILES | wc -w | xargs`
 CONFIG=$ROOT_DIR/tools/java/src/com/twitter/bazel/checkstyle/coding_style.xml
 
-echo "Checkstyles checking `echo $JAVA_FILES | wc -w` java files"
-(cd $ROOT_DIR java -cp $JARS com.puppycrawl.tools.checkstyle.Main -v -c $CONFIG $JAVA_FILES)
+echo "Checkstyles checking $JAVA_FILES_COUNT java files"
+start_secs=`date +%s`
+(cd $ROOT_DIR && java -cp $JARS com.puppycrawl.tools.checkstyle.Main -c $CONFIG $JAVA_FILES)
+end_secs=`date +%s`
+duration=`expr $end_secs - $start_secs`
+echo "Checkstyles successfully checked $JAVA_FILES_COUNT java files in $duration seconds"
