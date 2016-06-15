a5c4773199f265b65444546137388b569589a4f7
Neng Lu <nlu@twitter.com>
2016-06-15 15:16:39 -0700

    update

--- scripts/packages/package_info_generator.sh
+++ scripts/packages/package_info_generator.sh
@@ -58,7 +58,7 @@ done
 
 echo "heron.build.version : '${release_name}'"
 echo "heron.build.time : ${build_time}"
-echo "heron.build.timestamp : '${build_timestamp}'"
+echo "heron.build.timestamp : ${build_timestamp}"
 echo "heron.build.host : ${build_host}"
 echo "heron.build.user : ${build_user}"
 echo "heron.build.git.revision : ${git_hash}"
