4537dff67128432153c1cb9fb2e059998f26e93d
Taishi Nojima <tnojima@twitter.com>
2016-06-15 12:18:56 -0700

    Integrated build command into run_integration_test.sh so that users don't have to run it mannually.

--- scripts/run_integration_test.sh
+++ scripts/run_integration_test.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 #
 # Script to locally run the functional integration test.
-# This script assumes that integration tests are already built under ./bazel-bin directory
-# ($ bazel build --config=PLATFORM integration-test/src/...)
 #
 
 HTTP_SERVER="./bazel-bin/integration-test/src/python/http_server/http-server"
 TEST_RUNNER="./bazel-bin/integration-test/src/python/test_runner/test-runner.pex"
+INTEGRATION_TESTS="./bazel-genfiles/integration-test/src/java/integration-tests.jar"
+CORE_PKG="file://`pwd`/bazel-bin/scripts/packages/heron-core.tar.gz"
 
-declare -a required_files=(${HTTP_SERVER} ${TEST_RUNNER});
+function usage() {
+  echo "Usage: $0 [platform]" >&2
+  echo "Currently supported platforms are:" >&2
+  echo "  darwin, ubuntu, and centos5" >&2
+  exit 1
+}
 
-for file in ${required_files[@]}; do
-  if [ ! -f ${file} ]; then
-    echo "Required file ${file} does not exist." >&2
-    echo "Make sure that the integration tests are built prior to" >&2
-    echo "running this script using the following command." >&2
-    echo "  bazel build --config={PLATFORM} integration-test/src/..." >&2
-    exit 1;
-  fi
-done
+if [ $# -eq 1 ] && \
+   [[ "$1" == "darwin" || "$1" == "ubuntu" || "$1" == "centos5" ]];
+then
+  PLATFORM="$1"
+else
+  usage
+fi
 
+# building tar packages
+bazel build --config=${PLATFORM} heron/...
+bazel build --config=${PLATFORM} scripts/packages:tarpkgs
+bazel build --config=${PLATFORM} integration-test/src/...
+
+# run the simple http server
 ${HTTP_SERVER} 8080 &
 http_server_id=$!
 trap "kill -9 $http_server_id" SIGINT SIGTERM EXIT
 
+# run the integration tests
 ${TEST_RUNNER} \
-  -hc heron -tj bazel-genfiles/integration-test/src/java/integration-tests.jar \
+  -hc heron -tj ${INTEGRATION_TESTS} \
   -rh localhost -rp 8080\
   -tp integration-test/src/java/com/twitter/heron/integration_test/topology/ \
-  -cl local -rl heron-staging -ev devel -pi 'file://${HERON_DIST}/heron-core.tar.gz'
+  -cl local -rl heron-staging -ev devel -pi ${CORE_PKG}
