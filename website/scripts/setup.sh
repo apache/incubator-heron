266fa7876849e59d9335761ad855e71820ed39fd
Runhang Li <rli@twitter.com>
2016-06-17 00:08:06 -0700

    remove linkchecker.

--- website/scripts/setup.sh
+++ website/scripts/setup.sh
@@ -2,5 +2,5 @@
 
 brew list hugo || brew install hugo
 npm install
-sudo -H pip uninstall -y requests Pygments
-sudo -H pip install linkchecker Pygments==2.1.3 requests==2.9.0
+sudo -H pip uninstall -y Pygments
+sudo -H pip install Pygments==2.1.3
