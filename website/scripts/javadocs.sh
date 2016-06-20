1afe92b2a2f3db6550816b2802d7c747ca52655d
Runhang Li <objmagic@users.noreply.github.com>
2016-06-20 15:31:03 -0700

    Add comments on why Java API doc should be copied. (#956)
    * Add comments on why Java API doc should be copied.

--- website/scripts/javadocs.sh
+++ website/scripts/javadocs.sh
@@ -4,7 +4,9 @@ JAVADOC=javadoc
 FLAGS="-quiet"
 
 HERON_ROOT_DIR=$(git rev-parse --show-toplevel)
+# for display on GitHub website
 JAVADOC_OUTPUT_DIR=$HERON_ROOT_DIR/website/public/api
+# for display on local Hugo server
 JAVADOC_OUTPUT_LOCAL_DIR=$HERON_ROOT_DIR/website/static/api
 GEN_PROTO_DIR=$HERON_ROOT_DIR/bazel-bin/heron/proto/_javac
 
@@ -31,6 +33,14 @@ export CLASSPATH=$BIN_JARS:$GEN_JARS:$SCRIBE_JARS:$PROTO_JARS:$CLOSURE_CLASSES
 
 $JAVADOC $FLAGS -d $JAVADOC_OUTPUT_DIR $GEN_FILES $HERON_SRC_FILES $BACKTYPE_SRC_FILES $APACHE_SRC_FILES
 
+# Generated Java API doc needs to be copied to $JAVADOC_OUTPUT_LOCAL_DIR
+# for the following two reasons:
+# 1. When one is developing website locally using Hugo server, he should
+#    be able to click into API doc link and view API doc to
+#    check if the correct API link is given.
+# 2. ``wget`` needs to verify if links to Java API doc are valid when Hugo is
+#    serving the website locally. This means that Hugo should be able to display
+#    Java API doc properly.
 cp -r $JAVADOC_OUTPUT_DIR $JAVADOC_OUTPUT_LOCAL_DIR
 
 echo "Javdocs generated at $JAVADOC_OUTPUT_DIR"
