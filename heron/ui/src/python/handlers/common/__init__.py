4c742bdf40de4995b38434784ba321a3ef8f9f0b
Alan Ngai <alan@opsclarity.com>
2016-06-21 09:35:13 -0700

    added fixed per @nlu90

--- heron/ui/src/python/handlers/common/__init__.py
+++ heron/ui/src/python/handlers/common/__init__.py
@@ -3,3 +3,4 @@ __all__ = ['common']
 
 from heron.ui.src.python.handlers.common.consts import *
 from heron.ui.src.python.handlers.common.utils import *
+from heron.ui.src.python.handlers.common.graph import *
