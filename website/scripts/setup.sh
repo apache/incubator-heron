98da9ffcc46d12b5acf86fe481b3383b68ce6106
ablecao <ablecao@tencent.com>
2016-06-22 15:31:17 +0800

    #915 modify setup.sh, install proper deps relay on os type

--- website/scripts/setup.sh
+++ website/scripts/setup.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-brew list hugo || brew install hugo
+source ../../scripts/detect_os_type.sh
+
+PLAT_FORM=`platform`
+if [ $PLAT_FORM = darwin ]; then 
+  brew list hugo || brew install hugo
+elif [ $PLAT_FORM = ubuntu ]; then
+  sudo apt-get install golang git mercurial python-pygments -y
+  export GOROOT=/usr/lib/go
+  export GOPATH=$HOME/go
+  export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+  go get -u -v github.com/spf13/hugo
+elif [ $PLAT_FORM = centos ]; then
+  sudo yum -y install nodejs npm golang --enablerepo=epel
+  export GOROOT=/usr/lib/go
+  export GOPATH=$HOME/go
+  export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+  go get -u -v github.com/spf13/hugo
+fi
+
 npm install
 sudo -H pip uninstall -y requests Pygments
 sudo -H pip install linkchecker Pygments==2.1.3 requests==2.9.0
