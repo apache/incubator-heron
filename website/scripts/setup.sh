eefa8e31bef9ffb2fac8e46ecef903c305eee397
ablecao <ablecao@tencent.com>
2016-06-23 09:26:58 +0800

    #915 modify 'website/scripts/setup.sh' using  rather than

--- website/scripts/setup.sh
+++ website/scripts/setup.sh
@@ -2,20 +2,20 @@
 
 source ../scripts/detect_os_type.sh
 
-PLAT_FORM=`platform`
-if [ $PLAT_FORM = darwin ]; then
+PLATFORM=`platform`
+if [ $PLATFORM = darwin ]; then
   brew update && brew install nvm && source $(brew --prefix nvm)/nvm.sh
   nvm install node
   curl -L https://www.npmjs.com/install.sh | sh 
   brew list hugo || brew install hugo
   which wget || brew install wget
-elif [ $PLAT_FORM = ubuntu ]; then
+elif [ $PLATFORM = ubuntu ]; then
   sudo apt-get install golang git mercurial python-pygments -y
   export GOROOT=/usr/lib/go
   export GOPATH=$HOME/go
   export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
   go get -u -v github.com/spf13/hugo
-elif [ $PLAT_FORM = centos ]; then
+elif [ $PLATFORM = centos ]; then
   sudo yum -y install nodejs npm golang --enablerepo=epel
   export GOROOT=/usr/lib/go
   export GOPATH=$HOME/go
