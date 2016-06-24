f9882b325e3f5d257693f087d13263192016c075
Neng Lu <freeneng@gmail.com>
2016-06-24 16:30:52 -0700

    clean spi/common dir (#969)
    * move ShellUtils and NetworkUtils to utils dir

--- heron/packing/src/java/com/twitter/heron/packing/NullPacking.java
+++ heron/packing/src/java/com/twitter/heron/packing/NullPacking.java
@@ -17,8 +17,8 @@ package com.twitter.heron.packing;
 import java.util.HashMap;
 
 import com.twitter.heron.spi.common.Config;
-import com.twitter.heron.spi.common.PackingPlan;
 import com.twitter.heron.spi.packing.IPacking;
+import com.twitter.heron.spi.packing.PackingPlan;
 
 public class NullPacking implements IPacking {
 
