7e003fc7aad8a2b3bc35e90e9d9f37bdb9af05e0
Runhang Li <objmagic@users.noreply.github.com>
2016-07-17 23:23:15 -0700

    Fix Python style under integration-test. (#1100)

--- integration-test/src/python/test_runner/main.py
+++ integration-test/src/python/test_runner/main.py
@@ -1,12 +1,13 @@
+''' main '''
 import argparse
 import json
 import logging
 import os
 import pkgutil
 import sys
-import threading
 import time
 import uuid
+# pylint: disable=unused-wildcard-import
 from httplib import *
 
 # The location of default configure file
@@ -16,30 +17,32 @@ RETRY_ATTEMPTS = 15
 #seconds
 RETRY_INTERVAL = 30
 
-#Runs the test for one topology
 def runTest(topologyName, classPath, expectedResultFilePath, params):
-  httpServerUrl = "http://" + params.resultsServerHostname + ":" + params.resultsServerPort + "/results"
+  ''' Runs the test for one topology '''
+  httpServerUrl = "http://" + params.resultsServerHostname + ":" + \
+    params.resultsServerPort + "/results"
 
   #submit topology
   try:
     args = httpServerUrl + " " + topologyName
-    submitTopology(params.heronCliPath, params.cluster, params.role, params.env, params.testsJarPath, classPath,
-      params.releasePackageUri, args)
+    submitTopology(params.heronCliPath, params.cluster, params.role,
+                   params.env, params.testsJarPath, classPath,
+                   params.releasePackageUri, args)
   except Exception as e:
-    logging.error("Failed to submit %s topology: %s" %(topologyName, str(e)))
+    logging.error("Failed to submit %s topology: %s", topologyName, str(e))
     return "fail"
 
-  logging.info("Successfully submitted %s topology" %(topologyName))
+  logging.info("Successfully submitted %s topology", topologyName)
 
   # Fetch actual results. Sleep and retry if the result is not ready
   try:
-    (httpResponseCode, actualResult) = fetchResultFromServer(
-      params.resultsServerHostname,
-      params.resultsServerPort,
-      topologyName
+    _, actualResult = fetchResultFromServer(
+        params.resultsServerHostname,
+        params.resultsServerPort,
+        topologyName
     )
   except Exception as e:
-    logging.error("Fetching result failed for %s topology: %s" %(topologyName, str(e)))
+    logging.error("Fetching result failed for %s topology: %s", topologyName, str(e))
     return "fail"
   finally:
     killTopology(params.heronCliPath, params.cluster, params.role, params.env, topologyName)
@@ -52,7 +55,7 @@ def runTest(topologyName, classPath, expectedResultFilePath, params):
       with open(expectedResultFilePath, "r") as expectedResultFile:
         expectedResult = expectedResultFile.read().rstrip()
   except Exception as e:
-    logging.error("Failed to read expected result file %s: %s" %(expectedResultFilePath, str(e)))
+    logging.error("Failed to read expected result file %s: %s", expectedResultFilePath, str(e))
     return "fail"
 
   # Build a new instance of json decoder since the default one could not ignore "\n"
@@ -67,7 +70,7 @@ def runTest(topologyName, classPath, expectedResultFilePath, params):
 
   # Compare the actual and expected result
   if actualResult == expectedResult:
-    logging.info("Topology %s result matches expected result" % topologyName)
+    logging.info("Topology %s result matches expected result", topologyName)
     return "success"
   else:
     logging.error("Actual result did not match expected result")
@@ -75,23 +78,25 @@ def runTest(topologyName, classPath, expectedResultFilePath, params):
     logging.info("Expected result ---------- \n" + expectedResult)
     return "fail"
 
-# Make a http get request to fetch actual results from http server
 def fetchResultFromServer(serverAddress, serverPort, topologyName):
+  ''' Make a http get request to fetch actual results from http server '''
   for i in range(0, RETRY_ATTEMPTS):
-    logging.info("Fetching results for topology %s, retry count: %d" % (topologyName, i))
+    logging.info("Fetching results for topology %s, retry count: %d", topologyName, i)
     response = getHTTPResponse(serverAddress, int(serverPort), topologyName)
     if response.status == 200:
       return (response.status, response.read())
     elif i != RETRY_ATTEMPTS:
-      logging.info("Fetching results failed with status: %s; reason: %s; body: %s"
-        % (response.status, response.reason, response.read()))
+      logging.info("Fetching results failed with status: %s; reason: %s; body: %s",
+                   response.status, response.reason, response.read())
       time.sleep(RETRY_INTERVAL)
 
-  logging.error("Failed to fetch results after %d attempts" % RETRY_ATTEMPTS)
+  logging.error("Failed to fetch results after %d attempts", RETRY_ATTEMPTS)
   raise RuntimeError("exceeded fetching result retry attempts")
 
 def getHTTPResponse(serverAddress, serverPort, topologyName):
-  for i in range (0, RETRY_ATTEMPTS):
+  ''' get HTTP response '''
+  # pylint: disable=unused-variable
+  for i in range(0, RETRY_ATTEMPTS):
     try:
       connection = HTTPConnection(serverAddress, int(serverPort))
       connection.request('GET', '/results/' + topologyName)
@@ -101,64 +106,64 @@ def getHTTPResponse(serverAddress, serverPort, topologyName):
       time.sleep(RETRY_INTERVAL)
       continue
 
-  logging.error("Failed to get HTTP Response after %d attempts" % RETRY_ATTEMPTS)
+  logging.error("Failed to get HTTP Response after %d attempts", RETRY_ATTEMPTS)
   raise RuntimeError("Failed to get HTTP response")
 
-# Submit topology using heron-cli
-def submitTopology(heronCliPath, cluster, role, env, jarPath, classPath, pkgUri, args = None):
+def submitTopology(heronCliPath, cluster, role, env, jarPath, classPath, pkgUri, args=None):
+  ''' Submit topology using heron-cli '''
   logging.info("Submitting topology")
 
   # Form the command to submit a topology.
   # Note the single quote around the arg for heron.package.core.uri.
   # This is needed to prevent shell expansion.
   cmd = "%s submit %s/%s/%s %s %s %s --verbose" % (
-    heronCliPath, cluster, role, env, jarPath, classPath, args)
+      heronCliPath, cluster, role, env, jarPath, classPath, args)
 
   if pkgUri is not None:
     cmd = "%s --config-property heron.package.core.uri='%s'" %(cmd, pkgUri)
 
-  logging.info("Submitting command: %s" % (cmd))
+  logging.info("Submitting command: %s", cmd)
 
-  for i in range(0, RETRY_ATTEMPTS):
-    if (os.system(cmd) == 0):
+  for _ in range(0, RETRY_ATTEMPTS):
+    if os.system(cmd) == 0:
       logging.info("Successfully submitted topology")
       return
 
   raise RuntimeError("Unable to submit the topology")
 
-# Kill a topology using heron-cli
 def killTopology(heronCliPath, cluster, role, env, topologyName):
+  ''' Kill a topology using heron-cli '''
   logging.info("Killing topology")
   cmd = "%s kill %s/%s/%s %s --verbose" % (
-    heronCliPath, cluster, role, env, topologyName)
+      heronCliPath, cluster, role, env, topologyName)
 
-  logging.info("Submitting command: %s" % (cmd))
+  logging.info("Submitting command: %s", cmd)
   for i in range(0, RETRY_ATTEMPTS):
-    if (os.system(cmd) != 0):
+    if os.system(cmd) != 0:
       time.sleep(RETRY_INTERVAL)
-      logging.warning("killing topology %s with %d attempts" % (topologyName, i))
+      logging.warning("killing topology %s with %d attempts", topologyName, i)
     else:
-       logging.info("Successfully killed topology %s" % topologyName)
-       return
+      logging.info("Successfully killed topology %s", topologyName)
+      return
 
-  logging.error("Failed to kill topology %s" % topologyName)
+  logging.error("Failed to kill topology %s", topologyName)
   raise RuntimeError("Unable to kill the topology")
 
-# Run the test for each topology specified in the conf file
 def runAllTests(conf, args):
+  ''' Run the test for each topology specified in the conf file '''
   successes = []
   failures = []
   timestamp = str(int(time.time()))
   total = len(conf["topologies"])
-  current = 1;
+  current = 1
   for topologyConf in conf["topologies"]:
     topologyName = ("%s_%s_%s") % (timestamp, topologyConf["topologyName"], str(uuid.uuid4()))
     classPath = conf["topologyClasspathPrefix"] + topologyConf["classPath"]
     expectedResultFilePath = args.topologiesPath + "/" + topologyConf["expectedResultRelativePath"]
 
-    logging.info("==== Starting test %s of %s: %s ====" % (current, total, topologyName))
+    logging.info("==== Starting test %s of %s: %s ====", current, total, topologyName)
     start_secs = int(time.time())
-    if (runTest(topologyName, classPath, expectedResultFilePath, args) == "success"):
+    if runTest(topologyName, classPath, expectedResultFilePath, args) == "success":
       successes += [(topologyName, int(time.time()) - start_secs)]
     else:
       failures += [(topologyName, int(time.time()) - start_secs)]
@@ -166,9 +171,10 @@ def runAllTests(conf, args):
   return (successes, failures)
 
 def main():
+  ''' main '''
   root = logging.getLogger()
   root.setLevel(logging.DEBUG)
-  print (os.path.join(os.path.dirname(sys.modules[__name__].__file__), DEFAULT_TEST_CONF_FILE), 'rb')
+  print(os.path.join(os.path.dirname(sys.modules[__name__].__file__), DEFAULT_TEST_CONF_FILE), 'rb')
   conf_file = DEFAULT_TEST_CONF_FILE
   # Read the configuration file from package
   confString = pkgutil.get_data(__name__, conf_file)
@@ -185,11 +191,11 @@ def main():
   parser.add_argument('-ev', '--env', dest='env', default=conf['env'])
   parser.add_argument('-rl', '--role', dest='role', default=conf['role'])
   parser.add_argument('-rh', '--results-server-hostname', dest='resultsServerHostname')
-  parser.add_argument('-rp', '--results-server-port', dest='resultsServerPort', default=conf['resultsServerPort'])
+  parser.add_argument('-rp', '--results-server-port', dest='resultsServerPort',
+                      default=conf['resultsServerPort'])
   parser.add_argument('-tp', '--topologies-path', dest='topologiesPath')
   parser.add_argument('-pi', '--release-package-uri', dest='releasePackageUri', default=None)
 
-  #TODO: Enable this option
   #parser.add_argument('-dt', '--disable-topologies', dest='disabledTopologies', default='',
   #                    help='comma separated test case(classpath) name that will not be run')
   #parser.add_argument('-et', '--enable-topologies', dest='enableTopologies', default=None,
@@ -201,14 +207,14 @@ def main():
   total = len(failures) + len(successes)
 
   if not failures:
-    logging.info("SUCCESS: %s (all) tests passed:" % len(successes))
+    logging.info("SUCCESS: %s (all) tests passed:", len(successes))
     for test in successes:
-      logging.info("  - %s: %s" % (("[%ss]" % test[1]).ljust(8), test[0]))
+      logging.info("  - %s: %s", ("[%ss]" % test[1]).ljust(8), test[0])
     sys.exit(0)
   else:
-    logging.error("FAILURE: %s/%s tests failed:" % (len(failures), total))
+    logging.error("FAILURE: %s/%s tests failed:", len(failures), total)
     for test in failures:
-      logging.error("  - %s: %s" % (("[%ss]" % test[1]).ljust(8), test[0]))
+      logging.error("  - %s: %s", ("[%ss]" % test[1]).ljust(8), test[0])
     sys.exit(1)
 
 if __name__ == '__main__':
