1d7499526c70d05ff0ed7d9c618030f55227e796
ablecao <ablecao@tencent.com>
2016-07-20 15:13:40 +0800

    #1028 add WordCount.java

--- heron/examples/src/java/com/twitter/heron/examples/WordCountShellTopology.java
+++ heron/examples/src/java/com/twitter/heron/examples/WordCountShellTopology.java
@@ -14,55 +14,17 @@
 
 package com.twitter.heron.examples;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
-import org.apache.storm.topology.BasicOutputCollector;
-import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
-import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Fields;
-import org.apache.storm.tuple.Tuple;
-import org.apache.storm.tuple.Values;
 
 /**
  * This topology demonstrates Storm's stream groupings and multilang
  * capabilities.
  */
 public class WordCountShellTopology {
-
-  public static class WordCount extends BaseBasicBolt {
-    private static final long serialVersionUID = 1L;
-    private Map<String, Integer> counts = new HashMap<String, Integer>();
-
-    public Map<String, Integer> getCounts() {
-      return counts;
-    }
-
-    public void setCounts(Map<String, Integer> counts) {
-      this.counts = counts;
-    }
-
-    @Override
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
-      String word = tuple.getString(0);
-      Integer count = counts.get(word);
-      if (count == null) {
-        count = 0;
-      }
-      count++;
-      counts.put(word, count);
-      collector.emit(new Values(word, count));
-    }
-
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-      declarer.declare(new Fields("word", "count"));
-    }
-  }
 
   public static void main(String[] args) throws Exception {
 
