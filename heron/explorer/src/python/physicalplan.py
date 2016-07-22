383f3027cd3277cb14e2d3914b732bc609735803
Runhang Li <rli@twitter.com>
2016-07-21 21:16:55 -0700

    Improve error message.

--- heron/explorer/src/python/physicalplan.py
+++ heron/explorer/src/python/physicalplan.py
@@ -107,10 +107,11 @@ def run_metrics(command, parser, cl_args, unknown_args):
   for comp in components:
     try:
       metrics = utils.get_component_metrics(comp, cluster, env, topology, role)
-      stat, header = to_table(metrics)
-      cresult.append((comp, stat, header))
     except:
+      Log.error("Fail to connect to tracker: \'%s\'", cl_args["tracker_url"])
       return False
+    stat, header = to_table(metrics)
+    cresult.append((comp, stat, header))
   for i, (comp, stat, header) in enumerate(cresult):
     if i != 0:
       print ''
