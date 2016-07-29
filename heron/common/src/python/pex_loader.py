2d70c5e6eb6e94199954bb3780c0af3011160b8f
Taishi Nojima <tnojima@twitter.com>
2016-07-29 14:51:46 -0700

    Added checks to sys.path for pex_loader to avoid duplicate insertion

--- heron/common/src/python/pex_loader.py
+++ heron/common/src/python/pex_loader.py
@@ -37,13 +37,16 @@ def load_pex(path_to_pex, include_deps=True):
   """Loads pex file and its dependencies to the current python path"""
   abs_path_to_pex = os.path.abspath(path_to_pex)
   Log.debug("Add a pex to the path: " + abs_path_to_pex)
-  sys.path.insert(0, abs_path_to_pex)
+  if abs_path_to_pex not in sys.path:
+    sys.path.insert(0, abs_path_to_pex)
 
   # add dependencies to path
   if include_deps:
     for dep in _get_deps_list(abs_path_to_pex):
-      Log.debug("Add a new dependency to the path: " + dep)
-      sys.path.insert(0, os.path.join(abs_path_to_pex, dep))
+      to_join = os.path.join(abs_path_to_pex, dep)
+      if to_join not in sys.path:
+        Log.debug("Add a new dependency to the path: " + dep)
+        sys.path.insert(0, to_join)
 
   Log.debug("Python path: " + str(sys.path))
 
