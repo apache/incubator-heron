a798a9fbd7712558c7d1430929d79d9e04c8a431
Taishi Nojima <tnojima@twitter.com>
2016-08-02 13:53:38 -0700

    Refactored to follow the convention

--- heron/spi/src/java/com/twitter/heron/spi/common/Keys.java
+++ heron/spi/src/java/com/twitter/heron/spi/common/Keys.java
@@ -165,8 +165,8 @@ public class Keys {
     return ConfigKeys.get("TOPOLOGY_DEFINITION_FILE");
   }
 
-  public static String topologyJarFile() {
-    return ConfigKeys.get("TOPOLOGY_JAR_FILE");
+  public static String topologyBinaryFile() {
+    return ConfigKeys.get("TOPOLOGY_BINARY_FILE");
   }
 
   public static String topologyPackageFile() {
@@ -357,8 +357,8 @@ public class Keys {
     return ConfigKeys.get("SANDBOX_SHELL_BINARY");
   }
 
-  public static String pyheronInstanceSandboxBinary() {
-    return ConfigKeys.get("SANDBOX_PYHERON_INSTANCE_BINARY");
+  public static String pythonInstanceSandboxBinary() {
+    return ConfigKeys.get("SANDBOX_PYTHON_INSTANCE_BINARY");
   }
 
   public static String schedulerSandboxJar() {
--- heron/spi/tests/java/com/twitter/heron/spi/utils/SchedulerUtilsTest.java
+++ heron/spi/tests/java/com/twitter/heron/spi/utils/SchedulerUtilsTest.java
@@ -164,7 +164,7 @@ public class SchedulerUtilsTest {
     String[] expectedArgs =
         {"--cluster", null, "--role", null,
             "--environment", null, "--topology_name", null,
-            "--topology_jar", null, "--http_port", "1"};
+            "--topology_bin", null, "--http_port", "1"};
     Assert.assertArrayEquals(expectedArgs, SchedulerUtils.schedulerCommandArgs(
         Mockito.mock(Config.class), Mockito.mock(Config.class), freePorts));
   }
