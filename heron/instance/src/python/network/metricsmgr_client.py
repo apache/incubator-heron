fe681bcabfa5919174a8a45cdb65e75832528a09
TAISHI <taishi.nojima@yale.edu>
2016-08-04 01:30:22 -0700

    Added unit tests

--- heron/instance/src/python/network/metricsmgr_client.py
+++ heron/instance/src/python/network/metricsmgr_client.py
@@ -14,7 +14,7 @@
 '''metrics manager client'''
 import socket
 
-from heron.common.src.python.log import Log
+from heron.common.src.python.utils.log import Log
 from heron.common.src.python.network import HeronClient, StatusCode
 from heron.proto import metrics_pb2, common_pb2
 
@@ -84,8 +84,9 @@ class MetricsManagerClient(HeronClient):
 
     Log.debug("Sending MetricsCli register request: \n" + str(request))
 
+    timeout_sec = float(self.sys_config[constants.INSTANCE_RECONNECT_METRICSMGR_INTERVAL_SEC])
     self.send_request(request, "MetricsClientContext",
-                      metrics_pb2.MetricPublisherRegisterResponse(), 10)
+                      metrics_pb2.MetricPublisherRegisterResponse(), timeout_sec)
 
   # pylint: disable=no-self-use
   def _handle_register_response(self, response):
