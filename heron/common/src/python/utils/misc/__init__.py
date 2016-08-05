4a59ae8fa81f767ffc3ef1f38507db7500d77136
Taishi Nojima <taishi.nojima@yale.edu>
2016-08-05 13:11:07 -0700

    Update Python's common/util modules (#1185)
    * Add TaskHook support for python topology
    * Add system config to outgoing_tuple_helper
    * Add default value for instance set data tuple size in bytes
    * Add unit tests for task_hook
    
    * Added custom grouping support
    
    * Added unit test
    
    * Fixed unittests
    
    * Added backpressure, message timeout
    Added cleanup() for heron client when error
    
    * Fixed style

--- heron/common/src/python/utils/misc/__init__.py
+++ heron/common/src/python/utils/misc/__init__.py
@@ -1,7 +1,9 @@
 '''common module for miscellaneous classes'''
-__all__ = ['pplan_helper', 'serializer', 'communicator', 'outgoing_tuple_helper']
+__all__ = ['pplan_helper', 'serializer', 'communicator',
+           'outgoing_tuple_helper', 'custom_grouping_helper']
 
 from .pplan_helper import PhysicalPlanHelper
-from .serializer import PythonSerializer, HeronSerializer
+from .serializer import PythonSerializer, HeronSerializer, default_serializer
 from .communicator import HeronCommunicator
 from .outgoing_tuple_helper import OutgoingTupleHelper
+from .custom_grouping_helper import CustomGroupingHelper, Target
