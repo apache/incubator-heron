75c5c76e4b44df1570e95b3aab66d44b76b17b6d
Taishi Nojima <taishi.nojima@yale.edu>
2016-08-12 00:45:29 -0700

    Add python integration test topologies (#1248)
    * Python integration test topologies

---
+++ integration-test/src/python/integration_test/topology/all_grouping/__init__.py
@@ -0,0 +1,4 @@
+"""All grouping integration test topology"""
+__all__ = ['all_grouping']
+
+from .all_grouping import all_grouping_buidler
---
+++ integration-test/src/python/integration_test/topology/multi_spouts_multi_tasks/__init__.py
@@ -0,0 +1,4 @@
+"""multi spouts multi tasks integration test topology"""
+__all__ = ['multi_spouts_multi_tasks']
+
+from .multi_spouts_multi_tasks import multi_spouts_multi_tasks_builder
