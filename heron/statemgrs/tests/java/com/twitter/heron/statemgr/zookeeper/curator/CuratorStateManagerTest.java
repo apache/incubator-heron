8ef30d0a6de08dadbe56b78c1f34d10b4e7645f5
Bill Graham <billg@twitter.com>
2016-08-26 08:15:26 -0700

    Fix broken CuratorStateManagerTest

--- heron/statemgrs/tests/java/com/twitter/heron/statemgr/zookeeper/curator/CuratorStateManagerTest.java
+++ heron/statemgrs/tests/java/com/twitter/heron/statemgr/zookeeper/curator/CuratorStateManagerTest.java
@@ -107,7 +107,7 @@ public class CuratorStateManagerTest {
     // Verify curator client is invoked
     Mockito.verify(mockClient).start();
     Mockito.verify(mockClient).blockUntilConnected(Mockito.anyInt(), Mockito.any(TimeUnit.class));
-    Mockito.verify(mockClient, Mockito.times(5)).createContainers(Mockito.anyString());
+    Mockito.verify(mockClient, Mockito.times(6)).createContainers(Mockito.anyString());
 
     // Verify initTree is called
     Mockito.verify(spyStateManager).initTree();
