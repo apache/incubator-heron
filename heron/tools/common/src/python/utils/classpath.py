a86c9d7aa30484cb3621c992f1ef2eef25ad0151
Karthik Ramasamy <kramasamy@twitter.com>
2016-08-27 10:36:32 -0700

    include checking of directory for classpath

--- heron/tools/common/src/python/utils/classpath.py
+++ heron/tools/common/src/python/utils/classpath.py
@@ -21,17 +21,23 @@ def valid_path(path):
   '''
   Check if an entry in the class path exists as either a directory or a file
   '''
-  # check if the class path directory exists
+  # check if the suffic of classpath suffix exists as directory
   if path.endswith('*'):
-    Log.debug('Checking class path directory: %s', path[-1])
+    Log.debug('Checking classpath entry suffix as directory: %s', path[-1])
     if os.path.isdir(path[-1]):
       return True
     return False
 
-  # check if the class path file exists
-  Log.debug('Checking class path file: %s', path)
-  if os.path.isfile(path):
+  # check if the classpath entry is a directory
+  Log.debug('Checking classpath entry as directory: %s', path)
+  if os.path.isdir(path):
     return True
+  else:
+    # check if the classpath entry is a file
+    Log.debug('Checking classpath entry as file: %s', path)
+    if os.path.isfile(path):
+      return True
+
   return False
 
 
