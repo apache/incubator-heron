ce786df60a110a2c14f3ffff7d427af69a10ec90
Bill Graham <billg@twitter.com>
2016-09-08 14:54:06 -0700

    AuroraSchedler implements IScalable

--- heron/schedulers/src/java/com/twitter/heron/scheduler/aurora/AuroraScheduler.java
+++ heron/schedulers/src/java/com/twitter/heron/scheduler/aurora/AuroraScheduler.java
@@ -20,35 +20,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.xml.bind.DatatypeConverter;
 
+import com.google.common.base.Optional;
+
 import com.twitter.heron.api.generated.TopologyAPI;
 import com.twitter.heron.common.basics.FileUtils;
 import com.twitter.heron.proto.scheduler.Scheduler;
+import com.twitter.heron.scheduler.UpdateTopologyManager;
 import com.twitter.heron.spi.common.Config;
 import com.twitter.heron.spi.common.Context;
 import com.twitter.heron.spi.common.Misc;
 import com.twitter.heron.spi.packing.PackingPlan;
 import com.twitter.heron.spi.packing.Resource;
+import com.twitter.heron.spi.scheduler.IScalable;
 import com.twitter.heron.spi.scheduler.IScheduler;
 import com.twitter.heron.spi.utils.Runtime;
 import com.twitter.heron.spi.utils.SchedulerUtils;
 import com.twitter.heron.spi.utils.TopologyUtils;
 
-public class AuroraScheduler implements IScheduler {
+public class AuroraScheduler implements IScheduler, IScalable {
   private static final Logger LOG = Logger.getLogger(AuroraLauncher.class.getName());
 
   private Config config;
   private Config runtime;
   private AuroraController controller;
+  private UpdateTopologyManager updateTopologyManager;
 
   @Override
   public void initialize(Config mConfig, Config mRuntime) {
     this.config = mConfig;
     this.runtime = mRuntime;
     this.controller = getController();
+    this.updateTopologyManager =
+        new UpdateTopologyManager(runtime, Optional.<IScalable>of(this));
   }
 
   /**
@@ -111,8 +121,24 @@ public class AuroraScheduler implements IScheduler {
 
   @Override
   public boolean onUpdate(Scheduler.UpdateTopologyRequest request) {
-    LOG.severe("Topology onUpdate not implemented by this scheduler.");
-    return false;
+    try {
+      updateTopologyManager.updateTopology(
+          request.getCurrentPackingPlan(), request.getProposedPackingPlan());
+    } catch (ExecutionException | InterruptedException e) {
+      LOG.log(Level.SEVERE, "Could not update topology for request: " + request, e);
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public void addContainers(Set<PackingPlan.ContainerPlan> containersToAdd) {
+    controller.addContainers(containersToAdd.size());
+  }
+
+  @Override
+  public void removeContainers(Set<PackingPlan.ContainerPlan> containersToRemove) {
+    controller.removeContainers(containersToRemove);
   }
 
   /**
