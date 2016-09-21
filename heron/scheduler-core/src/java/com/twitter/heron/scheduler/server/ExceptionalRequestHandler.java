4bfb676e8cb16dfe53139c4ba1fa485de263e966
Bill Graham <billg@twitter.com>
2016-09-21 10:23:27 -0700

    supress IllegalCatch warning

--- heron/scheduler-core/src/java/com/twitter/heron/scheduler/server/ExceptionalRequestHandler.java
+++ heron/scheduler-core/src/java/com/twitter/heron/scheduler/server/ExceptionalRequestHandler.java
@@ -1,16 +1,16 @@
-//  Copyright 2016 Twitter. All rights reserved.
+// Copyright 2016 Twitter. All rights reserved.
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  http://www.apache.org/licenses/LICENSE-2.0
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.twitter.heron.scheduler.server;
 
 import java.io.IOException;
@@ -45,6 +45,7 @@ class ExceptionalRequestHandler implements HttpHandler {
     this.scheduler = scheduler;
   }
 
+  @SuppressWarnings("IllegalCatch")
   @Override
   public void handle(HttpExchange exchange) throws IOException {
 
@@ -61,13 +62,12 @@ class ExceptionalRequestHandler implements HttpHandler {
       } finally {
         Runtime.schedulerShutdown(runtime).terminate();
       }
-    }
-    catch (Throwable e) {
+    } catch (Exception e) {
       handleFailure(exchange, e);
     }
   }
 
-  private static void handleFailure(HttpExchange exchange, Throwable e) {
+  private static void handleFailure(HttpExchange exchange, Exception e) {
     LOG.log(Level.SEVERE,
         String.format("Failed to handle %s request", exchange.getRequestURI()), e);
     sendResponse(exchange, false);
