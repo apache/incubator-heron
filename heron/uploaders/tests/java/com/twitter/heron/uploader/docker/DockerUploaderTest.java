10c419776d1cd3344dff7c45a2caef8b2d196f81
Chris Pardy <cpardy@fitbit.com>
2016-09-23 16:59:58 -0400

    Fix Checkstyle in tests

--- heron/uploaders/tests/java/com/twitter/heron/uploader/docker/DockerUploaderTest.java
+++ heron/uploaders/tests/java/com/twitter/heron/uploader/docker/DockerUploaderTest.java
@@ -1,6 +1,20 @@
+// Copyright 2016 Twitter. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.twitter.heron.uploader.docker;
 
-import com.twitter.heron.spi.common.Config;
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +23,7 @@ import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.File;
+import com.twitter.heron.spi.common.Config;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
