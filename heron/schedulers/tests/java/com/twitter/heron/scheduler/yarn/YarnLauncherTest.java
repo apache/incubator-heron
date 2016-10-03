aeeba629a9694c9e0681cb0f0c60b2aa10f7b910
Ashvin Agrawal <ashvin@apache.org>
2016-10-03 14:18:37 -0700

    Refactor test for readability

--- heron/schedulers/tests/java/com/twitter/heron/scheduler/yarn/YarnLauncherTest.java
+++ heron/schedulers/tests/java/com/twitter/heron/scheduler/yarn/YarnLauncherTest.java
@@ -16,11 +16,17 @@ package com.twitter.heron.scheduler.yarn;
 
 import java.io.File;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
+import org.apache.reef.driver.parameters.DriverIdentifier;
 import org.apache.reef.driver.parameters.DriverMemory;
+import org.apache.reef.runtime.common.driver.DriverRuntimeConfigurationOptions.JobControlHandler;
+import org.apache.reef.runtime.common.driver.api.RuntimeParameters;
+import org.apache.reef.runtime.common.driver.client.ClientManager;
+import org.apache.reef.runtime.common.driver.resourcemanager.NodeDescriptorHandler;
+import org.apache.reef.runtime.common.driver.resourcemanager.ResourceAllocationHandler;
+import org.apache.reef.runtime.common.driver.resourcemanager.ResourceManagerStatus;
+import org.apache.reef.runtime.common.driver.resourcemanager.ResourceStatusHandler;
 import org.apache.reef.runtime.yarn.client.parameters.JobQueue;
 import org.apache.reef.tang.Configuration;
 import org.apache.reef.tang.types.NamedParameterNode;
@@ -31,71 +37,89 @@ import org.mockito.Mockito;
 import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.Cluster;
 import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.Environ;
 import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.HeronCorePackageName;
+import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.HttpPort;
 import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.Role;
 import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.TopologyJar;
 import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.TopologyName;
 import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.TopologyPackageName;
+import com.twitter.heron.scheduler.yarn.HeronConfigurationOptions.VerboseLogMode;
 import com.twitter.heron.spi.common.Config;
 import com.twitter.heron.spi.common.Keys;
 
 public class YarnLauncherTest {
-  private Map<String, String> testConfigMap = new HashMap<>();
-  private Map<String, String> expectedMap = new HashMap<>();
-
+  // This test verifies if launcher correctly provides all heron specific configurations are to reef
+  // framework. It does so by ensuring required configs exist. The test fails if an unknown config
+  // is found. Presence of unknown config should be verified and then added to the test below
   @Test
   public void getHMDriverConfConstructsReefConfig() throws Exception {
     YarnLauncher launcher = new YarnLauncher();
     YarnLauncher spyLauncher = Mockito.spy(launcher);
     Mockito.doNothing().when(spyLauncher).addLibraryToClasspathSet(Mockito.anyString());
 
-    testConfigMap.clear();
-    expectedMap.clear();
-    setConfigs(Keys.topologyName(), "topology", TopologyName.class);
-    setConfigs(Keys.topologyBinaryFile(), "binary", TopologyJar.class);
-    setConfigs(Keys.topologyPackageFile(), "package", TopologyPackageName.class);
-    setConfigs(Keys.cluster(), "cluster", Cluster.class);
-    setConfigs(Keys.role(), "role", Role.class);
-    setConfigs(Keys.environ(), "env", Environ.class);
-    setConfigs(YarnContext.HERON_SCHEDULER_YARN_QUEUE, "queue", JobQueue.class);
-    setConfigs(YarnContext.YARN_SCHEDULER_DRIVER_MEMORY_MB, "123", DriverMemory.class);
-    setConfigs(Keys.corePackageUri(), new File(".").getName(), HeronCorePackageName.class);
+    // inputConf contains configs provided to the launcher
+    Map<String, String> inputConf = new HashMap<>();
+    // expected contains the reef configs launcher is expected to construct using testConfigs
+    Map<String, String> expected = new HashMap<>();
 
-    Set<String> reefSpecificConfigs = new HashSet<>();
-    reefSpecificConfigs.add("JobControlHandler");
-    reefSpecificConfigs.add("NodeDescriptorHandler");
-    reefSpecificConfigs.add("ResourceAllocationHandler");
-    reefSpecificConfigs.add("ResourceStatusHandler");
-    reefSpecificConfigs.add("RuntimeStatusHandler");
-    reefSpecificConfigs.add("VerboseLogMode");
-    reefSpecificConfigs.add("HttpPort");
-    reefSpecificConfigs.add("DriverIdentifier");
+    setConfigs(inputConf, expected, Keys.topologyName(), "topology", TopologyName.class);
+    setConfigs(inputConf, expected, Keys.topologyBinaryFile(), "binary", TopologyJar.class);
+    setConfigs(inputConf, expected, Keys.topologyPackageFile(), "pack", TopologyPackageName.class);
+    setConfigs(inputConf, expected, Keys.cluster(), "cluster", Cluster.class);
+    setConfigs(inputConf, expected, Keys.role(), "role", Role.class);
+    setConfigs(inputConf, expected, Keys.environ(), "env", Environ.class);
+    setConfigs(inputConf, expected, YarnContext.HERON_SCHEDULER_YARN_QUEUE, "q", JobQueue.class);
+    setConfigs(inputConf, expected, YarnContext.YARN_SCHEDULER_DRIVER_MEMORY_MB,
+        "123", DriverMemory.class);
+    setConfigs(inputConf, expected, Keys.corePackageUri(),
+        new File(".").getName(), HeronCorePackageName.class);
+
+    // the following expected values are mostly specific to reef runtime
+    expected.put(JobControlHandler.class.getSimpleName(), ClientManager.class.getName());
+    expected.put(NodeDescriptorHandler.class.getSimpleName(),
+        NodeDescriptorHandler.class.getName());
+    expected.put(ResourceAllocationHandler.class.getSimpleName(),
+        ResourceAllocationHandler.class.getName());
+    expected.put(ResourceStatusHandler.class.getSimpleName(),
+        ResourceStatusHandler.class.getName());
+    expected.put(RuntimeParameters.RuntimeStatusHandler.class.getSimpleName(),
+        ResourceManagerStatus.class.getName());
+    expected.put(VerboseLogMode.class.getSimpleName(), "false");
+    expected.put(HttpPort.class.getSimpleName(), "0");
+    expected.put(DriverIdentifier.class.getSimpleName(), "topology");
 
     Config.Builder builder = new Config.Builder();
-    for (String s : testConfigMap.keySet()) {
-      builder.put(s, testConfigMap.get(s));
+    for (String configName : inputConf.keySet()) {
+      builder.put(configName, inputConf.get(configName));
     }
     builder.put(Keys.stateManagerClass(), "statemanager");
     builder.put(Keys.packingClass(), "packing");
     Config config = builder.build();
 
     spyLauncher.initialize(config, null);
-    Configuration constructedConfig = spyLauncher.getHMDriverConf();
+    Configuration resultConf = spyLauncher.getHMDriverConf();
 
-    for (NamedParameterNode<?> reefConfigNode : constructedConfig.getNamedParameters()) {
-      String value = expectedMap.get(reefConfigNode.getName());
-      if (value != null) {
-        Assert.assertEquals(value, constructedConfig.getNamedParameter(reefConfigNode));
-        expectedMap.remove(reefConfigNode.getName());
+    for (NamedParameterNode<?> reefConfigNode : resultConf.getNamedParameters()) {
+      String resultValue = resultConf.getNamedParameter(reefConfigNode);
+      String expectedValue = expected.get(reefConfigNode.getName());
+      if (expectedValue == null) {
+        Assert.fail(String.format("Unknown config, %s:%s, provided to heron driver",
+            reefConfigNode.getName(), resultConf.getNamedParameter(reefConfigNode)));
       } else {
-        reefSpecificConfigs.remove(reefConfigNode.getName());
+        Assert.assertEquals(expectedValue, resultValue);
       }
+      expected.remove(reefConfigNode.getName());
     }
-    Assert.assertEquals(0, expectedMap.size());
-    Assert.assertEquals(0, reefSpecificConfigs.size());
+
+    Assert.assertEquals(String.format("Missing expected configurations: %s", expected),
+        0, expected.size());
   }
 
-  private void setConfigs(String heronConfigKey, String value, Class<?> reefConfigKey) {
-    testConfigMap.put(heronConfigKey, value);
-    expectedMap.put(reefConfigKey.getSimpleName(), value);
+  private void setConfigs(Map<String, String> inputConf,
+                          Map<String, String> outputConf,
+                          String heronConfigKey,
+                          String value,
+                          Class<?> reefConfigKey) {
+    inputConf.put(heronConfigKey, value);
+    outputConf.put(reefConfigKey.getSimpleName(), value);
   }
 }
