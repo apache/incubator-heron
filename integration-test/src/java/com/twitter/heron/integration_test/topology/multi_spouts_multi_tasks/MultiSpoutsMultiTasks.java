7a7946ed920636eeef116e2ca66fd09e20104f87
Bill Graham <billg@twitter.com>
2016-10-17 14:58:47 -0700

    Refactor integration tests to share more code (#1494)

--- integration-test/src/java/com/twitter/heron/integration_test/topology/multi_spouts_multi_tasks/MultiSpoutsMultiTasks.java
+++ integration-test/src/java/com/twitter/heron/integration_test/topology/multi_spouts_multi_tasks/MultiSpoutsMultiTasks.java
@@ -13,12 +13,10 @@
 // limitations under the License.
 package com.twitter.heron.integration_test.topology.multi_spouts_multi_tasks;
 
-import java.net.URL;
+import java.net.MalformedURLException;
 
-import com.twitter.heron.api.Config;
-import com.twitter.heron.api.HeronSubmitter;
 import com.twitter.heron.api.tuple.Fields;
-import com.twitter.heron.integration_test.common.BasicConfig;
+import com.twitter.heron.integration_test.common.AbstractTestTopology;
 import com.twitter.heron.integration_test.common.bolt.IdentityBolt;
 import com.twitter.heron.integration_test.common.spout.ABSpout;
 import com.twitter.heron.integration_test.core.TestTopologyBuilder;
@@ -26,32 +24,24 @@ import com.twitter.heron.integration_test.core.TestTopologyBuilder;
 /**
  * Topology to test Spout with multiple threads
  */
-public final class MultiSpoutsMultiTasks {
+public final class MultiSpoutsMultiTasks extends AbstractTestTopology {
 
-  private MultiSpoutsMultiTasks() {
+  private MultiSpoutsMultiTasks(String[] args) throws MalformedURLException {
+    super(args);
   }
 
-  public static void main(String[] args) throws Exception {
-    if (args.length < 2) {
-      throw new RuntimeException("HttpServerUrl and TopologyName "
-          + "are needed as command line arguments");
-    }
-
-    URL httpServerUrl = new URL(args[0]);
-    String topologyName = args[1];
-
-    TestTopologyBuilder builder = new TestTopologyBuilder(topologyName, httpServerUrl.toString());
-
+  @Override
+  protected TestTopologyBuilder buildTopology(TestTopologyBuilder builder) {
     builder.setSpout("ab-spout-1", new ABSpout(), 3);
     builder.setSpout("ab-spout-2", new ABSpout(), 3);
     builder.setBolt("identity-bolt", new IdentityBolt(new Fields("word")), 1)
         .shuffleGrouping("ab-spout-1")
         .shuffleGrouping("ab-spout-2");
+    return builder;
+  }
 
-    // Conf
-    Config conf = new BasicConfig();
-
-    // Submit it!
-    HeronSubmitter.submitTopology(topologyName, conf, builder.createTopology());
+  public static void main(String[] args) throws Exception {
+    MultiSpoutsMultiTasks topology = new MultiSpoutsMultiTasks(args);
+    topology.submit();
   }
 }
--- integration-test/src/java/com/twitter/heron/integration_test/topology/non_grouping/NonGrouping.java
+++ integration-test/src/java/com/twitter/heron/integration_test/topology/non_grouping/NonGrouping.java
@@ -13,44 +13,33 @@
 // limitations under the License.
 package com.twitter.heron.integration_test.topology.non_grouping;
 
-import java.net.URL;
+import java.net.MalformedURLException;
 
-import com.twitter.heron.api.Config;
-import com.twitter.heron.api.HeronSubmitter;
 import com.twitter.heron.api.tuple.Fields;
-import com.twitter.heron.integration_test.common.BasicConfig;
+import com.twitter.heron.integration_test.common.AbstractTestTopology;
 import com.twitter.heron.integration_test.common.bolt.IdentityBolt;
 import com.twitter.heron.integration_test.common.spout.ABSpout;
 import com.twitter.heron.integration_test.core.TestTopologyBuilder;
 
-
 /**
  * Topology to test NonGrouping
  */
-public final class NonGrouping {
+public final class NonGrouping extends AbstractTestTopology {
 
-  private NonGrouping() {
+  private NonGrouping(String[] args) throws MalformedURLException {
+    super(args);
   }
 
-  public static void main(String[] args) throws Exception {
-    if (args.length < 2) {
-      throw new RuntimeException("HttpServerUrl and TopologyName are "
-          + "needed as command line arguments");
-    }
-
-    URL httpServerUrl = new URL(args[0]);
-    String topologyName = args[1];
-
-    TestTopologyBuilder builder = new TestTopologyBuilder(topologyName, httpServerUrl.toString());
-
+  @Override
+  protected TestTopologyBuilder buildTopology(TestTopologyBuilder builder) {
     builder.setSpout("ab-spout", new ABSpout(), 1);
     builder.setBolt("identity-bolt", new IdentityBolt(new Fields("word")), 3)
         .noneGrouping("ab-spout");
+    return builder;
+  }
 
-    // Conf
-    Config conf = new BasicConfig();
-
-    // Submit it!
-    HeronSubmitter.submitTopology(topologyName, conf, builder.createTopology());
+  public static void main(String[] args) throws Exception {
+    NonGrouping topology = new NonGrouping(args);
+    topology.submit();
   }
 }
