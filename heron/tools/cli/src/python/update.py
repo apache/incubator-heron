c52cd20634ec1d7803c9bf70c99699061bb863d1
Bill Graham <billg@twitter.com>
2016-10-26 11:33:35 -0700

    add packing jars to update command

--- heron/tools/cli/src/python/update.py
+++ heron/tools/cli/src/python/update.py
@@ -79,13 +79,16 @@ def run(command, parser, cl_args, unknown_args):
     if Log.getEffectiveLevel() == logging.DEBUG:
       new_args.append("--verbose")
 
-    lib_jars = config.get_heron_libs(jars.scheduler_jars() + jars.statemgr_jars())
+    lib_jars = config.get_heron_libs(
+      jars.scheduler_jars() + jars.statemgr_jars() + jars.packing_jars()
+    )
+    extra_jars = cl_args['extra_launch_classpath'].split(':')
 
     # invoke the runtime manager to kill the topology
     execute.heron_class(
         'com.twitter.heron.scheduler.RuntimeManagerMain',
         lib_jars,
-        extra_jars=[],
+        extra_jars=extra_jars,
         args=new_args
     )
 
