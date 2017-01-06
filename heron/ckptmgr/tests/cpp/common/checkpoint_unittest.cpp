4feeba1c36609877cd5a3547c4479b05bc637f48
Karthik Ramasamy <kramasamy@twitter.com>
2017-01-05 17:43:11 -0800

    initial tests for checkpoint

--- heron/ckptmgr/tests/cpp/common/checkpoint_unittest.cpp
+++ heron/ckptmgr/tests/cpp/common/checkpoint_unittest.cpp
@@ -14,34 +14,75 @@
  * limitations under the License.
  */
 
+#include "common/checkpoint.h"
 #include <string>
-#include <vector>
+
+#include "proto/messages.h"
 #include "gtest/gtest.h"
-#include "basics/basics.h"
-#include "basics/fileutils.h"
-#include "basics/modinit.h"
 
 namespace heron {
 namespace state {
 
-TEST(FileUtilsTest, basename) {
-  const std::string file1("/users/newuser/temp.file");
-  EXPECT_EQ("temp.file", FileUtils::baseName(file1));
+class CheckpointTest : public ::testing::Test {
+ public:
+  CheckpointTest() {}
+  ~CheckpointTest() {}
 
-  const std::string file2("temp.file");
-  EXPECT_EQ("temp.file", FileUtils::baseName(file2));
+  void SetUp() {}
+  void TearDown() {}
 
-  const std::string file3("/users/newuser/tempfile");
-  EXPECT_EQ("tempfile", FileUtils::baseName(file3));
+  heron::proto::ckptmgr::SaveStateCheckpoint* createSaveMessage() {
+    auto protomsg = new heron::proto::ckptmgr::SaveStateCheckpoint;
+    auto instance = protomsg->mutable_instance();
+    instance->mutable_instance_id()->assign("instance-1");
+    instance->mutable_stmgr_id()->assign("stmgr-1");
 
-  const std::string file4("tempfile");
-  EXPECT_EQ("tempfile", FileUtils::baseName(file4));
+    auto info = instance->mutable_info();
+    info->set_task_id(1);
+    info->mutable_component_name()->assign("component_name-1");
+    info->set_component_index(1);
 
-  const std::string file5("newuser/tempfile");
-  EXPECT_EQ("tempfile", FileUtils::baseName(file5));
+    auto checkpoint = protomsg->mutable_checkpoint();
+    checkpoint->mutable_checkpoint_id()->assign("checkpoint-1");
+    checkpoint->mutable_state()->assign("abcdefghijklmnopqrstuvwxyz");
+    return protomsg;
+  }
 
-  const std::string file6("");
-  EXPECT_EQ("", FileUtils::baseName(file6));
+  heron::proto::ckptmgr::RestoreStateCheckpoint* createRestoreMessage() {
+    auto protomsg = new heron::proto::ckptmgr::RestoreStateCheckpoint;
+    auto instance = protomsg->mutable_instance();
+    instance->mutable_instance_id()->assign("instance-2");
+    instance->mutable_stmgr_id()->assign("stmgr-2");
+
+    auto info = instance->mutable_info();
+    info->set_task_id(2);
+    info->mutable_component_name()->assign("component_name-2");
+    info->set_component_index(2);
+
+    protomsg->mutable_checkpoint_id()->assign("checkpoint-2");
+    return protomsg;
+  }
+};
+
+TEST_F(CheckpointTest, constructor) {
+  auto save_message = createSaveMessage();
+  Checkpoint sckpt("topology-1", save_message);
+
+  EXPECT_EQ(sckpt.getTopology(), "topology-1");
+  EXPECT_EQ(sckpt.getCkptId(), "checkpoint-1");
+  EXPECT_EQ(sckpt.getComponent(), "component_name-1");
+  EXPECT_EQ(sckpt.getInstance(), "instance-1");
+  EXPECT_EQ(sckpt.checkpoint(), save_message);
+
+  auto restore_message = createRestoreMessage();
+  Checkpoint rckpt("topology-2", restore_message);
+
+  EXPECT_EQ(rckpt.getTopology(), "topology-2");
+  EXPECT_EQ(rckpt.getCkptId(), "checkpoint-2");
+  EXPECT_EQ(rckpt.getComponent(), "component_name-2");
+  EXPECT_EQ(rckpt.getInstance(), "instance-2");
+  EXPECT_EQ(rckpt.checkpoint(), nullptr);
+  EXPECT_EQ(rckpt.nbytes(), 0);
 }
 
 }  // namespace state
