c2885ef68f1aaade9c9f12ab89655f4f8564591f
Runhang Li <rli@twitter.com>
2017-01-20 03:25:26 -0800

    Avoid star import.

--- heron/scheduler-core/tests/java/com/twitter/heron/scheduler/dryrun/UpdateDryRunRenderTest.java
+++ heron/scheduler-core/tests/java/com/twitter/heron/scheduler/dryrun/UpdateDryRunRenderTest.java
@@ -36,11 +36,11 @@ import com.twitter.heron.spi.common.Config;
 import com.twitter.heron.spi.common.ConfigKeys;
 import com.twitter.heron.spi.packing.InstanceId;
 import com.twitter.heron.spi.packing.PackingPlan;
-// SUPPRESS CHECKSTYLE AvoidStarImport
-import com.twitter.heron.spi.packing.PackingPlan.*;
 import com.twitter.heron.spi.packing.Resource;
 
 import static org.junit.Assert.assertEquals;
+import static com.twitter.heron.spi.packing.PackingPlan.ContainerPlan;
+import static com.twitter.heron.spi.packing.PackingPlan.InstancePlan;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(TopologyAPI.Topology.class)
