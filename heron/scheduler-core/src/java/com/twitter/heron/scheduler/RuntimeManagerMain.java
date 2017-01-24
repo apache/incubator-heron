b75c2a9bd77e1336520eb0f9d213d7633ae5d51f
Runhang Li <rli@twitter.com>
2017-01-24 09:51:10 -0800

    Use enum.

--- heron/scheduler-core/src/java/com/twitter/heron/scheduler/RuntimeManagerMain.java
+++ heron/scheduler-core/src/java/com/twitter/heron/scheduler/RuntimeManagerMain.java
@@ -16,6 +16,7 @@ package com.twitter.heron.scheduler;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -320,7 +321,7 @@ public class RuntimeManagerMain {
     } catch (UpdateDryRunResponse response) {
       LOG.log(Level.FINE, "Sending out dry-run response");
       // Output may contain UTF-8 characters, so we should print using UTF-8 encoding
-      PrintStream out = new PrintStream(System.out, true, "UTF-8");
+      PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8.name());
       out.print(runtimeManagerMain.renderDryRunResponse(response));
       // SUPPRESS CHECKSTYLE RegexpSinglelineJava
       // Exit with status code 200 to indicate dry-run response is sent out
