4c948d459ffceec9bf7d0f0d647374440db1702d
Mark Li <objmagic@users.noreply.github.com>
2017-02-16 10:26:15 -0800

    Use resources label as much as possible (#1710)

--- heron/common/tests/java/com/twitter/heron/common/config/Constants.java
+++ heron/common/tests/java/com/twitter/heron/common/config/Constants.java
@@ -14,17 +14,12 @@
 
 package com.twitter.heron.common.config;
 
-public final class Constants {
-  public static final String ROLE_KEY = "role";
-  public static final String ENVIRON_KEY = "environ";
-  public static final String USER_KEY = "user";
-  public static final String GROUP_KEY = "group";
-  public static final String VERSION_KEY = "version";
+final class Constants {
+  static final String ROLE_KEY = "role";
+  static final String ENVIRON_KEY = "environ";
+  static final String USER_KEY = "user";
 
-  public static final String LAUNCHER_CLASS_KEY = "heron.launcher.class";
-
-  public static final String TEST_DATA_PATH =
-      "/__main__/heron/common/tests/java/com/twitter/heron/common/config/testdata";
+  static final String LAUNCHER_CLASS_KEY = "heron.launcher.class";
 
   private Constants() {
   }
