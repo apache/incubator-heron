272571d68af19d522625f286d6b8367c5aa8614b
Bill Graham <billg@twitter.com>
2017-03-07 15:41:52 -0800

    Adding clarifying comments to the ExclamationTopology example

--- heron/examples/src/java/com/twitter/heron/examples/ExclamationTopology.java
+++ heron/examples/src/java/com/twitter/heron/examples/ExclamationTopology.java
@@ -98,10 +98,26 @@ public final class ExclamationTopology {
       // declarer.declare(new Fields("word"));
     }
 
+    /**
+     * Implementing this method is optional and only necessary if BOTH of the following are true:
+     *
+     * a.) you plan to dynamically scale your bolt/spout at runtime using 'heron update'.
+     * b.) you need to take action based on a runtime change to the component parallelism.
+     *
+     * Most bolts and spouts should be written to be unaffected by changes in their parallelism,
+     * but some must be aware of it. An example would be a spout that consumes a subset of queue
+     * partitions, which must be algorithmically divided amongst the total number of spouts.
+     * <P>
+     * Note that this method is from the IUpdatable Heron interface which does not exist in Storm.
+     * It is fine to implement IUpdatable along with other Storm interfaces, but implementing it
+     * will bind an otherwise generic Storm implementation to Heron.
+     *
+     * @param heronTopologyContext Heron topology context.
+     */
     @Override
-    public void update(com.twitter.heron.api.topology.TopologyContext topologyContext) {
+    public void update(com.twitter.heron.api.topology.TopologyContext heronTopologyContext) {
       List<Integer> newTaskIds =
-          topologyContext.getComponentTasks(topologyContext.getThisComponentId());
+          heronTopologyContext.getComponentTasks(heronTopologyContext.getThisComponentId());
       System.out.println("Bolt updated with new topologyContext. New taskIds: " + newTaskIds);
     }
   }
