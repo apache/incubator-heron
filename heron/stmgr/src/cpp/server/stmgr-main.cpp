8cbf0ac7ce395c48423728a3d4c16dddfcb9e309
Cong Wang <xiyou.wangcong@gmail.com>
2017-03-09 20:46:16 -0800

    Read back pressure configs from config file (#1756)
    heron.streammgr.network.backpressure.highwatermark.mb and
    heron.streammgr.network.backpressure.lowwatermark.mb are already available
    in heron_internals.yaml, but we never actually read them from it. This PR makes
    stream manager reading these configs from heron_internals.yaml. Note, other
    non-stmgr connections still use the default values in the original code.

--- heron/stmgr/src/cpp/server/stmgr-main.cpp
+++ heron/stmgr/src/cpp/server/stmgr-main.cpp
@@ -70,8 +70,15 @@ int main(int argc, char* argv[]) {
     LOG(FATAL) << "Corrupt topology defn file" << std::endl;
   }
 
+  sp_int64 high_watermark = heron::config::HeronInternalsConfigReader::Instance()
+                              ->GetHeronStreammgrNetworkBackpressureHighwatermarkMb() *
+                                1024 * 1024;
+  sp_int64 low_watermark = heron::config::HeronInternalsConfigReader::Instance()
+                              ->GetHeronStreammgrNetworkBackpressureLowwatermarkMb() *
+                                1024 * 1024;
   heron::stmgr::StMgr mgr(&ss, myport, topology_name, topology_id, topology, myid, instances,
-                          zkhostportlist, topdir, metricsmgr_port, shell_port);
+                          zkhostportlist, topdir, metricsmgr_port, shell_port, high_watermark,
+                          low_watermark);
   mgr.Init();
   ss.loop();
   return 0;
