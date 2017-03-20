4fb695f76b323bfb231c0e4af30549824bb0f8cf
Cong Wang <xiyou.wangcong@gmail.com>
2017-03-20 14:58:34 -0700

    Use variadic templates and std::bind to rewrite callback (#1768)

--- heron/common/src/cpp/basics/callback.h
+++ heron/common/src/cpp/basics/callback.h
@@ -66,6 +66,8 @@
 #ifndef HERON_CALLBACK_H_
 #define HERON_CALLBACK_H_
 
+#include <functional>
+
 /**
  * Basic CallBack definition.
  * There are two types of CallBacks.
@@ -74,176 +76,47 @@
  */
 class CallBack {
  public:
-  // Constructor - Users should not use this directly. Instead, use the
-  // CreateCallback functions detailed later in this file for creating CallBacks.
-  explicit CallBack(bool persist) {
-    persistent_ = persist;
-  }
+  // Constructor. Users should use the CreateCallback functions detailed below.
+  CallBack(bool persist, std::function<void()> f)
+    : persistent_(persist), bind_(f) {}
 
-  // Virtual Destructor
-  virtual ~CallBack() { }
+  virtual ~CallBack() {}
 
-  // The main exported method. This invokes the underlying function and cleans up
-  // if necessary.
+  // The main exported function
   void Run() {
-    // In case of permanent callbacks, the RunCallback might delete itself.
-    // For such cases after RunCallback returns it is not safe to access
+    // In case of persistent callbacks, the RunCallback might delete itself.
+    // In that case after RunCallback returns it is not safe to access
     // the class member persistent_. So make a copy now.
     bool persist = persistent_;
-    RunCallback();
-    if (!persist) { delete this; }
+    bind_();
+    if (!persist) {
+      delete this;
+    }
   }
 
-  // Returns if the CallBack is a temporary or permanent callback.
-  bool isPersistent() const {
-    return persistent_;
-  }
+  // Return whether the callback is temporary or permanent.
+  bool isPersistent() const { return persistent_; }
 
-  void makePersistent() {
-    persistent_ = true;
-  }
-
- protected:
-  // See callback.cc for details.
-  virtual void RunCallback() = 0;
+  void makePersistent() { persistent_ = true; }
 
  private:
   bool persistent_;
-};
-
-/*
- * Defines a callback for functions that take no arguments
- */
-class CallBack_0 : public CallBack {
- public:
-  CallBack_0(bool persist, void (*cb)()) : CallBack(persist) {
-    cb_ = cb;
-  }
-
-  virtual ~CallBack_0() { }
-
-  virtual void RunCallback() {
-    cb_();
-  }
-
- private:
-  void (*cb_)();                    // function ptr with no arg
-};
-
-/*
- * Defines a callback for functions that take 1 arg
- */
-template<typename C>
-class CallBack_1 : public CallBack {
- public:
-  CallBack_1(bool persist, void(*cb)(C), C c) : CallBack(persist) {
-    cb_ = cb;
-    c_ = c;
-  }
-  virtual ~CallBack_1() { }
-  virtual void RunCallback() {
-    cb_(c_);
-  }
- private:
-  void  (*cb_)(C);                  // function ptr with 1 arg
-  C c_;                             // actual value of the arg
-};
-
-/*
- * Defines call for functions that take 2 args
- */
-template<typename C, typename D>
-class CallBack_2 : public CallBack {
- public:
-  CallBack_2(bool _permanent, void(*cb)(C, D), C c, D d) :
-    CallBack(_permanent) {
-    cb_ = cb;
-    c_ = c;
-    d_ = d;
-  }
-  virtual ~CallBack_2() { }
-  virtual void RunCallback() {
-    cb_(c_, d_);
-  }
- private:
-  void (*cb_)(C, D);                // function ptr with 2 args
-  C c_;                             // actual value of the 1st arg
-  D d_;                             // actual value of the 2nd arg
-};
-
-/*
- * Defines callback for functions that take 3 args
- */
-template<typename C, typename D, typename E>
-class CallBack_3 : public CallBack {
- public:
-  CallBack_3(bool _permanent, void(*cb)(C, D, E), C c, D d, E e)
-    : CallBack(_permanent) {
-    cb_ = cb;
-    c_ = c;
-    d_ = d;
-    e_ = e;
-  }
-  virtual ~CallBack_3() { }
-  virtual void RunCallback() {
-    cb_(c_, d_, e_);
-  }
- private:
-  void (*cb_)(C, D, E);             // function ptr with 3 args
-  C c_;                             // actual value of the 1st arg
-  D d_;                             // actual value of the 2nd arg
-  E e_;                             // actual value of the 3rd arg
+  std::function<void()> bind_;
 };
 
 //
 // Helper functions to create callbacks
 //
+template <typename... C>
 inline
-CallBack* CreateCallback(void (*cb)()) {
-  auto cb0 = new CallBack_0(false, cb);
-  return cb0;
+CallBack* CreateCallback(void (*cb)(C... args), C... args) {
+  return new CallBack(false, std::bind(cb, args...));
 }
 
-template<typename C>
-CallBack* CreateCallback(void (*cb)(C), C c) {
-  auto cb1 = new CallBack_1<C>(false, cb, c);
-  return cb1;
-}
-
-template<typename C, typename D>
-CallBack* CreateCallback(void (*cb)(C, D), C c, D d) {
-  auto cb2 = new CallBack_2<C, D>(false, cb, c, d);
-  return cb2;
-}
-
-template<typename C, typename D, typename E>
-CallBack* CreateCallback(void (*cb)(C, D, E), C c, D d, E e) {
-  auto cb3 = new CallBack_3<C, D, E>(false, cb, c, d, e);
-  return cb3;
-}
-
+template <typename... C>
 inline
-CallBack* CreatePersistentCallback(void (*cb)()) {
-  auto cb0 = new CallBack_0(true, cb);
-  return cb0;
-}
-
-template<typename C>
-CallBack* CreatePersistentCallback(void (*cb)(C), C c) {
-  auto cb1 = new CallBack_1<C>(true, cb, c);
-  return cb1;
-}
-
-template<typename C, typename D>
-CallBack* CreatePersistentCallback(void (*cb)(C, D), C c, D d) {
-  auto cb2 = new CallBack_2<C, D>(true, cb, c, d);
-  return cb2;
-}
-
-template<typename C, typename D, typename E>
-CallBack* CreatePersistentCallback(void (*cb)(C, D, E), C c, D d, E e) {
-  auto cb3 = new CallBack_3<C, D, E>(true, cb, c, d, e);
-  return cb3;
+CallBack* CreatePersistentCallback(void (*cb)(C... args), C... args) {
+  return new CallBack(true, std::bind(cb, args...));
 }
 
 #endif  // HERON_CALLBACK_H_
