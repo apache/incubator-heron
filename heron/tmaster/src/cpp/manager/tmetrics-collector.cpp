1053669d971a0c26c600592f8623024009270cb3
Huijun Wu <huijunw@twitter.com>
2017-04-10 13:50:58 -0700

    improve log

--- heron/tmaster/src/cpp/manager/tmetrics-collector.cpp
+++ heron/tmaster/src/cpp/manager/tmetrics-collector.cpp
@@ -65,7 +65,7 @@ TMetricsCollector::TMetricsCollector(sp_int32 _max_interval, EventLoop* eventLoo
       auto_restart_last_(0),
       tmaster_(_tmaster) {
   LOG(INFO) << "Auto restart backpressure container: window size " << auto_restart_window_
-            << " minutes ; min interval " << auto_restart_interval_ << " minutes";
+            << " milliseconds ; min interval " << auto_restart_interval_ << " milliseconds";
   interval_ = config::HeronInternalsConfigReader::Instance()
                   ->GetHeronTmasterMetricsCollectorPurgeIntervalSec();
   CHECK_EQ(max_interval_ % interval_, 0);
