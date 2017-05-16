ac7ca93d7342a233fe4f0575185f5d7ff03e4a6a
ananth <ananthgs@yahoo.com>
2017-05-15 23:58:41 -0700

    added getJobLinks with ecs Task ID's

--- heron/schedulers/src/java/com/twitter/heron/scheduler/ecs/EcsScheduler.java
+++ heron/schedulers/src/java/com/twitter/heron/scheduler/ecs/EcsScheduler.java
@@ -14,18 +14,19 @@
 
 package com.twitter.heron.scheduler.ecs;
 
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import java.util.ArrayList;
+//import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
-import com.google.common.annotations.VisibleForTesting;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.commons.io.IOUtils;
 
@@ -37,7 +38,6 @@ import com.twitter.heron.spi.common.Context;
 import com.twitter.heron.spi.packing.PackingPlan;
 import com.twitter.heron.spi.scheduler.IScheduler;
 import com.twitter.heron.spi.utils.ShellUtils;
-
 
 public class EcsScheduler implements IScheduler {
   private static final Logger LOG = Logger.getLogger(EcsScheduler.class.getName());
@@ -58,14 +58,12 @@ public class EcsScheduler implements IScheduler {
   }
 
 
-  @VisibleForTesting
   protected int startExecutorSyncProcess(int container) {
     String executingInShell = new String();
     executingInShell = getExecutorCommand(container)[0];
     return ShellUtils.runProcess(executingInShell, null);
   }
 
-  @VisibleForTesting
   protected void startExecutor(final int container) {
     LOG.info("Starting a new executor for container: " + container);
     int shellOutput = startExecutorSyncProcess(container);
@@ -73,7 +71,6 @@ public class EcsScheduler implements IScheduler {
         + container + String.valueOf(shellOutput));
   }
 
-  @VisibleForTesting
   private String[] getExecutorCommand(int container) {
     List<Integer> freePorts = new ArrayList<>(SchedulerUtils.PORTS_REQUIRED_FOR_EXECUTOR);
     Integer localFreePort = null;
@@ -160,11 +157,78 @@ public class EcsScheduler implements IScheduler {
       startExecutor(container.getId());
     }
     LOG.info("Executor for each container have been started.");
+    LOG.info("Listing each of tasks started.");
+   // List<String> jobLinks = new ArrayList<String>();
+    //jobLinks = getJobLinks();
     return true;
   }
 
+  @Override
   public List<String> getJobLinks() {
-    return null;
+    List<String> list = new ArrayList<String>();
+    StringBuilder familyListcmd = new StringBuilder();
+    //String tempStr = "aws ecs list-task-definition-families --family-prefix ecscompose-";
+    familyListcmd.append(EcsContext.composeFamilyName(config));
+    familyListcmd.append(EcsContext.topologyName(config));
+    //tempStr = tempStr + EcsContext.topologyName(config);
+    LOG.info("final list cmd:" + familyListcmd);
+    StringBuilder stdout = new StringBuilder();
+    StringBuilder stderr = new StringBuilder();
+    List<String> familyString;
+    int status = ShellUtils.runProcess(familyListcmd.toString(), stdout);
+    if (status != 0) {
+      LOG.severe(String.format(
+          "Failed to run process. Command=%s, STDOUT=%s, STDERR=%s",
+            familyListcmd.toString(), stdout, stderr));
+    } else {
+      String listjsonString = stdout.toString();
+      try {
+        familyString = parseJsonName(listjsonString, EcsContext.composeListby(config));
+        for (String familyName : familyString) {
+          StringBuilder listout = new StringBuilder();
+          StringBuilder listerr = new StringBuilder();
+          StringBuilder taskListcmd = new StringBuilder();
+          taskListcmd.append(EcsContext.composeListCmd(config));
+          taskListcmd.append("  ");
+          taskListcmd.append(familyName);
+          int jobListstatus = ShellUtils.runProcess(taskListcmd.toString(), listout);
+          if (jobListstatus != 0) {
+            LOG.severe(String.format(
+                "Failed to run process. Command=%s, STDOUT=%s, STDERR=%s",
+                taskListcmd.toString(), listout, listerr));
+          } else {
+            List<String>  taskString = parseJsonName(listout.toString(),
+                                                      EcsContext.composeTaskTag(config));
+            for (String taskId : taskString) {
+              list.add(taskId);
+            }
+          }
+        }
+      } catch (JsonParseException e) {
+        LOG.severe("Unable to get list due to Parsing issues");
+      } catch (IOException ioe) {
+        LOG.severe("Unable to get list due to IO issues");
+      }
+    }
+    return list;
+  }
+
+  private List<String> parseJsonName(String jString, String jName) throws JsonParseException,
+      IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode actualObj = mapper.readTree(jString);
+    //JsonNode jsonNode1 = actualObj.get("families");
+    List<String> jsonList = new ArrayList<String>();
+    final JsonNode arrNode = new ObjectMapper().readTree(jString).get(jName);
+    if (arrNode.isArray()) {
+      for (final JsonNode objNode : arrNode) {
+        String taskDefn = objNode.asText();
+        jsonList.add(taskDefn.toString());
+      }
+    } else {
+      jsonList.add(arrNode.textValue());
+    }
+    return jsonList;
   }
 
   @Override
@@ -175,11 +239,15 @@ public class EcsScheduler implements IScheduler {
 
   @Override
   public boolean onRestart(Scheduler.RestartTopologyRequest request) {
+    // TODO(ananthgs): Need to see if re-starting each task is good.
+    LOG.severe("Topology onRestart not implemented by this scheduler Please use kill & start.");
     return false;
   }
 
   @Override
   public boolean onUpdate(Scheduler.UpdateTopologyRequest request) {
+    // TODO(ananthgs): Need to decide how to get pplans and  update
+    LOG.severe("Topology onUpdate not implemented by this scheduler.");
     return false;
   }
 
