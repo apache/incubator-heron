40367fa179e585b2fd61df0865f6a28b2d0d23d2
Neng Lu <freeneng@gmail.com>
2017-05-17 15:18:20 -0700

    add stateful checkpoints support in cpp statemgr (#1861)

--- heron/statemgrs/src/cpp/statemgr/heron-localfilestatemgr.h
+++ heron/statemgrs/src/cpp/statemgr/heron-localfilestatemgr.h
@@ -84,6 +84,18 @@ class HeronLocalFileStateMgr : public HeronStateMgr {
   void SetExecutionState(const proto::system::ExecutionState& _state,
                          VCallback<proto::system::StatusCode> _cb);
 
+  void CreateStatefulCheckpoints(const std::string& _topology_name,
+                      const proto::ckptmgr::StatefulConsistentCheckpoints& _ckpt,
+                      VCallback<proto::system::StatusCode> _cb);
+  void DeleteStatefulCheckpoints(const std::string& _topology_name,
+                            VCallback<proto::system::StatusCode> _cb);
+  void GetStatefulCheckpoints(const std::string& _topology_name,
+                      proto::ckptmgr::StatefulConsistentCheckpoints* _return,
+                      VCallback<proto::system::StatusCode> _cb);
+  void SetStatefulCheckpoints(const std::string& _topology_name,
+                      const proto::ckptmgr::StatefulConsistentCheckpoints& _state,
+                      VCallback<proto::system::StatusCode> _cb);
+
   void ListTopologies(std::vector<sp_string>* _return, VCallback<proto::system::StatusCode> _cb);
   void ListExecutionStateTopologies(std::vector<sp_string>* _return,
                                     VCallback<proto::system::StatusCode> _cb);
