f2e426752bb77a6e44d1aaed0871e624bd705863
ananth <ananthgs@yahoo.com>
2017-05-19 13:33:56 -0700

    modified:   setupEcs.sh

--- heron/config/src/yaml/conf/ecs/setupEcs.sh
+++ heron/config/src/yaml/conf/ecs/setupEcs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-##### PLEASE NOTE USE THIS AS A REFETECE. YOU MAY NEED TO CHANGE REGION/CLUSTER/KEYPAIR VALUES
+##### PLEASE NOTE USE THIS AS A REFERENCE. YOU MAY NEED TO CHANGE REGION/CLUSTER/AMI/KEYPAIR VALUES
 
 
 REGIONS=""
+
+SCOPE_AAS_PROBE_TOKEN="$1"
 
 if [ -z "$(which aws)" ]; then
     echo "error: Cannot find AWS-CLI, please make sure it's installed"
@@ -39,22 +41,22 @@ echo "done"
 
 # Security group
 echo -n "Creating Security Group (ecs-heron-demo) .. "
-SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ecs-heron-group  --description 'ECS Heron' --query 'GroupId' --output text)
+SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ecs-heron-securitygroup  --description 'ECS Heron' --query 'GroupId' --output text)
 # Wait for the group to get associated with the VPC
 sleep 5
 #opening all ports as port assignment will be random
 aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
 
 # Key pair
-echo -n "Creating Key Pair (ecs-heron-key, file ecs-heron-key.pem) .. "
-aws ec2 create-key-pair --key-name ecs-heron-key --query 'KeyMaterial' --output text > ecs-heron-key.pem
-chmod 600 ecs-heron-key.pem
+echo -n "Creating Key Pair (ecs-heron-keypair, file ecs-heron-keypair.pem) .. "
+aws ec2 create-key-pair --key-name ecs-heron-keypair --query 'KeyMaterial' --output text > ecs-heron-keypair.pem
+chmod 600 ecs-heron-keypair.pem
 echo "done"
 
 # IAM role
 echo -n "Creating IAM role (ecs-heron-role) .. "
-aws iam create-role --role-name ecs-heron-role --assume-role-policy-document file://data/ecs-heron-role.json > /dev/null
-aws iam put-role-policy --role-name ecs-heron-role --policy-name ecs-heron-policy --policy-document file://data/ecs-heron-policy.json
+aws iam create-role --role-name ecs-heron-role --assume-role-policy-document file://ecs-heron-role.json > /dev/null
+aws iam put-role-policy --role-name ecs-heron-role --policy-name ecs-heron-policy --policy-document file://ecs-heron-policy.json
 aws iam create-instance-profile --instance-profile-name ecs-heron-instance-profile > /dev/null
 # Wait for the instance profile to be ready, otherwise we get an error when trying to use it
 while ! aws iam get-instance-profile --instance-profile-name ecs-heron-instance-profile  2>&1 > /dev/null; do
@@ -68,24 +70,23 @@ echo "done"
 echo -n "Creating Launch Configuration (ecs-heron-launch-configuration) .. "
 # Wait for the role to be ready, otherwise we get:
 # A client error (ValidationError) occurred when calling the CreateLaunchConfiguration operation: You are not authorized to perform this operation.
-# Unfortunately even if you can list the profile, "aws autoscaling create-launch-configuration" barks about it not existing so lets sleep instead
 
 sleep 15
 
 TMP_USER_DATA_FILE=$(mktemp /tmp/ecs-heron-user-data-XXXX)
 trap 'rm $TMP_USER_DATA_FILE' EXIT
-cp data/set-ecs-cluster-name.sh $TMP_USER_DATA_FILE
+cp set-ecs-cluster-name.sh $TMP_USER_DATA_FILE
 if [ -n "$SCOPE_AAS_PROBE_TOKEN" ]; then
     echo "echo SERVICE_TOKEN=$SCOPE_AAS_PROBE_TOKEN >> /etc/ecs-heron/scope.config" >> $TMP_USER_DATA_FILE
 fi
 
-aws autoscaling create-launch-configuration --image-id $AMI --launch-configuration-name ecs-heron-launch-configuration --key-name ecs-heron-key --security-groups $SECURITY_GROUP_ID --instance-type t2.micro --user-data file://$TMP_USER_DATA_FILE  --iam-instance-profile ecs-heron-instance-profile --associate-public-ip-address --instance-monitoring Enabled=false
+aws autoscaling create-launch-configuration --image-id $AMI --launch-configuration-name ecs-heron-launch-configuration --key-name ecs-heron-keypair --security-groups $SECURITY_GROUP_ID --instance-type t2.micro --user-data file://$TMP_USER_DATA_FILE  --iam-instance-profile ecs-heron-instance-profile --associate-public-ip-address --instance-monitoring Enabled=false
 echo "done"
 
 #set up the ecs cli config 
-ecs-cli configure --region $REGION --access-key ecs-heron-key --cluster $CLUSTER
+ecs-cli configure --region $REGION --access-key ecs-heron-keypair --cluster ecs-heron-cluster 
 
 #create a CloudFormation template
-ecs-cli up --keypair ecs --capability-iam --size 2 --instance-type t2.micro
+ecs-cli up --keypair ecs-heron-keypair --capability-iam --size 2 --instance-type t2.micro
 
 echo "Setup is ready! Please submit the ECS HERON Topology"
