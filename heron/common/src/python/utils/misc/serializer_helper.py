2299f9d32929cbe435594cd73923afbbaa46ce03
Sanjeev Kulkarni <sanjeev@streaml.io>
2017-06-23 08:29:07 -0700

    Fixed path

--- heron/common/src/python/utils/misc/serializer_helper.py
+++ heron/common/src/python/utils/misc/serializer_helper.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 '''serializer_helper.py'''
 
-from heron.common.src.python.utils.misc import PythonSerializer
+from heron.api.src.python.serializer import PythonSerializer
 
 import heron.common.src.python.constants as constants
 import heron.common.src.python.pex_loader as pex_loader
