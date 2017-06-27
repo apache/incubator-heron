70b38f15938dd77b1b52a5c8dc9dce433b6db5ca
Karthik Ramasamy <kramasamy@gmail.com>
2017-06-27 12:13:32 -0700

    changed travis scripts to take an input arg that indicates the CI env (#2003)

--- scripts/travis/ci.sh
+++ scripts/travis/ci.sh
@@ -3,12 +3,16 @@
 set -e
 
 DIR=`dirname $0`
-
 source ${DIR}/common.sh
+
+# check if the ci environ argument is provided
+if [ $# -eq 0 ]; then
+  echo "ci environ arg not provided (travis|applatix)"
+fi
 
 T="${DIR}/build.sh"
 start_timer "$T"
-${DIR}/build.sh
+${DIR}/build.sh $1
 end_timer "$T"
 
 T="make site"
@@ -18,7 +22,7 @@ end_timer "$T"
 
 T="${DIR}/test.sh"
 start_timer "$T"
-${DIR}/test.sh
+${DIR}/test.sh $1
 end_timer "$T"
 
 print_timer_summary
