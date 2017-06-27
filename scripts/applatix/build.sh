1e80453730c064832c947d05183c36fe4bd1425e
Karthikeyan Ramasamy <karthikz@Karthikz-MacBook-Pro.local>
2017-06-27 13:45:13 -0700

    fixed the applatix scripts to pass the ci environ

--- scripts/applatix/build.sh
+++ scripts/applatix/build.sh
@@ -4,8 +4,11 @@ export PATH=$PATH:/root/bin
 
 which gcc
 gcc --version
+
 which g++
 g++ --version
+
 which python
 python --version
-cd /heron && USER=abc scripts/travis/build.sh 
+
+cd /heron && USER=abc scripts/travis/build.sh applatix
