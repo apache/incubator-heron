e2a9a6ab213e54ac49c6b037a06562782e8a05e1
Luc Perkins <lucperkins@gmail.com>
2017-06-27 15:31:30 -0700

    fix pydoc gen script once again

--- website/scripts/python-doc-gen.sh
+++ website/scripts/python-doc-gen.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 HERON_ROOT_DIR=$(git rev-parse --show-toplevel)
-INPUT=heron-py
+INPUT=pyheron
 TMP_DIR=$(mktemp -d)
 
-rm -rf static/api/python
+rm -rf static/api/python/*
 
 pdoc $INPUT \
   --html \
   --html-dir $TMP_DIR
 
 mv $TMP_DIR/pyheron static/api/python
-
-ls static/api/python
