72cbdfb1efaf1b4ac204aaf4f7dcf51e482136bc
Sanjeev Kulkarni <sanjeev@streaml.io>
2017-06-27 04:10:03 -0700

    Misc fixes in Python. Remove pyheron in docs plus remove common dep from examples

--- heron/examples/src/python/misc/test_task_hook.py
+++ heron/examples/src/python/misc/test_task_hook.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 '''module for example task hook'''
 from collections import Counter
+import logging
 
-from heron.common.src.python.utils.log import Log
 from heron.api.src.python.task_hook import ITaskHook
 
 # pylint: disable=unused-argument
@@ -23,39 +23,40 @@ class TestTaskHook(ITaskHook):
   CONST = 10000
 
   def prepare(self, conf, context):
-    Log.info("In prepare of TestTaskHook")
+    self.logger = logging.getLogger()
+    self.logger.info("In prepare of TestTaskHook")
     self.counter = Counter()
 
   # pylint: disable=no-self-use
   def clean_up(self):
-    Log.info("In clean_up of TestTaskHook")
+    self.logger.info("In clean_up of TestTaskHook")
 
   def emit(self, emit_info):
     self.counter['emit'] += 1
     if self.counter['emit'] % self.CONST == 0:
-      Log.info("TestTaskHook: emitted %s tuples" % str(self.counter['emit']))
+      self.logger.info("TestTaskHook: emitted %s tuples", str(self.counter['emit']))
 
   def spout_ack(self, spout_ack_info):
     self.counter['sp_ack'] += 1
     if self.counter['sp_ack'] % self.CONST == 0:
-      Log.info("TestTaskHook: spout acked %s tuples" % str(self.counter['sp_ack']))
+      self.logger.info("TestTaskHook: spout acked %s tuples", str(self.counter['sp_ack']))
 
   def spout_fail(self, spout_fail_info):
     self.counter['sp_fail'] += 1
     if self.counter['sp_fail'] % self.CONST == 0:
-      Log.info("TestTaskHook: spout failed %s tuples" % str(self.counter['sp_fail']))
+      self.logger.info("TestTaskHook: spout failed %s tuples", str(self.counter['sp_fail']))
 
   def bolt_execute(self, bolt_execute_info):
     self.counter['bl_exec'] += 1
     if self.counter['bl_exec'] % self.CONST == 0:
-      Log.info("TestTaskHook: bolt executed %s tuples" % str(self.counter['bl_exec']))
+      self.logger.info("TestTaskHook: bolt executed %s tuples", str(self.counter['bl_exec']))
 
   def bolt_ack(self, bolt_ack_info):
     self.counter['bl_ack'] += 1
     if self.counter['bl_ack'] % self.CONST == 0:
-      Log.info("TestTaskHook: bolt acked %s tuples" % str(self.counter['bl_ack']))
+      self.logger.info("TestTaskHook: bolt acked %s tuples", str(self.counter['bl_ack']))
 
   def bolt_fail(self, bolt_fail_info):
     self.counter['bl_fail'] += 1
     if self.counter['bl_fail'] % self.CONST == 0:
-      Log.info("TestTaskHook: bolt failed %s tuples" % str(self.counter['bl_fail']))
+      self.logger.info("TestTaskHook: bolt failed %s tuples", str(self.counter['bl_fail']))
