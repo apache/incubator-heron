8bcc560aa16c2d76a64be42d7f40b4247d9e9900
Karthikeyan Ramasamy <karthikz@Karthikz-MacBook-Pro.local>
2017-06-28 18:15:09 -0700

    flatten the directory structure for python integration test topologies

--- integration-test/src/python/integration_test/topology/test_topology_main.py
+++ integration-test/src/python/integration_test/topology/test_topology_main.py
@@ -17,18 +17,18 @@ import argparse
 import logging
 import sys
 
-from .basic_one_task import basic_one_task_builder
 from .all_grouping import all_grouping_buidler
+from .basic_one_task import basic_one_task_builder
+from .bolt_double_emit_tuples import bolt_double_emit_tuples_builder
+from .fields_grouping import fields_grouping_builder
+from .global_grouping import global_grouping_builder
+from .multi_spouts_multi_tasks import multi_spouts_multi_tasks_builder
 from .none_grouping import none_grouping_builder
 from .one_bolt_multi_tasks import one_bolt_multi_tasks_builder
 from .one_spout_bolt_multi_tasks import one_spout_bolt_multi_tasks_builder
-from .shuffle_grouping import shuffle_grouping_builder
-from .one_spout_two_bolts import one_spout_two_bolts_builder
 from .one_spout_multi_tasks import one_spout_multi_tasks_builder
-from .multi_spouts_multi_tasks import multi_spouts_multi_tasks_builder
-from .fields_grouping import fields_grouping_builder
-from .bolt_double_emit_tuples import bolt_double_emit_tuples_builder
-from .global_grouping import global_grouping_builder
+from .one_spout_two_bolts import one_spout_two_bolts_builder
+from .shuffle_grouping import shuffle_grouping_builder
 
 TOPOLOGY_BUILDERS = {
     'Heron_IntegrationTest_BasicOneTask': basic_one_task_builder,
