4d4b1734d1a3218bb859024420d76fc9bcc48d10
Sanjeev Kulkarni <sanjeevrk@gmail.com>
2017-07-08 14:21:38 -0700

    Python DSL (#2033)
    * Added first cut of python dsl
    
    * Added first cut of text input
    
    * Made everything a Streamlet
    
    * Added BUILD file and did the first set of fixes
    
    * Added MapBolt
    
    * Added FlatMapBolt
    
    * Added FilterBolt and FlatMapBolt
    
    * Added partial sample bolt
    
    * Added JoinBolt and JoinGrouping
    
    * Added RepartitionBolt
    
    * Made everything TIMEWINDOW
    
    * Join now support sliding interval
    
    * Added ReduceByWindow Bolt
    
    * Switched everything to reducebykeyandwindow bolt
    
    * Fixed all compilation issues
    
    * Added StaticLines Streamlet
    
    * Merged spouts inside the streamlet files
    
    * Changed function name
    
    * Added a dsl example
    
    * reduce takes timewindow
    
    * Fixed the format of Stream constructor
    
    * Fixed bugs while composing dag
    
    * Fixed more bugs
    
    * Fixed the way to specify custom grouping
    
    * Fixed generation of stage names
    
    * Specify classpath rather than instances
    
    * Fixed the Reduce/Join Custom grouping
    
    * Fixed logic for join/reduce tuple
    
    * Fixed the format of output tupled
    
    * Addressed review comments

---
+++ heron/dsl/src/python/inputs/inmemory/staticlines.py
@@ -0,0 +1,83 @@
+# Copyright 2016 - Twitter, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''staticlines.py: module for defining a simple static lines input'''
+
+from heron.api.src.python import Spout, Stream
+from heron.dsl.src.python import Streamlet, OperationType
+
+# pylint: disable=access-member-before-definition
+# pylint: disable=attribute-defined-outside-init
+class StaticLinesSpout(Spout):
+  """StaticLinesSpout: Generates a line from a set of static lines again and again
+  """
+  outputs = [Stream(fields=['_output_'], name='output')]
+
+  # pylint: disable=unused-argument
+  def initialize(self, config, context):
+    """Implements StaticLines Spout's initialize method"""
+    self.logger.info("Initializing StaticLinesSpout with the following")
+    self.logger.info("Component-specific config: \n%s" % str(config))
+    self.words = ["Mary had a little lamb",
+                  "Humpy Dumpy sat on a wall",
+                  "Here we round the Moulberry bush"]
+    self.index = 0
+    self.emit_count = 0
+    self.ack_count = 0
+    self.fail_count = 0
+
+  def _get_next_line(self):
+    retval = self.words[self.index]
+    self.index += 1
+    if self.index >= len(self.words):
+      self.index = 0
+    return retval
+
+  def next_tuple(self):
+    self.emit([self._get_next_line()], stream='output')
+    self.emit_count += 1
+
+  def ack(self, tup_id):
+    self.ack_count += 1
+    self.logger.debug("Acked tuple %s" % str(tup_id))
+
+  def fail(self, tup_id):
+    self.fail_count += 1
+    self.logger.debug("Failed tuple %s" % str(tup_id))
+
+class StaticLinesStreamlet(Streamlet):
+  """A StaticLinesStreamlet spews a set of words forever
+  """
+  # pylint: disable=no-self-use
+  def __init__(self, stage_name=None, parallelism=None):
+    super(StaticLinesStreamlet, self).__init__(operation=OperationType.Input,
+                                               stage_name=stage_name,
+                                               parallelism=parallelism)
+
+  @staticmethod
+  def staticLinesGenerator(stage_name=None, parallelism=None):
+    return StaticLinesStreamlet(stage_name=stage_name, parallelism=parallelism)
+
+  def _build(self, bldr, stage_names):
+    if self._parallelism is None:
+      self._parallelism = 1
+    if self._parallelism < 1:
+      raise RuntimeError("StaticLines parallelism has to be >= 1")
+    if self._stage_name is None:
+      index = 1
+      self._stage_name = "staticlines"
+      while self._stage_name in stage_names:
+        index = index + 1
+        self._stage_name = "staticlines" + str(index)
+      bldr.add_spout(self._stage_name, StaticLinesSpout, par=self._parallelism)
+    return bldr
---
+++ heron/examples/src/python/word_count_dsl.py
@@ -0,0 +1,28 @@
+# copyright 2016 twitter. all rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''Example WordCountTopology'''
+import sys
+
+from heron.dsl.src.python import StaticLinesStreamlet, TimeWindow
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print "Topology's name is not specified"
+    sys.exit(1)
+
+  counts = StaticLinesStreamlet.staticLinesGenerator(parallelism=2) \
+           .flatMap(lambda line: line.split(), parallelism=2) \
+           .map(lambda word: (word, 1), parallelism=2) \
+           .reduceByWindow(TimeWindow(10, 2), lambda x, y: x + y)
+  counts.run(sys.argv[1])
