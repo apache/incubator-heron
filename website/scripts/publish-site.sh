d4eebf890d8e157e6d7a044e8a654e496f9167a3
Luc Perkins <lucperkins@gmail.com>
2017-07-19 15:33:15 -0700

    modify publish script for the site

--- website/scripts/publish-site.sh
+++ website/scripts/publish-site.sh
@@ -6,9 +6,9 @@ ROOT_DIR=$(git rev-parse --show-topleveo)
     cd $ROOT_DIR
     rm -rf website/public
     git submodule update --init
-    (cd website/public && git checkout gh-pages)
-    (cd website && make travis-site)
+    cd website/public && git checkout gh-pages && git remote rename origin upstream && cd $ROOT_DIR
+    cd website && make travis-site && cd $ROOT_DIR
     cd website/public
     git add -A && git commit -m "New website build at $(date)"
-    git push -u origin gh-pages
+    git push -q upstream HEAD:gh-pages
 )
