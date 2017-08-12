10118027f68100c1d3a2592407bf3e430291e046
Karthik Ramasamy <karthik@streaml.io>
2017-08-11 22:48:33 -0700

    start a few topologies when you start the docker

--- docker/start-services.sh
+++ docker/start-services.sh
@@ -3,4 +3,7 @@
 heron-tracker --rootpath ~/.herondata/repository/state/onenode > /var/log/heron-tracker.log 2>&1 &
 heron-ui > /var/log/heron-ui.log 2>&1 &
 heron-apiserver --base-template=local --cluster=onenode -Dheron.directory.home=/usr/local/heron > /var/log/heron-apiserver.log 2>&1 &
+sleep 5
+heron submit onenode --service-url http://localhost:9000 /usr/local/heron/examples/heron-examples.jar com.twitter.heron.examples.ExclamationTopology ExclamationTopology
+heron submit onenode --service-url http://localhost:9000 /usr/local/heron/examples/heron-examples.jar com.twitter.heron.examples.MultiSpoutExclamationTopology MultiSpoutExclamationTopology
 sleep infinity
