783185c5ef7f1964343304a676f9cd86b7428ff6
Karthik Ramasamy <karthik@streaml.io>
2017-08-30 11:30:46 -0700

    changes to make heron compile with bazel 0.5.4 (#2207)
    * changes to make heron compile with bazel 0.5.1
    
    * changes to compile with bazel 0.5.2
    
    * added bazel 0.5.2 for travis
    
    * remove crosstool_top for travis with bazel 0.5.2
    
    * remove crosstool_top for applatix with bazel 0.5.2
    
    * added support for bazel 0.5.4 rc4 candidate
    
    * fixed warnings in pkg_tar and heron downloader
    
    * upgrade to new bazel 0.5.4 release
    
    * changed the path for heron-python-instance target
    
    * updated docs to use bazel 0.5.4 and updated heron version to be 0.15.1

--- heron/tmaster/src/cpp/manager/tmaster.cpp
+++ heron/tmaster/src/cpp/manager/tmaster.cpp
@@ -170,7 +170,7 @@ void TMaster::OnPackingPlanFetch(proto::system::PackingPlan* newPackingPlan,
       // In case a assignment already exists, we will throw this
       // list out and re-init with whats in assignment.
       absent_stmgrs_.clear();
-      for (sp_uint32 i = 0; i < packing_plan_->container_plans_size(); ++i) {
+      for (sp_int32 i = 0; i < packing_plan_->container_plans_size(); ++i) {
         LOG(INFO) << "Adding container id " << packing_plan_->container_plans(i).id()
                   << " to absent_stmgrs_";
         // the packing plan represents container ids by the numerical id of the container. The
