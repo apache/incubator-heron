e077df0e0aea110ed8ba8b1b7753160e3b388475
Karthik Ramasamy <karthik@streaml.io>
2017-09-01 18:23:01 -0700

    remove start services

--- docker/start-services.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-heron-tracker --rootpath ~/.herondata/repository/state/onenode > /var/log/heron-tracker.log 2>&1 &
-heron-ui > /var/log/heron-ui.log 2>&1 &
-heron-apiserver --base-template=local --cluster=onenode -Dheron.directory.home=/usr/local/heron > /var/log/heron-apiserver.log 2>&1 &
-sleep 5
-heron submit onenode --service-url http://localhost:9000 /usr/local/heron/examples/heron-examples.jar com.twitter.heron.examples.ExclamationTopology ExclamationTopology
-heron submit onenode --service-url http://localhost:9000 /usr/local/heron/examples/heron-examples.jar com.twitter.heron.examples.MultiSpoutExclamationTopology MultiSpoutExclamationTopology
-sleep infinity
