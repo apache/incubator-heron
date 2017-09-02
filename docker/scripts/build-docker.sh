8774e08bb40c77bf83d5df362cfd4ce39467716e
Karthik Ramasamy <karthik@streaml.io>
2017-09-01 18:24:42 -0700

    add supported platforms

--- docker/scripts/build-docker.sh
+++ docker/scripts/build-docker.sh
@@ -81,7 +81,7 @@ case $# in
     echo "  "
     echo "Usage: $0 <platform> <version_string> <artifact-directory> "
     echo "  "
-    echo "Platforms Supported: ubuntu14.04"
+    echo "Platforms Supported: ubuntu14.04, ubuntu15.10, ubuntu16.04, centos7"
     echo "  "
     echo "Example:"
     echo "  ./build-docker.sh ubuntu14.04 0.12.0 ~/ubuntu"
