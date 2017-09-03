78ec201d7e567c3e01e75a8534cacbd36cb1e16e
Sanjeev Kulkarni <sanjeev@streaml.io>
2017-09-03 16:17:40 -0700

    Introduced a Context class that allows us to run dsl streamlets

---
+++ heron/dsl/src/java/com/twitter/heron/dsl/Context.java
@@ -0,0 +1,51 @@
+//  Copyright 2017 Twitter. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package com.twitter.heron.dsl;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.twitter.heron.api.Config;
+import com.twitter.heron.api.HeronSubmitter;
+import com.twitter.heron.api.exception.AlreadyAliveException;
+import com.twitter.heron.api.exception.InvalidTopologyException;
+import com.twitter.heron.api.topology.TopologyBuilder;
+
+/**
+ * A Streamlet is a (potentially unbounded) ordered collection of tuples.
+ Streamlets originate from pub/sub systems(such Pulsar/Kafka), or from static data(such as
+ csv files, HDFS files), or for that matter any other source. They are also created by
+ transforming existing Streamlets using operations such as map/flatMap, etc.
+ Besides the tuples, a Streamlet has the following properties associated with it
+ a) name. User assigned or system generated name to refer the streamlet
+ b) nPartitions. Number of partitions that the streamlet is composed of. The nPartitions
+ could be assigned by the user or computed by the system
+ */
+public final class Context {
+  static void run(String name, Config config, Builder builder) {
+    Set<String> stageNames = new HashSet<>();
+    TopologyBuilder topologyBuilder = builder.build();
+    try {
+      HeronSubmitter.submitTopology(name, config, topologyBuilder.createTopology());
+    } catch (AlreadyAliveException e) {
+      e.printStackTrace();
+    } catch (InvalidTopologyException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private Context() { }
+}
