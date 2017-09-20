389b3f563d68ab502cab36374faa5220e0708e39
Sanjeev Kulkarni <sanjeevrk@gmail.com>
2017-09-19 22:48:20 -0700

    Java DSL for Heron (#2285)
    * Placeholder for Java DSL
    
    * More changes to address comments
    
    * Compiling version
    
    * Added FlatMap
    
    * Add filter
    
    * Added repartition
    
    * Added WindowConfig
    
    * First cut of JoinStreamlet
    
    * Fixed comparison bug
    
    * Added reduce
    
    * Added ability to create KVStreamlet from Streamlet
    
    * Added java docs
    
    * Added Union and reduceByWindow functions
    
    * Introduced clone and generalized repartition function
    
    * Make Window bolt conform to the proposed window interface
    
    * Finally compiling
    
    * Fix style checks
    
    * Moved Bolts to bolts dir and Streamlets to streamlets dir
    
    * Convert Streamlet/KVStreamlet into interfaces
    
    * moved window config to base
    
    * Moved all impl classes into impl
    
    * Made the Streamlet interface generics based
    
    * Added inner/left/outer joins
    
    * Add support for atleast once semantics by doing acking
    
    * Assign WindowConfig parameters to the WindowBolt
    
    * Made non windowed bolts stateful
    
    * Introducing Builder and keeping track of all children
    
    * Calculate children for kvstreamlets as well
    
    * Moved StreamletImpl and KVStreamletImpl to more intuitive locations
    
    * Moved groupings to their own dir
    
    * Adjusted the build into the new semantics
    
    * Introduced a Context class that allows us to run dsl streamlets
    
    * Added a createStreamlet interface to have an easy way to create Streamlets
    
    * WordCount Topology using Java DSL.
    
    * Fixed logic while building
    
    * Added SerializableSupplier concept to make Supplier Spouts Serializable
    
    * Introduced SerializablePredicate
    
    * Added SerializableBinaryOperator
    
    * Added SerializableBiFunction
    
    * Introduced SerializableFunction
    
    * Made KeyValue Serializable. With this the word count topology is now running
    
    * Added a trivial sink log
    
    * Reduced Operations should return WindowInformation
    
    * Updated java docs
    
    * Added JavaDocs for groupings
    
    * Added JavaDocs to spouts
    
    * Added Javadocs for all streamlet implementations
    
    * Added a ContextImpl class and made Context an interface
    
    * Added WindowConfigImpl to hide implementation details
    
    * Renamed as per feedback
    
    * Fixed build
    
    * Took feedback
    
    * Took feedback
    
    * Took feedback
    
    * Took feedback
    
    * Added a generic to function as a sink
    
    * Renamed spouts to sources and bolts to operators
    
    * Added sinks folder
    
    * Moved Spout references to Source
    
    * Moved Bolts to operators
    
    * Renamed Bolts to Sinks
    
    * Renamed Impl to Base
    
    * Took Bill's suggestion
    
    * Renamed Context as Runner
    
    * Added a transform operator that gives users more flexible transforms
    
    * Renamed to newSource
    
    * Renamed window functions based on feedback
    
    * Renamed to toSink
    
    * added count info to windows
    
    * Added check for name
    
    * Fixdd misplaced comments
    
    * Fixed comments
    
    * Refactored common code to single place
    
    * Took feedback into account
    
    * Fixed comments
    
    * Took feedback
    
    * Moved the examples to the newly formed example
    
    * Added unittest for BaseStreamlet
    
    * Added unittest for KVBaseStreamlet

---
+++ heron/dsl/src/java/com/twitter/heron/dsl/impl/streamlets/TransformStreamlet.java
@@ -0,0 +1,54 @@
+//  Copyright 2017 Twitter. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package com.twitter.heron.dsl.impl.streamlets;
+
+import java.util.Set;
+
+import com.twitter.heron.api.topology.TopologyBuilder;
+import com.twitter.heron.dsl.TransformFunction;
+import com.twitter.heron.dsl.impl.BaseStreamlet;
+import com.twitter.heron.dsl.impl.operators.TransformOperator;
+
+/**
+ * TransformStreamlet represents a Streamlet that is made up of applying the user
+ * supplied transform function to each element of the parent streamlet. It differs
+ * from the simple MapStreamlet in the sense that it provides setup/cleanup flexibility
+ * for the users to setup things and cleanup before the beginning of the computation
+ */
+public class TransformStreamlet<R, T> extends BaseStreamlet<T> {
+  private BaseStreamlet<R> parent;
+  private TransformFunction<? super R, ? extends T> transformFunction;
+
+  public TransformStreamlet(BaseStreamlet<R> parent,
+                            TransformFunction<? super R, ? extends T> transformFunction) {
+    this.parent = parent;
+    this.transformFunction = transformFunction;
+    setNumPartitions(parent.getNumPartitions());
+  }
+
+  @Override
+  public boolean doBuild(TopologyBuilder bldr, Set<String> stageNames) {
+    if (getName() == null) {
+      setName(defaultNameCalculator("transform", stageNames));
+    }
+    if (stageNames.contains(getName())) {
+      throw new RuntimeException("Duplicate Names");
+    }
+    stageNames.add(getName());
+    bldr.setBolt(getName(), new TransformOperator<R, T>(transformFunction),
+        getNumPartitions()).shuffleGrouping(parent.getName());
+    return true;
+  }
+}
