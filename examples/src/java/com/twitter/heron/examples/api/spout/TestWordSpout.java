af70b45db5172990bb5304bd7262f2f369d693d2
Sanjeev Kulkarni <sanjeevrk@gmail.com>
2017-09-28 20:44:53 -0700

    Seperate out the low level api examples and higher level dsl examples (#2361)
    * Seperate out the low level api examples and higher level dsl examples
    
    * Fix package targets

--- examples/src/java/com/twitter/heron/examples/api/spout/TestWordSpout.java
+++ examples/src/java/com/twitter/heron/examples/api/spout/TestWordSpout.java
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-package com.twitter.heron.examples.spout;
+package com.twitter.heron.examples.api.spout;
 
 import java.time.Duration;
 import java.util.Map;
