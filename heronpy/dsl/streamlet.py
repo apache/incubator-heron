6efb966b45064e1ac5ae52efdd3a658d2b246d9c
Sanjeev Kulkarni <sanjeev@streaml.io>
2017-09-29 19:37:31 -0700

    First cut of enhanced streamlet

--- heronpy/dsl/streamlet.py
+++ heronpy/dsl/streamlet.py
@@ -19,8 +19,6 @@ from heronpy.api.topology import TopologyBuilder
 
 from heronpy.dsl.dslboltbase import DslBoltBase
 
-TimeWindow = namedtuple('TimeWindow', 'duration sliding_interval')
-
 # pylint: disable=too-many-instance-attributes
 class Streamlet(object):
   """A Streamlet is a (potentially unbounded) ordered collection of tuples
@@ -28,46 +26,66 @@ class Streamlet(object):
      csv files, HDFS files), or for that matter any other source. They are also created by
      transforming existing Streamlets using operations such as map/flat_map, etc.
   """
-  @property
-  def name(self):
-    """The name of the Streamlet"""
-    pass
+  def __init__(self):
+    """
+    """
+    self._children = None
+    self._name = None
+    self._num_partitions = 1
+    self._children = []
+    self._built = False
+    self._output = DslBoltBase.outputs[0].stream_id
 
-  @name.setter
-  def name(self, nm):
-    pass
+  def set_name(self, name):
+    """Sets the name of the Streamlet"""
+    self._name = name
+    return self
 
-  @property
-  def num_partitions(self):
-    """The number of partitions"""
-    pass
+  def get_name(self):
+    return self._name
 
-  @num_partitions.setter
-  def num_partitions(self, n):
-    pass
+  def set_num_partitions(self, num_partitions):
+    """Sets the number of partitions"""
+    self._num_partitions = num_partitions
+    return self
+
+  def get_num_partitions(self):
+    return self._num_partitions
 
   def map(self, map_function):
     """Return a new Streamlet by applying map_function to each element of this Streamlet.
     """
-    pass
+    from heronpy.dsl.mapbolt import MapStreamlet
+    map_streamlet = MapStreamlet(map_function, self)
+    self._add_child(map_streamlet)
+    return map_streamlet
 
   def flat_map(self, flatmap_function):
     """Return a new Streamlet by applying map_function to each element of this Streamlet
        and flattening the result
     """
-    pass
+    from heronpy.dsl.flatmapbolt import FlatMapStreamlet
+    fm_streamlet = FlatMapStreamlet(flatmap_function, self)
+    self._add_child(fm_streamlet)
+    return fm_streamlet
 
   def filter(self, filter_function):
     """Return a new Streamlet containing only the elements that satisfy filter_function
     """
-    pass
+    from heronpy.dsl.filterbolt import FilterStreamlet
+    filter_streamlet = FilterStreamlet(filter_function, self)
+    self._add_child(filter_streamlet)
+    return filter_streamlet
 
   def repartition(self, num_partitions):
     """Return a new Streamlet containing all elements of the this streamlet but having
     num_partitions partitions. Note that this is different from num_partitions(n) in
     that new streamlet will be created by the repartition call.
     """
-    pass
+    from heronpy.dsl.repartitionbolt import RepartitionStreamlet
+    repartition_streamlet = RepartitionStreamlet(parallelism, self)
+    self._add_child(repartition_streamlet)
+    return repartition_streamlet
 
   def repartition(self, num_partitions, repartition_function):
     """Same as above except to use repartition_function to choose
@@ -77,13 +95,19 @@ class Streamlet(object):
     It could also return a list of partitions if it wants to send it to multiple
     partitions.
     """
-    pass
+    from heronpy.dsl.repartitionbolt import RepartitionStreamlet
+    repartition_streamlet = RepartitionStreamlet(num_partitions, repartition_function, self)
+    self._add_child(repartition_streamlet)
+    return repartition_streamlet
 
   def clone(self, num_clones):
     """Return num_clones number of streamlets each containing all elements
     of the current streamlet
     """
-    pass
+    retval = []
+    for i in xrange(num_clones):
+      retval.append(self.repartition(self.get_num_partitions()))
+    return retval
 
   def reduce_by_window(self, window_config, reduce_function):
     """Return a new Streamlet in which each element of this Streamlet are collected
@@ -91,12 +115,19 @@ class Streamlet(object):
       reduce_function takes two element at one time and reduces them to one element that
       is used in the subsequent operations.
     """
-    pass
+    from heronpy.dsl.reducebywindowbolt import ReduceByWindowBolt
+    reduce_streamlet = ReduceByWindowBolt(window_config, reduce_function, self)
+    self._add_child(reduce_streamlet)
+    return reduce_streamlet
 
   def union(self, other_streamlet):
     """Returns a new Streamlet that consists of elements of both this and other_streamlet
     """
-    pass
+    from heronpy.dsl.unionbolt import UnionBolt
+    union_streamlet = UnionBolt(self, other_streamlet)
+    self._add_child(union_streamlet)
+    other_streamlet._add_child(union_streamlet)
+    return union_streamlet
 
   def transform(self, transform_function):
     """Returns a  new Streamlet by applying the transform_function on each element of this
@@ -104,51 +135,65 @@ class Streamlet(object):
     Before starting to cycle over the Streamlet, the open function of the transform_function is
     called. This allows the transform_function to do any kind of initialization/loading, etc.
     """
-    pass
+    from heronpy.dsl.transformbolt import TransformStreamlet
+    transform_streamlet = TransformStreamlet(transform_function, self)
+    self._add_child(transform_streamlet)
+    return transform_streamlet
 
   def log(self):
     """Logs all elements of this streamlet. This returns nothing
     """
-    pass
+    from heronpy.dsl.logbolt import LogStreamlet
+    log_streamlet = LogStreamlet(self)
+    self._add_child(log_streamlet)
+    return
 
   def to_sink(self, consumer_function):
     """Calls consumer_function for each element of this streamlet. This function returns nothing
     """
-    pass
-
+    from heronpy.dsl.sinkbolt import SinkStreamlet
+    sink_streamlet = SinkStreamlet(consumer_function, self)
+    self._add_child(sink_streamlet)
+    return
 
   def join(self, join_streamlet, window_config, join_function):
     """Return a new Streamlet by joining join_streamlet with this streamlet
     """
     from heronpy.dsl.joinbolt import JoinStreamlet
-    return JoinStreamlet(time_window, parents=[self, join_streamlet],
-                         stage_name=stage_name, parallelism=parallelism)
+    join_streamlet = JoinStreamlet(JoinStreamlet.INNER, window_config,
+                                   join_function, self, join_streamlet)
+    self._add_child(join_streamlet)
+    join_streamlet._add_child(join_streamlet)
+    return join_streamlet
 
-  def reduce_by_key_and_window(self, time_window, reduce_function,
-                               stage_name=None, parallelism=None):
+  def outer_join(self, join_streamlet, window_config, join_function):
+    """Return a new Streamlet by outer joining join_streamlet with this streamlet
+    """
+    from heronpy.dsl.joinbolt import JoinStreamlet
+    join_streamlet = JoinStreamlet(JoinStreamlet.OUTER, window_config,
+                                   join_function, self, join_streamlet)
+    self._add_child(join_streamlet)
+    join_streamlet._add_child(join_streamlet)
+    return join_streamlet
+
+  def left_join(self, join_streamlet, window_config, join_function):
+    """Return a new Streamlet by left joining join_streamlet with this streamlet
+    """
+    from heronpy.dsl.joinbolt import JoinStreamlet
+    join_streamlet = JoinStreamlet(JoinStreamlet.LEFT, window_config,
+                                   join_function, self, join_streamlet)
+    self._add_child(join_streamlet)
+    join_streamlet._add_child(join_streamlet)
+    return join_streamlet
+
+  def reduce_by_key_and_window(self, window_config, reduce_function):
     """Return a new Streamlet in which each (key, value) pair of this Streamlet are collected
        over the time_window and then reduced using the reduce_function
     """
     from heronpy.dsl.reducebykeyandwindowbolt import ReduceByKeyAndWindowStreamlet
-    return ReduceByKeyAndWindowStreamlet(time_window, reduce_function,
-                                         parents=[self],
-                                         stage_name=stage_name, parallelism=parallelism)
-
-  def run(self, name, config=None):
-    """Runs the Streamlet. This is run as a Heron python topology under the name
-       'name'. The config attached is passed on to this Heron topology
-       Once submitted, run returns immediately
-    """
-    if name is None or not isinstance(name, str):
-      raise RuntimeError("Job Name has to be a string")
-    bldr = TopologyBuilder(name=name)
-    stage_names = {}
-    bldr = self._build(bldr, stage_names)
-    if config is not None:
-      if not isinstance(config, dict):
-        raise RuntimeError("config has to be a dict")
-      bldr.set_config(config)
-    bldr.build_and_submit()
+    reduce_streamlet = ReduceByKeyAndWindowStreamlet(window_config, reduce_function, self)
+    self._add_child(reduce_streamlet)
+    return reduce_streamlet
 
   ##################################################################
   ### Internal functions
@@ -156,41 +201,33 @@ class Streamlet(object):
 
   # pylint: disable=protected-access
   def _build(self, bldr, stage_names):
-    for parent in self._parents:
-      parent._build(bldr, stage_names)
-    if self._parallelism is None:
-      self._parallelism = self._calculate_parallelism()
-    if self._stage_name is None:
-      self._stage_name = self._calculate_stage_name(stage_names)
-    if self._stage_name in stage_names:
-      raise RuntimeError("duplicated stage name %s" % self._stage_name)
-    stage_names[self._stage_name] = 1
-    self._build_this(bldr)
-    return bldr
+    if self._built:
+      raise RuntimeError("Logic error while building")
+    if self.build_this(bldr, stage_names):
+      self._built = True
+      for children in self._children:
+        children.build(bldr, stage_names)
 
   @abstractmethod
-  def _build_this(self, builder):
+  def _build_this(self, builder, stage_names):
     """This is the method that's implemented by the operators.
     :type builder: TopologyBuilder
     :param builder: The operator adds in the current streamlet as a spout/bolt
     """
     raise RuntimeError("Streamlet's _build_this not implemented")
 
-  # pylint: disable=protected-access
-  @abstractmethod
-  def _calculate_parallelism(self):
-    """This is the method that's implemented by the operators with a default impl
-    :return: The parallelism required for this operator
-    """
-    parallelism = 1
-    for parent in self._parents:
-      if parent._parallelism > parallelism:
-        parallelism = parent._parallelism
-    return parallelism
+  def _add_child(self, child):
+    self._children.append(child)
 
-  @abstractmethod
-  def _calculate_stage_name(self, existing_stage_names):
+  def _default_stage_name_calculator(self, prefix, existing_stage_names):
     """This is the method that's implemented by the operators to get the name of the Streamlet
     :return: The name of the operator
     """
-    raise RuntimeError("Streamlet's calculate stage name not implemented")
+    index = 1
+    calculated_name = ""
+    while True:
+      calculated_name = prefix + "-" + str(index)
+      if calculated_name in existing_stage_names:
+        return calculated_name
+      index = index + 1
+    return "Should Never Got Here"
