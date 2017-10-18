787aabaad45a4931fb836e855b52158af4722b58
Sanjeev Kulkarni <sanjeevrk@gmail.com>
2017-10-18 14:41:54 -0700

    Heron C++ Instance (#2449)
    * CPP Instance
    
    * remove using emptyString now that EMPTY_STRING is a std::string
    
    * Add ldl flag
    
    * Renamed ReConnect to Reconnect

---
+++ heron/instance/src/cpp/boltimpl/bolt-output-collector-impl.cpp
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2017 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <list>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "boltimpl/bolt-output-collector-impl.h"
+#include "proto/messages.h"
+#include "network/network.h"
+#include "basics/basics.h"
+
+#include "boltimpl/tuple-impl.h"
+#include "boltimpl/tick-tuple.h"
+
+struct rootCompare {
+  bool operator() (const heron::proto::system::RootId& lhs,
+                   const heron::proto::system::RootId& rhs) const {
+    return lhs.taskid() < rhs.taskid() ||
+           (lhs.taskid() == rhs.taskid() && lhs.key() < rhs.key());
+  }
+};
+
+namespace heron {
+namespace instance {
+
+BoltOutputCollectorImpl::BoltOutputCollectorImpl(
+                          std::shared_ptr<api::serializer::IPluggableSerializer> serializer,
+                          std::shared_ptr<TaskContextImpl> taskContext,
+                          NotifyingCommunicator<google::protobuf::Message*>* dataFromSlave,
+                          std::shared_ptr<BoltMetrics> metrics)
+  : api::bolt::IBoltOutputCollector(serializer), metrics_(metrics) {
+  collector_ = new OutgoingTupleCollection(taskContext->getThisComponentName(), dataFromSlave);
+  ackingEnabled_ = taskContext->isAckingEnabled();
+  taskId_ = taskContext->getThisTaskId();
+}
+
+BoltOutputCollectorImpl::~BoltOutputCollectorImpl() {
+  delete collector_;
+}
+
+void BoltOutputCollectorImpl::reportError(std::exception& except) {
+  LOG(INFO) << "Reporting an error in topology code " << except.what();
+}
+
+void BoltOutputCollectorImpl::emitInternal(const std::string& streamId,
+                                           std::list<std::shared_ptr<api::tuple::Tuple>>& anchors,
+                                           const std::vector<std::string>& tup) {
+  auto msg = new proto::system::HeronDataTuple();
+  msg->set_key(0);
+  std::set<proto::system::RootId, rootCompare> mergedRoots;
+  for (auto anchor : anchors) {
+    std::shared_ptr<TupleImpl> t = std::dynamic_pointer_cast<TupleImpl>(anchor);
+    if (t) {
+      std::shared_ptr<const proto::system::HeronDataTuple> actualRepr = t->getDataTuple();
+      for (int i = 0; i < actualRepr->roots_size(); ++i) {
+        mergedRoots.insert(actualRepr->roots(i));
+      }
+    }
+  }
+  for (auto root : mergedRoots) {
+    msg->add_roots()->CopyFrom(root);
+  }
+  int64_t totalSize = 0;
+  for (auto& s : tup) {
+    totalSize += s.size();
+  }
+  collector_->addDataTuple(streamId, msg, totalSize);
+  metrics_->emittedTuple(streamId);
+}
+
+void BoltOutputCollectorImpl::ack(std::shared_ptr<api::tuple::Tuple> tup) {
+  if (ackingEnabled_) {
+    std::shared_ptr<TupleImpl> t = std::dynamic_pointer_cast<TupleImpl>(tup);
+    if (t) {
+      proto::system::AckTuple* ack = new proto::system::AckTuple();
+      std::shared_ptr<const proto::system::HeronDataTuple> actualRepr = t->getDataTuple();
+      ack->set_ackedtuple(actualRepr->key());
+      int64_t tupSize = 0;
+      for (int i = 0; i < actualRepr->roots_size(); ++i) {
+        ack->add_roots()->CopyFrom(actualRepr->roots(i));
+        tupSize += actualRepr->roots(i).ByteSize();
+      }
+      collector_->addAckTuple(ack, tupSize);
+      int64_t currentTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                                   std::chrono::system_clock::now().time_since_epoch()).count();
+      metrics_->ackedTuple(t->getSourceStreamId(), t->getSourceComponent(),
+                           currentTime - t->getCreationTimeNs());
+    }
+  }
+}
+
+void BoltOutputCollectorImpl::fail(std::shared_ptr<api::tuple::Tuple> tup) {
+  if (ackingEnabled_) {
+    std::shared_ptr<TupleImpl> t = std::dynamic_pointer_cast<TupleImpl>(tup);
+    if (t) {
+      proto::system::AckTuple* fl = new proto::system::AckTuple();
+      std::shared_ptr<const proto::system::HeronDataTuple> actualRepr = t->getDataTuple();
+      fl->set_ackedtuple(actualRepr->key());
+      int64_t tupSize = 0;
+      for (int i = 0; i < actualRepr->roots_size(); ++i) {
+        fl->add_roots()->CopyFrom(actualRepr->roots(i));
+        tupSize += actualRepr->roots(i).ByteSize();
+      }
+      collector_->addFailTuple(fl, tupSize);
+      int64_t currentTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                                   std::chrono::system_clock::now().time_since_epoch()).count();
+      metrics_->failedTuple(t->getSourceStreamId(), t->getSourceComponent(),
+                            currentTime - t->getCreationTimeNs());
+    }
+  }
+}
+
+}  // namespace instance
+}  // namespace heron
---
+++ heron/instance/src/cpp/spoutimpl/spout-instance.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2017 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HERON_INSTANCE_SPOUT_SPOUT_INSTANCE_H_
+#define HERON_INSTANCE_SPOUT_SPOUT_INSTANCE_H_
+
+#include <string>
+
+#include "slave/instance-base.h"
+
+#include "proto/messages.h"
+#include "network/network.h"
+#include "basics/basics.h"
+
+#include "utils/notifying-communicator.h"
+#include "spout/ispout.h"
+#include "topology/task-context.h"
+#include "serializer/ipluggable-serializer.h"
+#include "spoutimpl/spout-output-collector-impl.h"
+#include "spoutimpl/spout-metrics.h"
+
+namespace heron {
+namespace instance {
+
+class SpoutInstance : public InstanceBase {
+ public:
+  SpoutInstance(EventLoop* eventLoop, std::shared_ptr<TaskContextImpl> taskContext,
+                NotifyingCommunicator<google::protobuf::Message*>* dataFromSlave,
+                void* dllHandle);
+  virtual ~SpoutInstance();
+
+  // This essentially sets up the spout and calls open
+  virtual void Start();
+  virtual void Activate();
+  virtual void Deactivate();
+  virtual bool IsRunning() { return active_; }
+  virtual void DoWork();
+  virtual void HandleGatewayTuples(proto::system::HeronTupleSet2* tupleSet);
+
+ private:
+  void lookForTimeouts();
+  void produceTuple();
+  void doImmediateAcks();
+  bool canContinueWork();
+  bool canProduceTuple();
+  void handleAckTuple(const proto::system::AckTuple& ackTuple, bool isAck);
+
+  std::shared_ptr<TaskContextImpl> taskContext_;
+  NotifyingCommunicator<google::protobuf::Message*>* dataFromSlave_;
+  EventLoop* eventLoop_;
+  api::spout::ISpout* spout_;
+  std::shared_ptr<api::serializer::IPluggableSerializer> serializer_;
+  std::shared_ptr<SpoutOutputCollectorImpl> collector_;
+  std::shared_ptr<SpoutMetrics> metrics_;
+  bool active_;
+  bool ackingEnabled_;
+  bool enableMessageTimeouts_;
+  int64_t lookForTimeoutsTimer_;
+  // This is the max number of outstanding packets that are buffered
+  // to be sent to the gateway
+  int maxWriteBufferSize_;
+  // This is the max time to spend in emitting tuple in one go
+  int maxEmitBatchIntervalMs_;
+};
+
+}  // namespace instance
+}  // namespace heron
+
+#endif  // HERON_INSTANCE_SPOUT_SPOUT_INSTANCE_H_
