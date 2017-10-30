1c7955ccfb0052331fe31d1194482979c6aa846c
Luc Perkins <lucperkins@gmail.com>
2017-10-30 13:09:12 -0700

    add comments for streamlet clone example

--- examples/src/java/com/twitter/heron/examples/streamlet/StreamletCloneTopology.java
+++ examples/src/java/com/twitter/heron/examples/streamlet/StreamletCloneTopology.java
@@ -24,26 +24,41 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * This topology demonstrates clone operations on streamlets in the Heron
+ * Streamlet API for Java. A clone operation creates multiple identical copies
+ * of a streamlet. Clone operations are especially useful if you want to, for
+ * example send streamlet elements to separate sinks, as is done here. A
+ * supplier streamlet emits random scores in a game (per player). That initial
+ * streamlet is cloned into two. One of the cloned streams goes to a custom
+ * logging sink while the other goes to a dummy database sink.
+ */
 public class StreamletCloneTopology {
     private static final Logger LOG =
             Logger.getLogger(StreamletCloneTopology.class.getName());
 
+    /**
+     * A list of players of the game ("player1" through "player100").
+     */
     private static final List<String> PLAYERS = IntStream.range(1, 100)
             .mapToObj(i -> String.format("player%d", i))
             .collect(Collectors.toList());
 
+    /**
+     * A POJO for game scores.
+     */
     private static class GameScore implements Serializable {
         private static final long serialVersionUID = 1089454399729015529L;
-        private String userId;
+        private String playerId;
         private int score;
 
         GameScore() {
-            this.userId = StreamletUtils.randomFromList(PLAYERS);
+            this.playerId = StreamletUtils.randomFromList(PLAYERS);
             this.score = ThreadLocalRandom.current().nextInt(1000);
         }
 
-        String getUserId() {
-            return userId;
+        String getPlayerId() {
+            return playerId;
         }
 
         int getScore() {
@@ -51,33 +66,40 @@ public class StreamletCloneTopology {
         }
     }
 
-    private static class StateSink implements Sink<GameScore> {
+    /**
+     * A phony database sink. This sink doesn't actually interact with a database.
+     * Instead, it logs each incoming score to stdout.
+     */
+    private static class DatabaseSink implements Sink<GameScore> {
         private static final long serialVersionUID = 5544736723673011054L;
-        private Context ctx;
 
-        public void setup(Context context) {
-            this.ctx = context;
+        private void saveToDatabase(GameScore score) {
+            // This is a dummy operation, so no database logic will be implemented here
         }
 
+        public void setup(Context context) {}
+
         public void put(GameScore score) {
-            String key = String.format("score-%s", score.getUserId());
-            int incomingScore = score.getScore();
-            int currentScore = (int) ctx.getState().getOrDefault(key, 0);
-            int cumulativeScore = incomingScore + currentScore;
-            cumulativeScore = (cumulativeScore > 10000) ? 0 : cumulativeScore;
-            ctx.getState().put(key, cumulativeScore);
+            String logMessage = String.format("Saving a score of %d for player %s to the database",
+                    score.getScore(),
+                    score.getPlayerId());
+            LOG.info(logMessage);
+            saveToDatabase(score);
         }
 
         public void cleanup() {}
     }
 
+    /**
+     * A logging sink that simply prints a formatted log message for each incoming score.
+     */
     private static class FormattedLogSink implements Sink<GameScore> {
         private static final long serialVersionUID = 1251089445039059977L;
         public void setup(Context context) {}
 
         public void put(GameScore score) {
             String logMessage = String.format("The current score for player %s is %d",
-                    score.getUserId(),
+                    score.getPlayerId(),
                     score.getScore());
             LOG.info(logMessage);
         }
@@ -85,23 +107,45 @@ public class StreamletCloneTopology {
         public void cleanup() {}
     }
 
+    /**
+     * All Heron topologies require a main function that defines the topology's behavior
+     * at runtime
+     */
     public static void main(String[] args) throws Exception {
         Builder processingGraphBuilder = Builder.createBuilder();
 
+        /**
+         * A supplier streamlet of random GameScore objects is cloned into two
+         * separate streamlets.
+         */
         List<Streamlet<GameScore>> splitGameScoreStreamlet = processingGraphBuilder
                 .newSource(GameScore::new)
                 .clone(2);
 
+        /**
+         * Elements in the first cloned streamlet go to the database sink.
+         */
         splitGameScoreStreamlet.get(0)
-                .toSink(new StateSink());
+                .toSink(new DatabaseSink());
 
+        /**
+         * Elements in the second cloned streamlet go to the logging sink.
+         */
         splitGameScoreStreamlet.get(1)
                 .toSink(new FormattedLogSink());
 
         Config config = new Config();
 
+        /**
+         * Fetches the topology name from the first command-line argument
+         */
         String topologyName = StreamletUtils.getTopologyName(args);
 
+        /**
+         * Finally, the processing graph and configuration are passed to the Runner,
+         * which converts the graph into a Heron topology that can be run in a Heron
+         * cluster.
+         */
         new Runner().run(topologyName, config, processingGraphBuilder);
     }
 }
