ecb3a357a46eea46df68026f4d50aa71ad425e6d
Huijun Wu <huijunw@twitter.com>
2017-11-16 22:08:10 -0800

    enhance heron-shell /killexecutor (#2557)
    * add killing process
    
    * update killpg to kill
    
    * address comment
    
    * address comment
    
    * update comment
    
    * update log

--- heron/shell/src/python/handlers/killexecutorhandler.py
+++ heron/shell/src/python/handlers/killexecutorhandler.py
@@ -28,16 +28,40 @@ class KillExecutorHandler(tornado.web.RequestHandler):
   @tornado.web.asynchronous
   def post(self):
     """ post method """
+    def status_finish(ret):
+      self.set_status(ret)
+      self.finish()
+
+    def kill_parent():
+      status_finish(200)
+      logger.info("Killing parent executor")
+      os.killpg(os.getppid(), signal.SIGTERM)
+
     logger = logging.getLogger(__file__)
-    logger.info("Received 'Killing parent executor' request")
+    logger.info("Received 'Killing process' request")
     data = dict(urlparse.parse_qsl(self.request.body))
+
+    # check shared secret
     sharedSecret = data.get('secret')
     if sharedSecret != options.secret:
-      self.set_status(403)
-      self.finish()
+      status_finish(403)
       return
-    logger.info("Killing parent executor response 200")
-    self.set_status(200)
-    self.finish()
-    logger.info("Killing parent executor")
-    os.killpg(os.getppid(), signal.SIGTERM)
+
+    instanceId = data.get('instance_id_to_restart')
+    if instanceId:
+      filepath = instanceId + '.pid'
+      if os.path.isfile(filepath): # instance_id found
+        if instanceId.startswith('heron-executor-'): # kill heron-executor
+          kill_parent()
+        else: # kill other normal instance
+          fh = open(filepath)
+          firstLine = int(fh.readline())
+          fh.close()
+          logger.info("Killing process " + instanceId + " " + str(firstLine))
+          os.kill(firstLine, signal.SIGTERM)
+          status_finish(200)
+      else: # instance_id not found
+        logger.info(filepath + " not found")
+        status_finish(422)
+    else: # instance_id not given, which means kill the container
+      kill_parent()
