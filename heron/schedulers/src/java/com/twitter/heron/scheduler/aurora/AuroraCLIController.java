db87694fbc33c80625494fe5dda4490c6ad2ac7b
Maosong Fu <mfu@twitter.com>
2017-11-21 19:24:46 -0800

    Make add containers verbose

--- heron/schedulers/src/java/com/twitter/heron/scheduler/aurora/AuroraCLIController.java
+++ heron/schedulers/src/java/com/twitter/heron/scheduler/aurora/AuroraCLIController.java
@@ -116,6 +116,11 @@ class AuroraCLIController implements AuroraController {
     //clone instance 0
     List<String> auroraCmd = new ArrayList<>(Arrays.asList(
         "aurora", "job", "add", "--wait-until", "RUNNING", jobSpec + "/0", count.toString()));
+
+    if (isVerbose) {
+      auroraCmd.add("--verbose");
+    }
+
     LOG.info(String.format("Requesting %s new aurora containers %s", count, auroraCmd));
     if (!runProcess(auroraCmd)) {
       throw new RuntimeException("Failed to create " + count + " new aurora instances");
