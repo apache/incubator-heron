f4cbbde46ea22d30a6b4ab6576ff438bd5f910fc
Eren Avsarogullari <erenavsarogullari@gmail.com>
2017-12-22 07:11:18 +0000

    [Streamlet] Reinforce usage of StreamletNamePrefixes for more type-safety (#2627)
    * [Streamlet] Reinforce using StreamletNamePrefixes for more type-safety
    
    * [Streamlet] Fix documentation issues
    
    * Review comment is addressed.

--- heron/api/src/java/com/twitter/heron/streamlet/impl/streamlets/SourceStreamlet.java
+++ heron/api/src/java/com/twitter/heron/streamlet/impl/streamlets/SourceStreamlet.java
@@ -36,7 +36,7 @@ public class SourceStreamlet<R> extends StreamletImpl<R> {
 
   @Override
   public boolean doBuild(TopologyBuilder bldr, Set<String> stageNames) {
-    setDefaultNameIfNone(StreamletNamePrefixes.SOURCE.toString(), stageNames);
+    setDefaultNameIfNone(StreamletNamePrefix.SOURCE, stageNames);
     bldr.setSpout(getName(), new ComplexSource<R>(generator), getNumPartitions());
     return true;
   }
