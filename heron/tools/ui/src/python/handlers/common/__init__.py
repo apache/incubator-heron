407ff8ea4d7e94c54f83fb4b33f7970d837bee7f
cckellogg <cckellogg@gmail.com>
2017-12-23 15:36:20 -0800

    Move the logical plan format to the tracker. (#2639)

--- heron/tools/ui/src/python/handlers/common/__init__.py
+++ heron/tools/ui/src/python/handlers/common/__init__.py
@@ -3,4 +3,3 @@ __all__ = ['common']
 
 from heron.tools.ui.src.python.handlers.common.consts import *
 from heron.tools.ui.src.python.handlers.common.utils import *
-from heron.tools.ui.src.python.handlers.common.graph import *
