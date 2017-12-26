3ca034534c9e206bc314fd3f8361ac1eec474aa9
Ning Wang <nwang@twitter.com>
2017-12-26 13:37:59 -0800

    Make heron dir to global variable to be customizable

--- heron/tools/common/src/python/utils/config.py
+++ heron/tools/common/src/python/utils/config.py
@@ -58,6 +58,12 @@ ENV_REQUIRED = "heron.config.is.env.required"
 ROLE_KEY = "role.required"
 ENVIRON_KEY = "env.required"
 
+# global variabls that can be overriden when necessary.
+# Note that overriden has to happen before the first call
+# to get_heron_dir()/get_zipped_heron_dir()
+config_heron_dir = ''
+config_zipped_heron_dir = ''
+
 def create_tar(tar_filename, files, config_dir, config_files):
   '''
   Create a tar file with a given set of files
@@ -137,6 +143,16 @@ def get_classpath(jars):
 
 def get_heron_dir():
   """
+    This will return heron_dir. If it is missing, extract from .pex file.
+  """
+  global config_heron_dir
+  if not config_heron_dir:
+    config_heron_dir = get_heron_dir_internal()
+  return config_heron_dir
+
+
+def get_heron_dir_internal():
+  """
   This will extract heron directory from .pex file.
 
   For example,
@@ -152,7 +168,18 @@ def get_heron_dir():
   path = "/".join(os.path.realpath(__file__).split('/')[:-go_above_dirs])
   return normalized_class_path(path)
 
+
 def get_zipped_heron_dir():
+  """
+    This will return zipped_heron_dir. If it is missing, extract from .pex file.
+  """
+  global config_zipped_heron_dir
+  if not config_zipped_heron_dir:
+    config_zipped_heron_dir = get_zipped_heron_dir_internal()
+  return config_zipped_heron_dir
+
+
+def get_zipped_heron_dir_internal():
   """
   This will extract heron directory from .pex file,
   with `zip_safe = False' Bazel flag added when building this .pex file
