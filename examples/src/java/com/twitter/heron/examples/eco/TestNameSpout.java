d13f69d05bef359949fa3eda3486f4d11c017e33
Josh Fischer <josh@joshfischer.io>
2018-01-08 16:51:08 -0600

    saving progress

---
+++ examples/src/java/com/twitter/heron/examples/eco/TestNameSpout.java
@@ -0,0 +1,81 @@
+package com.twitter.heron.examples.eco;
+//  Copyright 2017 Twitter. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import org.apache.storm.Config;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+
+@SuppressWarnings("serial")
+public class TestNameSpout extends BaseRichSpout {
+  public static Logger LOG = Logger.getLogger(TestNameSpout.class.getName());
+  boolean _isDistributed;
+  SpoutOutputCollector _collector;
+
+  public TestNameSpout() {
+    this(true);
+  }
+
+  public TestNameSpout(boolean isDistributed) {
+    _isDistributed = isDistributed;
+  }
+
+  public void open(Map<String, Object> conf, TopologyContext context, SpoutOutputCollector collector) {
+    _collector = collector;
+  }
+
+  public void close() {
+
+  }
+
+  public void nextTuple() {
+    Utils.sleep(100);
+    final String[] words = new String[] {"nathan", "mike", "jackson", "golda", "bertels"};
+    final Random rand = new Random();
+    final String word = words[rand.nextInt(words.length)];
+    _collector.emit(new Values(word));
+  }
+
+  public void ack(Object msgId) {
+
+  }
+
+  public void fail(Object msgId) {
+
+  }
+
+  public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    declarer.declare(new Fields("name"));
+  }
+
+  @Override
+  public Map<String, Object> getComponentConfiguration() {
+    if(!_isDistributed) {
+      Map<String, Object> ret = new HashMap<String, Object>();
+      ret.put(Config.TOPOLOGY_WORKERS, 1);
+      return ret;
+    } else {
+      return null;
+    }
+  }
+}
