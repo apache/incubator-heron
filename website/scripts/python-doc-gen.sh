a24440e6082695080f46a84b85e09aa7cfb21e6a
Josh Fischer <josh@joshfischer.io>
2018-01-29 12:35:02 -0600

    reverting pyton script changes

--- website/scripts/python-doc-gen.sh
+++ website/scripts/python-doc-gen.sh
@@ -5,8 +5,8 @@ HERON_ROOT_DIR=$(git rev-parse --show-toplevel)
 INPUT=heronpy
 TMP_DIR=$(mktemp -d)
 
-sudo -H pip install heronpy==0.17.1
-sudo -H pip install --ignore-installed six
+sudo pip install heronpy==${HERONPY_VERSION}
+sudo pip install --ignore-installed six
 
 mkdir -p static/api && rm -rf static/api/python
 
