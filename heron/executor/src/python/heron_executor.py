18f901a5416f658fe29e259325b7a7aac223d70c
Huijun Wu <huijunw@twitter.com>
2018-02-01 12:47:37 -0800

    algin metricscache and dhalion toggle switch

--- heron/executor/src/python/heron_executor.py
+++ heron/executor/src/python/heron_executor.py
@@ -509,9 +509,9 @@ class HeronExecutor(object):
         '--ckptmgr_port=%s' % str(self.checkpoint_manager_port)]
     retval["heron-tmaster"] = tmaster_cmd
 
-    retval["heron-metricscache"] = self._get_metrics_cache_cmd()
 
     if self.health_manager_mode.lower() != "disabled":
+      retval["heron-metricscache"] = self._get_metrics_cache_cmd()
       retval["heron-healthmgr"] = self._get_healthmgr_cmd()
 
     retval[self.metricsmgr_ids[0]] = self._get_metricsmgr_cmd(
