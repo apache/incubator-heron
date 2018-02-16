540e6c2a72a0d06bdc85a76a3667f43f3ae5d38e
Huijun Wu <huijunw@twitter.com>
2018-02-16 15:57:52 -0800

    update id

--- heron/scheduler-core/tests/java/com/twitter/heron/scheduler/UpdateTopologyManagerTest.java
+++ heron/scheduler-core/tests/java/com/twitter/heron/scheduler/UpdateTopologyManagerTest.java
@@ -155,9 +155,9 @@ public class UpdateTopologyManagerTest {
     IScalable mockScheduler = mock(IScalable.class);
     HashSet<PackingPlan.ContainerPlan> mockRetrunSet = new HashSet<>();
     mockRetrunSet.add(new PackingPlan.ContainerPlan(0, new HashSet<>(),
-        new Resource(1, ByteAmount.ZERO, ByteAmount.ZERO)));
+        new Resource(5, ByteAmount.ZERO, ByteAmount.ZERO)));
     mockRetrunSet.add(new PackingPlan.ContainerPlan(1, new HashSet<>(),
-        new Resource(1, ByteAmount.ZERO, ByteAmount.ZERO)));
+        new Resource(6, ByteAmount.ZERO, ByteAmount.ZERO)));
     when(mockScheduler.addContainers(any())).thenReturn(mockRetrunSet);
     UpdateTopologyManager spyUpdateManager =
         spyUpdateManager(mockStateMgr, mockScheduler, testTopology);
