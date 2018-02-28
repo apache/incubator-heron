0acd78ec9f4e0100c09d58b80a102b7d47bd9665
Jerry Peng <jerry.boyang.peng@gmail.com>
2018-02-28 13:38:49 -0800

    refactoring

--- heron/common/src/java/com/twitter/heron/common/basics/WakeableLooper.java
+++ heron/common/src/java/com/twitter/heron/common/basics/WakeableLooper.java
@@ -15,9 +15,10 @@
 package com.twitter.heron.common.basics;
 
 import java.time.Duration;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Logger;
 
 /**
  * A WakeableLooper is a class that could:
@@ -40,11 +41,11 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public abstract class WakeableLooper {
   // The tasks could only be added but not removed
-  private final CopyOnWriteArrayList<Runnable> tasksOnWakeup;
+  private final List<Runnable> tasksOnWakeup;
   private final PriorityQueue<TimerTask> timers;
 
   // The tasks would be invoked before exit
-  private final CopyOnWriteArrayList<Runnable> exitTasks;
+  private final List<Runnable> exitTasks;
 
   // For selector since there is bug in selector.select(timeout): we could not
   // use a timeout > 10 * Integer.MAX_VALUE
@@ -55,9 +56,9 @@ public abstract class WakeableLooper {
 
   public WakeableLooper() {
     exitLoop = false;
-    tasksOnWakeup = new CopyOnWriteArrayList<>();
+    tasksOnWakeup = new ArrayList<>();
     timers = new PriorityQueue<TimerTask>();
-    exitTasks = new CopyOnWriteArrayList<>();
+    exitTasks = new ArrayList<>();
   }
 
   public void clear() {
@@ -67,6 +68,7 @@ public abstract class WakeableLooper {
   }
 
   public void clearTasksOnWakeup() {
+    if (Thread.currentThread().getName().equals("SlaveThread")) LOG.info("clear");
     tasksOnWakeup.clear();
   }
 
@@ -92,16 +94,13 @@ public abstract class WakeableLooper {
   }
 
   private void onExit() {
-    // Using CopyOnWriteArrayList to prevent from tasks in exitTasks from clearing
-    // the exitTasks list and potentially causing IndexOutOfBoundsException.
-    // The method iterator() in CopyOnWriteArrayList return an iterator object that holds
-    // different copy of the elements, hence the term snapshot iterator. The snapshot iterator
-    // doesn’t allow modifying the list while traversing, and it will not throw
-    // ConcurrentModificationException if the list is being modified by other thread during
-    // the traversal, and the read and write operations work on different copies of elements.
-    Iterator<Runnable> it = exitTasks.iterator();
-    while(it.hasNext()) {
-      it.next().run();
+    int s= exitTasks.size();
+    for (int i = 0; i < s; i ++) {
+      // in case previous task cleared the list
+      if (exitTasks.size() <= i) {
+        break;
+      }
+      exitTasks.get(i).run();
     }
   }
 
@@ -158,17 +157,19 @@ public abstract class WakeableLooper {
     return nextTimeoutInterval;
   }
 
+  Logger LOG = Logger.getLogger(WakeableLooper.class.getName());
   private void executeTasksOnWakeup() {
-    // Using CopyOnWriteArrayList to prevent from tasks in tasksOnWakeup from clearing
-    // the tasksOnWakeup list and potentially causing IndexOutOfBoundsException.
-    // The method iterator() in CopyOnWriteArrayList return an iterator object that holds
-    // different copy of the elements, hence the term snapshot iterator. The snapshot iterator
-    // doesn’t allow modifying the list while traversing, and it will not throw
-    // ConcurrentModificationException if the list is being modified by other thread during
-    // the traversal, and the read and write operations work on different copies of elements.
-    Iterator<Runnable> it = tasksOnWakeup.iterator();
-    while(it.hasNext()) {
-      it.next().run();
+    // Be careful here we could not use iterator, since it is possible that we may
+    // add some items into this list during the iteration, which may cause
+    // ConcurrentModificationException
+    // We pre-get the size to avoid execute the tasks added during execution
+    int s = tasksOnWakeup.size();
+    for (int i = 0; i < s; i++) {
+      // in case previous task cleared the list
+      if (tasksOnWakeup.size() <= i){
+        break;
+      }
+      tasksOnWakeup.get(i).run();
     }
   }
 
