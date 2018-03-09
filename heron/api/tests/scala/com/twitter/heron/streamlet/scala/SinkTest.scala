5b5cabc4de2a46fa0f741365b0626dbb09752b2d
Eren Avsarogullari <erenavsarogullari@gmail.com>
2018-03-09 16:11:54 +0000

    [Streamlet Scala API] Add Scala StreamletImpl Support - Part II (#2761)
    * Add Scala StreamletImpl Support Part 2
    
    * Review comments are addressed.
    
    * Review comments are addressed.

--- heron/api/tests/scala/com/twitter/heron/streamlet/scala/SinkTest.scala
+++ heron/api/tests/scala/com/twitter/heron/streamlet/scala/SinkTest.scala
@@ -15,8 +15,11 @@ package com.twitter.heron.streamlet.scala
 
 import scala.collection.mutable.ListBuffer
 
-import com.twitter.heron.streamlet.Context
-import com.twitter.heron.streamlet.scala.common.{BaseFunSuite, TestContext}
+import com.twitter.heron.streamlet.scala.common.{
+  BaseFunSuite,
+  TestContext,
+  TestListBufferSink
+}
 
 /**
   * Sink is how Streamlet's end. This class covers unit tests of Sink functionality
@@ -26,14 +29,14 @@ class SinkTest extends BaseFunSuite {
 
   test("Sink should support setup") {
     val list = ListBuffer[Int]()
-    val sink = new TestSink(list)
+    val sink = new TestListBufferSink(list)
     sink.setup(new TestContext())
     assert(list == List(1, 2))
   }
 
   test("Sink should put data") {
     val list = ListBuffer[Int]()
-    val sink = new TestSink(list)
+    val sink = new TestListBufferSink(list)
     sink.setup(new TestContext())
     sink.put(3)
     assert(list == List(1, 2, 3))
@@ -41,25 +44,11 @@ class SinkTest extends BaseFunSuite {
 
   test("Sink should support cleanup") {
     val list = ListBuffer[Int]()
-    val sink = new TestSink(list)
+    val sink = new TestListBufferSink(list)
     sink.setup(new TestContext())
     sink.put(3)
     sink.cleanup()
     assert(list.isEmpty)
-  }
-
-  private class TestSink(numbers: ListBuffer[Int]) extends Sink[Int] {
-
-    override def setup(context: Context): Unit = {
-      numbers += (1, 2)
-    }
-
-    override def put(tuple: Int): Unit = {
-      numbers += tuple
-    }
-
-    override def cleanup(): Unit = numbers.clear()
-
   }
 
 }
