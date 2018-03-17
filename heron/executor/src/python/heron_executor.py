755c63ffea223c5171ae6c4bfb2d3cb5cfb04b9f
Boyang Jerry Peng <jerry.boyang.peng@gmail.com>
2018-03-16 17:34:56 -0700

    Correct print errors

--- heron/executor/src/python/heron_executor.py
+++ heron/executor/src/python/heron_executor.py
@@ -830,7 +830,7 @@ class HeronExecutor(object):
 
       proc.async_stream_process_stdout(process, stdout_log_fn(name))
     except Exception:
-      Log.info("Exception running command %:", cmd)
+      Log.info("Exception running command %s", cmd)
       traceback.print_exc()
 
     return process
@@ -846,7 +846,7 @@ class HeronExecutor(object):
       # wait for termination
       self._wait_process_std_out_err(cmd, process)
     except Exception:
-      Log.info("Exception running command %:", cmd)
+      Log.info("Exception running command %s", cmd)
       traceback.print_exc()
 
     # return the exit code
