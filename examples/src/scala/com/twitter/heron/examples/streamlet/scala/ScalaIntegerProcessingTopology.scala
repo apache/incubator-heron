03a32096c7c965162d66042be102af928ae14de2
Eren Avsarogullari <erenavsarogullari@gmail.com>
2018-03-19 22:09:15 +0000

    Review comments are addressed.

--- examples/src/scala/com/twitter/heron/examples/streamlet/scala/ScalaIntegerProcessingTopology.scala
+++ examples/src/scala/com/twitter/heron/examples/streamlet/scala/ScalaIntegerProcessingTopology.scala
@@ -11,14 +11,6 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-
-//  http://www.apache.org/licenses/LICENSE-2.0
-
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
 package com.twitter.heron.examples.streamlet.scala
 
 import java.util.concurrent.ThreadLocalRandom
@@ -26,7 +18,6 @@ import java.util.concurrent.ThreadLocalRandom
 import org.apache.commons.lang3.StringUtils
 
 import com.twitter.heron.streamlet.Config
-
 import com.twitter.heron.streamlet.scala.{Builder, Runner}
 
 /**
