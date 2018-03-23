53cdc3e54eaf50245c3ed68456d1215a8089fe59
Karthik Ramasamy <karthik@streaml.io>
2018-03-23 13:04:53 -0700

    Do not include scribe sink when compiling on non x86 platforms. (#2801)
    * Do not include scribe sink when compiling on non x86 platforms.
    This is due to scribe dependency on thrift which needs to be
    generate java files - the binaries used are only x86 platforms.
    
    * reverted the change tools/platform - not relevant for this PR
    
    * updated the docker rules version

--- heron/metricsmgr/src/java/com/twitter/heron/metricsmgr/sink/scribe/ScribeSink.java
+++ heron/metricsmgr/src/java/com/twitter/heron/metricsmgr/sink/scribe/ScribeSink.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.twitter.heron.metricsmgr.sink;
+package com.twitter.heron.metricsmgr.sink.scribe;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
