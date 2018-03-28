3a51ee37de100e8e869648c2fd65fd416373bb55
Eren Avsarogullari <erenavsarogullari@gmail.com>
2018-03-28 19:00:50 +0100

    [Streamlet Scala API] Add Scala Example Support - Part II (#2810)
    * Add Scala Streamlet Examples - Part II
    
    * Review comments are addressed.

--- examples/src/scala/com/twitter/heron/examples/streamlet/scala/ScalaIntegerProcessingTopology.scala
+++ examples/src/scala/com/twitter/heron/examples/streamlet/scala/ScalaIntegerProcessingTopology.scala
@@ -15,8 +15,7 @@ package com.twitter.heron.examples.streamlet.scala
 
 import java.util.concurrent.ThreadLocalRandom
 
-import org.apache.commons.lang3.StringUtils
-
+import com.twitter.heron.examples.streamlet.scala.common.ScalaTopologyExampleUtils
 import com.twitter.heron.streamlet.Config
 import com.twitter.heron.streamlet.scala.{Builder, Runner}
 
@@ -42,9 +41,11 @@ object ScalaIntegerProcessingTopology {
 
     val zeroes = builder.newSource(() => 0).setName("zeroes")
 
-    builder
+    val numbers = builder
       .newSource(() => ThreadLocalRandom.current.nextInt(1, 11))
       .setName("random-numbers")
+
+    numbers
       .map[Int]((i: Int) => i * 10)
       .setNumPartitions(2)
       .filter((i: Int) => i >= 50)
@@ -60,23 +61,11 @@ object ScalaIntegerProcessingTopology {
       .build
 
     // Fetches the topology name from the first command-line argument
-    val topologyName = getTopologyName(args)
+    val topologyName = ScalaTopologyExampleUtils.getTopologyName(args)
 
     // Finally, the processing graph and configuration are passed to the Runner, which converts
     // the graph into a Heron topology that can be run in a Heron cluster.
     new Runner().run(topologyName, config, builder)
   }
 
-  /**
-    * Fetches the topology's name from the first command-line argument or
-    * throws an exception if not present.
-    */
-  private def getTopologyName(args: Array[String]) = {
-    require(args.length > 0, "A Topology name should be supplied")
-    require(StringUtils.isNotBlank(args(0)), "Topology name must not be blank")
-    args(0)
-  }
-
 }
-
-final class ScalaIntegerProcessingTopology private () {}
