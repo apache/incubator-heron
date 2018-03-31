f3c97fa2bc21dcc7cc1bbb890f1baf7bb4fb5003
Eren Avsarogullari <erenavsarogullari@gmail.com>
2018-03-31 20:27:20 +0100

    Fix Scala Streamlet Integration Test Result Problem

--- integration_test/src/scala/com/twitter/heron/integration_test/topology/scala_streamlet_with_filter_and_transform/ScalaStreamletWithFilterAndTransform.scala
+++ integration_test/src/scala/com/twitter/heron/integration_test/topology/scala_streamlet_with_filter_and_transform/ScalaStreamletWithFilterAndTransform.scala
@@ -57,15 +57,15 @@ class ScalaStreamletWithFilterAndTransform(args: Array[String])
     build(testTopologyBuilder, streamletBuilder)
   }
 
-  private class TextTransformer extends SerializableTransformer[Int, String] {
-    private val alphabet = List("a", "b", "c", "d", "e", "f", "g", "h")
+}
 
-    override def setup(context: Context): Unit = {}
+private class TextTransformer extends SerializableTransformer[Int, String] {
+  private val alphabet = List("a", "b", "c", "d", "e", "f", "g", "h")
 
-    override def transform(i: Int, fun: String => Unit): Unit =
-      fun(s"${alphabet(i)}-$i".toUpperCase)
+  override def setup(context: Context): Unit = {}
 
-    override def cleanup(): Unit = {}
-  }
+  override def transform(i: Int, fun: String => Unit): Unit =
+    fun(s"${alphabet(i)}-$i".toUpperCase)
 
+  override def cleanup(): Unit = {}
 }
