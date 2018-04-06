b446a874275105e7cfbb26e5cf1a859966914155
Huijun Wu <huijunw@twitter.com>
2018-04-06 13:28:26 -0700

    fix unit

--- heron/executor/tests/python/heron_executor_unittest.py
+++ heron/executor/tests/python/heron_executor_unittest.py
@@ -131,7 +131,9 @@ class HeronExecutorTest(unittest.TestCase):
              "-Xloggc:log-files/gc.healthmgr.log -Djava.net.preferIPv4Stack=true " \
              "-cp scheduler_classpath:healthmgr_classpath " \
              "com.twitter.heron.healthmgr.HealthManager --cluster cluster --role role " \
-             "--environment environ --topology_name topname --verbose"
+             "--environment environ --topology_name topname --metricsmgr_port metricsmgr_port " \
+             "--system_config_file %s --override_config_file %s --verbose" %\
+             (INTERNAL_CONF_PATH, OVERRIDE_PATH)
 
   def get_expected_instance_command(component_name, instance_id, container_id):
     instance_name = "container_%d_%s_%d" % (container_id, component_name, instance_id)
