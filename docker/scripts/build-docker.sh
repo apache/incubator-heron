29d93e511bf047511c3d0e214123a15264163394
Josh Fischer <josh@joshfischer.io>
2018-07-17 16:20:50 -0500

    (Ok To Merge) updating docker scripts (#2963)
    * updating ubuntu14.04 docker scripts
    
    * clean up
    
    * updating other distros
    
    * adding whitespace

--- docker/scripts/build-docker.sh
+++ docker/scripts/build-docker.sh
@@ -20,6 +20,7 @@ trap cleanup EXIT
 
 setup_scratch_dir() {
   if [ ! -f "$1" ]; then
+
     mkdir $1
     mkdir $1/artifacts
   fi
@@ -38,30 +39,25 @@ run_build() {
   setup_scratch_dir $SCRATCH_DIR
 
   # need to copy artifacts locally
-  TOOLS_FILE="$OUTPUT_DIRECTORY/heron-tools-install-$HERON_VERSION-$TARGET_PLATFORM.sh"
-  TOOLS_OUT_FILE="$SCRATCH_DIR/artifacts/heron-tools-install.sh"
-
-  CLIENT_FILE="$OUTPUT_DIRECTORY/heron-client-install-$HERON_VERSION-$TARGET_PLATFORM.sh"
-  CLIENT_OUT_FILE="$SCRATCH_DIR/artifacts/heron-client-install.sh"
-
   CORE_FILE="$OUTPUT_DIRECTORY/heron-core-$HERON_VERSION-$TARGET_PLATFORM.tar.gz"
   CORE_OUT_FILE="$SCRATCH_DIR/artifacts/heron-core.tar.gz"
 
-  cp $TOOLS_FILE $TOOLS_OUT_FILE
-  cp $CORE_FILE $CORE_OUT_FILE
+  ALL_FILE="$OUTPUT_DIRECTORY/heron-install.sh"
+  ALL_OUT_FILE="$SCRATCH_DIR/artifacts/heron-install.sh"
 
+  cp $CORE_FILE $CORE_OUT_FILE
+  cp $ALL_FILE $ALL_OUT_FILE
   export HERON_VERSION
 
   echo "Building docker image with tag:$DOCKER_TAG"
   docker build --squash -t "$DOCKER_TAG" -t "$DOCKER_LATEST_TAG" -f "$DOCKER_FILE" "$SCRATCH_DIR"
-
   # save the image as a tar file
   DOCKER_IMAGE_FILE="$OUTPUT_DIRECTORY/heron-docker-$HERON_VERSION-$TARGET_PLATFORM.tar"
 
   echo "Saving docker image to $DOCKER_IMAGE_FILE"
   docker save -o $DOCKER_IMAGE_FILE $DOCKER_TAG
   gzip $DOCKER_IMAGE_FILE
-}
+} 
 
 case $# in
   3)
