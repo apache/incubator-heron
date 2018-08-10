03fe95586ab1941f7eb22698ad18e732be0756b7
Faria Kalim <faria.kalim@gmail.com>
2018-08-10 16:47:11 -0700

    corrected variable in update.py

--- heron/tools/cli/src/python/update.py
+++ heron/tools/cli/src/python/update.py
@@ -141,7 +141,7 @@ def convert_args_dict_to_list(dict_extra_args):
     list_extra_args += ["--runtime_config",
                         ','.join(dict_extra_args['runtime_config'])]
   if 'container_number' in dict_extra_args:
-    list_extra_args += ["--container-number",
+    list_extra_args += ["--container_number",
                         ','.join(dict_extra_args['container_number'])]
   if 'dry_run' in dict_extra_args and dict_extra_args['dry_run']:
     list_extra_args += ['--dry_run']
