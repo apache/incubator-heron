3c4ec25dc464ed3bc4dfcf7df78346dc38efec71
Ning Wang <nwang@twitter.com>
2018-08-22 10:02:17 -0700

    Move license header in react dom components (#3003)
    * Move license header in react dom components
    
    * remove empty line at beginning

--- heron/tools/ui/resources/static/js/config.js
+++ heron/tools/ui/resources/static/js/config.js
@@ -1,3 +1,5 @@
+/** @jsx React.DOM */
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,8 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-/** @jsx React.DOM */
 
 var ConfigTable = React.createClass({
   getInitialState: function() {
