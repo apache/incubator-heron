d03adc55b46dcf9c8c5e9e2639b0fd90ab940d36
thinker0 <thinker0@gmail.com>
2018-11-21 23:45:52 +0900

    check LineLength

--- storm-compatibility/src/java/org/apache/storm/utils/Time.java
+++ storm-compatibility/src/java/org/apache/storm/utils/Time.java
@@ -1,13 +1,20 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.  The ASF licenses this file to you under the Apache License, Version
- * 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
- * and limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.storm.utils;
@@ -22,9 +29,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * This class implements time simulation support. When time simulation is enabled, methods on this class will use fixed time. When time
- * simulation is disabled, methods will pass through to relevant java.lang.System/java.lang.Thread calls. Methods using units higher than
- * nanoseconds will pass through to System.currentTimeMillis(). Methods supporting nanoseconds will pass through to System.nanoTime().
+ * This class implements time simulation support. When time simulation is enabled,
+ * methods on this class will use fixed time. When time simulation is disabled,
+ * methods will pass through to relevant java.lang.System/java.lang.Thread calls.
+ * Methods using units higher than nanoseconds will pass through to System.currentTimeMillis().
+ * Methods supporting nanoseconds will pass through to System.nanoTime().
  */
 public class Time {
   private static final Logger LOG = Logger.getLogger(Time.class.getName());
@@ -66,7 +75,8 @@ public class Time {
     try {
       synchronized (SLEEP_TIMES_LOCK) {
         if (!SIMULATING.get()) {
-          LOG.log(Level.FINER, Thread.currentThread() + " is still sleeping after simulated time disabled.",
+          LOG.log(Level.FINER, Thread.currentThread()
+                  + " is still sleeping after simulated time disabled.",
               new RuntimeException("STACK TRACE"));
           throw new InterruptedException();
         }
@@ -75,7 +85,8 @@ public class Time {
       while (SIMULATED_CURR_TIME_NANOS.get() < targetTimeNanos) {
         synchronized (SLEEP_TIMES_LOCK) {
           if (!SIMULATING.get()) {
-            LOG.log(Level.FINER, Thread.currentThread() + " is still sleeping after simulated time disabled.",
+            LOG.log(Level.FINER, Thread.currentThread()
+                    + " is still sleeping after simulated time disabled.",
                 new RuntimeException("STACK TRACE"));
             throw new InterruptedException();
           }
