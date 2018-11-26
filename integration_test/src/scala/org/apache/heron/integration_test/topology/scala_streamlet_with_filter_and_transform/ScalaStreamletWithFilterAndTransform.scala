4d1ab84eea99a374c6838cb9401d6a7fc908af24
Eren Avsarogullari <erenavsarogullari@gmail.com>
2018-11-26 22:50:27 +0000

    [Scala Streamlet API] Add Integration Test for Source and Union Operations (#3121)
    * [Scala Streamlet API] Add New Integration Test
    
    * Documentation is added

--- integration_test/src/scala/org/apache/heron/integration_test/topology/scala_streamlet_with_filter_and_transform/ScalaStreamletWithFilterAndTransform.scala
+++ integration_test/src/scala/org/apache/heron/integration_test/topology/scala_streamlet_with_filter_and_transform/ScalaStreamletWithFilterAndTransform.scala
@@ -1,21 +1,21 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+  * Licensed to the Apache Software Foundation (ASF) under one
+  * or more contributor license agreements.  See the NOTICE file
+  * distributed with this work for additional information
+  * regarding copyright ownership.  The ASF licenses this file
+  * to you under the Apache License, Version 2.0 (the
+  * "License"); you may not use this file except in compliance
+  * with the License.  You may obtain a copy of the License at
+  *
+  *   http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing,
+  * software distributed under the License is distributed on an
+  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  * KIND, either express or implied.  See the License for the
+  * specific language governing permissions and limitations
+  * under the License.
+  */
 package org.apache.heron.integration_test.topology.scala_streamlet_with_filter_and_transform
 
 import java.util.concurrent.atomic.AtomicInteger
@@ -28,9 +28,7 @@ import org.apache.heron.integration_test.common.{
   AbstractTestTopology,
   ScalaIntegrationTestBase
 }
-import org.apache.heron.streamlet.scala.{
-  Builder, SerializableTransformer
-}
+import org.apache.heron.streamlet.scala.{Builder, SerializableTransformer}
 
 object ScalaStreamletWithFilterAndTransform {
   def main(args: Array[String]): Unit = {
@@ -41,7 +39,7 @@ object ScalaStreamletWithFilterAndTransform {
 }
 
 /**
-  * Scala Streamlet Integration Test
+  * Scala Streamlet Integration Test by covering source, filter, transform operations.
   */
 class ScalaStreamletWithFilterAndTransform(args: Array[String])
     extends AbstractTestTopology(args)
