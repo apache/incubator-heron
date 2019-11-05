dba556acb4267ef7749458899fd2b15bf4af34b2
Josh Fischer <josh@joshfischer.io>
2019-11-04 20:27:25 -0600

    clean up

--- website2/website/scripts/compile-docker.sh
+++ website2/website/scripts/compile-docker.sh
@@ -48,8 +48,7 @@ TARGET_PLATFORM="ubuntu18.04"
 DOCKER_FILE=$(dockerfile_path_for_platform $TARGET_PLATFORM)
 verify_dockerfile_exists $DOCKER_FILE
 copy_bazel_rc_to  $PROJECT_DIR/website/scripts/bazelrc
-rm -rf ~/heron-static-site
-mkdir ~/heron-static-site
+
 echo "docker file"
 echo $DOCKER_FILE
 
@@ -62,21 +61,3 @@ docker run \
   -v $PROJECT_DIR/..:/dist \
   -t heron-compiler:$TARGET_PLATFORM  make -C /dist/website2/website/ buildsite
 
-
-# make -C /dist/website2/website/Makefile buildsite
-#docker run \
-#    --rm \
-#    -e TARGET_PLATFORM=$TARGET_PLATFORM \
-#    -e SCRATCH_DIR="/scratch" \
-#    -e SOURCE_TARBALL="/src.tar.gz" \
-#    -e OUTPUT_DIRECTORY="/dist" \
-#    -e HERON_VERSION=$HERON_VERSION \
-#    -e HERON_GIT_REV="${HERON_GIT_REV}" \
-#    -e HERON_BUILD_VERSION="${HERON_BUILD_VERSION}" \
-#    -e HERON_BUILD_HOST="${HERON_BUILD_HOST}" \
-#    -e HERON_BUILD_USER="${HERON_BUILD_USER}" \
-#    -e HERON_BUILD_TIME="${HERON_BUILD_TIME}" \
-#    -e HERON_TREE_STATUS="${HERON_TREE_STATUS}" \
-#    -v "$SOURCE_TARBALL:/src.tar.gz:ro" \
-#    -v "$OUTPUT_DIRECTORY:/dist" \
-#    -t heron-compiler:$TARGET_PLATFORM /compile-platform.sh
