27179acd3676a86b5c1fceec6f5b599ce60f3357
Josh Fischer <josh@joshfischer.io>
2019-12-10 07:08:28 -0600

    Joshfischer/apache content (#3419)
    * adding apache content to footer
    
    * add version for 0.20.2-incubating
    
    * adding back branch whoami block

--- website2/website/core/Footer.js
+++ website2/website/core/Footer.js
@@ -24,17 +24,29 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer" id="footer">
-    
-        <div className="apache-disclaimer">
-Disclaimer
-Apache Heron (incubating) is an effort undergoing incubation at The Apache Software Foundation (ASF), 
-sponsored by Incubator. Incubation is required of all newly accepted projects until a further review indicates 
-that the infrastructure, communications, and decision making process have stabilized in a manner consistent with 
-other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, 
-it does indicate that the project has yet to be fully endorsed by the ASF.
 
-</div>
-        <section className="copyright">{this.props.config.copyright}</section>
+        <div className="apache-disclaimer">
+          Apache Heron is an effort undergoing incubation at <a target="_blank" href="https://apache.org/">The Apache Software Foundation (ASF)</a> sponsored by the Apache Incubator PMC.
+          Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects.
+          While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.
+          <br></br>
+          <br></br>
+          Apache®, the names of Apache projects, and the feather logo are either <a rel="external" href="https://www.apache.org/foundation/marks/list/">registered trademarks or trademarks</a> of the Apache Software Foundation in the United States and/or other countries.
+          <br></br>
+          <br></br>
+          <div className="copyright-box">{this.props.config.copyright}</div>
+
+        </div>
+        <div className="apache-links">
+        <a class="item" rel="external" href="https://incubator.apache.org/">Apache Incubator</a>
+            <div><a class="item" rel="external" href="https://www.apache.org/">About the ASF</a></div>
+            <div><a class="item" rel="external" href="https://www.apache.org/events/current-event">Events</a></div>
+            <div><a class="item" rel="external" href="https://www.apache.org/foundation/thanks.html">Thanks</a></div>
+            <div><a class="item" rel="external" href="https://www.apache.org/foundation/sponsorship.html">Become a Sponsor</a></div>
+            <div><a class="item" rel="external" href="https://www.apache.org/security/">Security</a></div>
+            <div><a class="item" rel="external" href="https://www.apache.org/licenses/">License</a></div>
+        </div>
+
       </footer>
     );
   }
