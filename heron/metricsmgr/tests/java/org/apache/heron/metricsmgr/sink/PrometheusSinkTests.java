b34a76b5a69c0caa83d724fca6b7ab4c117beda9
choi se <thinker0@gmail.com>
2020-04-02 14:14:28 +0900

    Support custom metrics rules for PrometheusSink (#3493)
    * Support Java 11
    
    * config travis to use oracle jdk 11
    
    * Java 11 support (#3399)
    
    * Support Java 11
    
    * config travis to use oracle jdk 11
    
    * Add check jdk version
    
    * Fix command arguments.
    
    Change insert gc_options
    
    Update list
    
    Fix gc-logging
    
    * Add missing parameter
    
    * typo
    
    * Add pause time
    
    * wip
    
    * Support jmx_exporter format configuration.
    
    * Fix checkstyle
    
    * Remove unused
    
    * Java 11 support (#3399)
    
    * Support Java 11
    
    * config travis to use oracle jdk 11
    
    * Add check jdk version
    
    * Fix command arguments.
    
    Change insert gc_options
    
    Update list
    
    Fix gc-logging
    
    * wip
    
    * Support jmx_exporter format configuration.
    
    * Fix checkstyle
    
    * Remove unused
    
    * Update kafkaOffset metrics
    
    * Add Rules
    
    * Make log/sink/consume Streamlet component support setName and setNumPartitions (#3459)
    
    * Patch to fix cppcheck with newer glibc (#3471)
    
    * Add documents for setting up a docker based development environment (#3475)
    
    * Improve concurrency for needed parts. (#3107)
    
    * Change concurrent Map
    
    * Change concurrent Map
    
    * HashMap changes for unneeded parts.
    
    * HashMap changes for unneeded parts.
    
    * Review changes
    
    * Changes HashMap for unneeded parts.
    
    * Improve concurrency for needed parts.
    
    * Remove unused imports.
    
    * Remove unused imports.
    
    * Remove unused imports.
    
    * Fix NPE
    
    (cherry picked from commit 545d3814b315c29d3e396309a2ededaad193ec32)
    
    * Fix WhitespaceAround
    
    * Add dummy Object
    
    * Fix ConstantName
    
    (cherry picked from commit 8d6d5067072e92d6e276f93e18297ddedc647c6c)
    
    * Update kafkaOffset metrics
    
    * Add Rules
    
    * Update line is longer than 100 characters
    
    * Update line is longer than 100 characters
    
    * Add attrNameSnakeCase or other metrics fix
    
    * fix checkstyle
    
    Co-authored-by: Ning Wang <wangninggm@gmail.com>
    Co-authored-by: Ning Wang <nwang@twitter.com>
    Co-authored-by: Nicholas Nezis <nicholas.nezis@gmail.com>

--- heron/metricsmgr/tests/java/org/apache/heron/metricsmgr/sink/PrometheusSinkTests.java
+++ heron/metricsmgr/tests/java/org/apache/heron/metricsmgr/sink/PrometheusSinkTests.java
@@ -28,6 +28,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -81,6 +84,45 @@ public class PrometheusSinkTests {
     Mockito.when(context.getTopologyName()).thenReturn("testTopology");
     Mockito.when(context.getSinkId()).thenReturn("testId");
 
+    /*
+    # example: metrics.yaml
+    rules:
+      - pattern: kafka.(\w+)<type=(.+), name=(.+)PerSec\w*, (.+)=(.+)><>Count
+        name: kafka_$1_$2_$3_total
+        attrNameSnakeCase: true
+        type: COUNTER
+        labels:
+          "$4": "$5"
+        type: COUNTER
+    */
+    /*
+    example: metrics
+      kafkaOffset/nginx-lfp-beacon/totalSpoutLag
+      kafkaOffset/lads_event_meta_backfill_data/partition_10/spoutLag
+     */
+    List<Map<String, Object>> rules = Lists.newArrayList();
+    defaultConf.put("rules", rules);
+    Map<String, Object> rule1 = Maps.newHashMap();
+    Map<String, Object> labels1 = Maps.newHashMap();
+    rules.add(rule1);
+    rule1.put("pattern", "kafkaOffset/(.+)/(.+)");
+    rule1.put("name", "kafka_offset_$2");
+    rule1.put("type", "COUNTER");
+    rule1.put("attrNameSnakeCase", true);
+    rule1.put("labels", labels1);
+    labels1.put("topic", "$1");
+
+    Map<String, Object> rule2 = Maps.newHashMap();
+    Map<String, Object> labels2 = Maps.newHashMap();
+    rules.add(rule2);
+    rule2.put("pattern", "kafkaOffset/(.+)/partition_(\\d+)/(.+)");
+    rule2.put("name", "kafka_offset_partition_$3");
+    rule2.put("type", "COUNTER");
+    rule2.put("labels", labels2);
+    rule2.put("attrNameSnakeCase", true);
+    labels2.put("topic", "$1");
+    labels2.put("partition", "$2");
+
     Iterable<MetricsInfo> infos = Arrays.asList(new MetricsInfo("metric_1", "1.0"),
         new MetricsInfo("metric_2", "2.0"));
 
@@ -133,7 +175,9 @@ public class PrometheusSinkTests {
   public void testResponseWhenMetricNamesHaveAnInstanceId() throws IOException {
     Iterable<MetricsInfo> infos = Arrays.asList(
         new MetricsInfo("__connection_buffer_by_instanceid/container_1_word_5/packets", "1.0"),
-        new MetricsInfo("__time_spent_back_pressure_by_compid/container_1_exclaim1_1", "1.0")
+        new MetricsInfo("__time_spent_back_pressure_by_compid/container_1_exclaim1_1", "1.0"),
+        new MetricsInfo("__client_stmgr-92/__ack_tuples_to_stmgrs", "1.0"),
+        new MetricsInfo("__instance_bytes_received/1", "1.0")
     );
 
     records = Arrays.asList(
@@ -154,7 +198,59 @@ public class PrometheusSinkTests {
             "container_1_word_5", "1.0"),
         createMetric(topology, "__stmgr__", "stmgr-1",
             "time_spent_back_pressure_by_compid",
-            "container_1_exclaim1_1", "1.0")
+            "container_1_exclaim1_1", "1.0"),
+        createMetric(topology, "__stmgr__", "stmgr-1",
+            "client_stmgr_ack_tuples_to_stmgrs", "stmgr-92", "1.0"),
+        createMetric(topology, "__stmgr__", "stmgr-1",
+            "instance_bytes_received", "1", "1.0")
+    );
+
+    final Set<String> generatedLines =
+        new HashSet<>(Arrays.asList(new String(sink.generateResponse()).split("\n")));
+
+    assertEquals(expectedLines.size(), generatedLines.size());
+
+    expectedLines.forEach((String line) -> {
+      assertTrue(generatedLines.contains(line));
+    });
+  }
+
+  @Test
+  public void testApacheStormKafkaMetrics() throws IOException {
+    Iterable<MetricsInfo> infos = Arrays.asList(
+        new MetricsInfo("kafkaOffset/event_data/partition_0/spoutLag", "1.0"),
+        new MetricsInfo("kafkaOffset/event_data/partition_10/spoutLag", "1.0"),
+        new MetricsInfo("kafkaOffset/event_data/partition_0/earliestTimeOffset", "1.0"),
+        new MetricsInfo("kafkaOffset/event_data/totalRecordsInPartitions", "1.0"),
+        new MetricsInfo("kafkaOffset/event_data/totalSpoutLag", "1.0"),
+        new MetricsInfo("kafkaOffset/event_data/partition_2/spoutLag", "1.0")
+    );
+
+    records = Arrays.asList(
+        newRecord("shared-aurora-036:31/spout-release-1/container_1_spout-release-1_31",
+            infos, Collections.emptyList())
+    );
+    PrometheusTestSink sink = new PrometheusTestSink();
+    sink.init(defaultConf, context);
+    for (MetricsRecord r : records) {
+      sink.processRecord(r);
+    }
+
+    final String topology = "testTopology";
+
+    final List<String> expectedLines = Arrays.asList(
+        createOffsetMetric(topology, "spout-release-1", "container_1_spout-release-1_31",
+            "kafka_offset_partition_spout_lag", "event_data", "0", "1.0"),
+        createOffsetMetric(topology, "spout-release-1", "container_1_spout-release-1_31",
+            "kafka_offset_partition_spout_lag", "event_data", "10", "1.0"),
+        createOffsetMetric(topology, "spout-release-1", "container_1_spout-release-1_31",
+            "kafka_offset_partition_earliest_time_offset", "event_data", "0", "1.0"),
+        createOffsetMetric(topology, "spout-release-1", "container_1_spout-release-1_31",
+            "kafka_offset_total_records_in_partitions", "event_data", null, "1.0"),
+        createOffsetMetric(topology, "spout-release-1", "container_1_spout-release-1_31",
+            "kafka_offset_total_spout_lag", "event_data", null, "1.0"),
+        createOffsetMetric(topology, "spout-release-1", "container_1_spout-release-1_31",
+            "kafka_offset_partition_spout_lag", "event_data", "2", "1.0")
     );
 
     final Set<String> generatedLines =
@@ -195,12 +291,28 @@ public class PrometheusSinkTests {
 
     if (metricNameInstanceId != null) {
       return String.format("heron_%s"
-              + "{topology=\"%s\",component=\"%s\",instance_id=\"%s\",metric_instance_id=\"%s\"}"
+              + "{component=\"%s\",instance_id=\"%s\",metric_instance_id=\"%s\",topology=\"%s\"}"
               + " %s %d",
-          metric, topology, component, instance, metricNameInstanceId, value, NOW);
+          metric, component, instance, metricNameInstanceId, topology, value, NOW);
     } else {
-      return String.format("heron_%s{topology=\"%s\",component=\"%s\",instance_id=\"%s\"} %s %d",
-          metric, topology, component, instance, value, NOW);
+      return String.format("heron_%s{component=\"%s\",instance_id=\"%s\",topology=\"%s\"} %s %d",
+          metric, component, instance, topology, value, NOW);
+    }
+  }
+
+  private String createOffsetMetric(String topology, String component, String instance,
+                              String metric, String topic, String partition, String value) {
+
+    if (partition != null) {
+      return String.format("heron_%s"
+              + "{component=\"%s\",instance_id=\"%s\",partition=\"%s\","
+              + "topic=\"%s\",topology=\"%s\"}"
+              + " %s %d",
+          metric, component, instance, partition, topic, topology, value, NOW);
+    } else {
+      return String.format("heron_%s"
+              + "{component=\"%s\",instance_id=\"%s\",topic=\"%s\",topology=\"%s\"} %s %d",
+          metric, component, instance, topic, topology, value, NOW);
     }
   }
 
