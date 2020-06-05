ae043e13f53167acd68b3338e5eb302a9d66a605
Josh Fischer <josh@joshfischer.io>
2020-06-05 15:59:24 -0500

    fixing javadocs

--- website2/website/scripts/javadocs.sh
+++ website2/website/scripts/javadocs.sh
@@ -38,9 +38,12 @@ else
 fi
 
 (cd $HERON_ROOT_DIR && $BAZEL_CMD \
-  `bazel query 'kind("java_library", "heron/...")'`\
+  `bazel query 'kind("java_library", "heron/...")'` \
+  `bazel query 'kind("java_library", "contrib/...")'` \
   `bazel query 'kind("java_test", "heron/...")'` \
   `bazel query 'kind("java_library", "integration_test/...")'`)
+
+
 
 HERON_SRC_FILES=`find $HERON_ROOT_DIR -path "*/org/apache/*" -name "*.java" -not -path "$HERON_ROOT_DIR/tools/*"`
 BACKTYPE_SRC_FILES=`find $HERON_ROOT_DIR -path "*/backtype/storm/*" -name "*.java"`
@@ -50,15 +53,15 @@ GEN_FILES=`find $GEN_PROTO_DIR -name "*.java"`
 rm -rf $JAVADOC_OUTPUT_DIR
 mkdir -p $JAVADOC_OUTPUT_DIR
 
-BIN_JARS=`find $HERON_ROOT_DIR/bazel-incubator-heron/_bin/. -name "*\.jar" | tr '\n' ':'`
 GEN_JARS=`find $HERON_ROOT_DIR/bazel-bin/external/. -name "*\.jar" | tr '\n' ':'`
 SCRIBE_JARS=`find $HERON_ROOT_DIR/bazel-bin/. -name "libthrift_scribe_java.jar" | tr '\n' ':'`
 PROTO_JARS=`find $HERON_ROOT_DIR/bazel-bin/heron/proto/. -name "*\.jar" | tr '\n' ':'`
 CLOSURE_CLASSES="$HERON_ROOT_DIR/bazel-bin/storm-compatibility/src/java/_javac/storm-compatibility-java/libstorm-compatibility-java_classes/."
 CONTRIB_JARS=`find $HERON_ROOT_DIR/bazel-bin/contrib/. -name "*\.jar" | tr '\n' ':'`
 
-export CLASSPATH=$BIN_JARS:$GEN_JARS:$SCRIBE_JARS:$PROTO_JARS:$CLOSURE_CLASSES:$CONTRIB_JARS
+export CLASSPATH=$GEN_JARS:$SCRIBE_JARS:$PROTO_JARS:$CLOSURE_CLASSES:$CONTRIB_JARS
 
+echo "before java doc command"
 $JAVADOC $FLAGS \
   -encoding "UTF-8" \
   -windowtitle "Heron Java API" \
@@ -77,4 +80,5 @@ $JAVADOC $FLAGS \
 cp -r $JAVADOC_OUTPUT_DIR $JAVADOC_OUTPUT_LOCAL_DIR
 
 echo "Javdocs generated at $JAVADOC_OUTPUT_DIR"
+echo "Javaodcs copied to: $JAVADOC_OUTPUT_LOCAL_DIR"
 exit 0
