daab28d14d7be38d2c2ffd34226f733941109788
Oliver Bristow <evilumbrella+github@gmail.com>
2020-12-01 01:50:54 +0000

    WIP: unittest → pytest

--- heron/tools/tracker/tests/python/query_unittest.py
+++ heron/tools/tracker/tests/python/query_unittest.py
@@ -16,161 +16,160 @@
 # under the License.
 ''' query_unittest.py '''
 # pylint: disable=missing-docstring, undefined-variable
-import unittest
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 
 from heron.tools.tracker.src.python.query import *
+from heron.tools.tracker.src.python.tracker import Tracker
 
 import pytest
 
+@pytest.fixture
+def mock_query():
+  tracker = MagicMock(Tracker)
+  return Query(tracker)
 
-class QueryTest(unittest.TestCase):
-  def setUp(self):
-    self.tracker = Mock()
-    self.query = Query(self.tracker)
+def test_find_closing_braces(mock_query):
+  query = "(())"
+  assert 3 == mock_query.find_closing_braces(query)
 
-  def test_find_closing_braces(self):
-    query = "(())"
-    assert 3 == self.query.find_closing_braces(query)
+  query = "hello()"
+  with pytest.raises(Exception):
+    mock_query.find_closing_braces(query)
 
-    query = "hello()"
-    with pytest.raises(Exception):
-      self.query.find_closing_braces(query)
+  query = "(hello)"
+  assert 6 == mock_query.find_closing_braces(query)
 
-    query = "(hello)"
-    assert 6 == self.query.find_closing_braces(query)
+  query = "(no closing braces"
+  with pytest.raises(Exception):
+    mock_query.find_closing_braces(query)
 
-    query = "(no closing braces"
-    with pytest.raises(Exception):
-      self.query.find_closing_braces(query)
+  query = "()()"
+  assert 1 == mock_query.find_closing_braces(query)
 
-    query = "()()"
-    assert 1 == self.query.find_closing_braces(query)
+def test_get_sub_parts(mock_query):
+  query = "abc, def, xyz"
+  assert ["abc", "def", "xyz"] == mock_query.get_sub_parts(query)
 
-  def test_get_sub_parts(self):
-    query = "abc, def, xyz"
-    assert ["abc", "def", "xyz"] == self.query.get_sub_parts(query)
+  query = "(abc, xyz)"
+  assert ["(abc, xyz)"] == mock_query.get_sub_parts(query)
 
-    query = "(abc, xyz)"
-    assert ["(abc, xyz)"] == self.query.get_sub_parts(query)
+  query = "a(x, y), b(p, q)"
+  assert ["a(x, y)", "b(p, q)"] == mock_query.get_sub_parts(query)
 
-    query = "a(x, y), b(p, q)"
-    assert ["a(x, y)", "b(p, q)"] == self.query.get_sub_parts(query)
+  query = ",,"
+  assert ["", "", ""] == mock_query.get_sub_parts(query)
 
-    query = ",,"
-    assert ["", "", ""] == self.query.get_sub_parts(query)
-
-    query = "())"
-    with pytest.raises(Exception):
-      self.query.get_sub_parts(query)
+  query = "())"
+  with pytest.raises(Exception):
+    mock_query.get_sub_parts(query)
 
   # pylint: disable=too-many-statements
-  def test_parse_query_string(self):
-    query = "TS(a, b, c)"
-    root = self.query.parse_query_string(query)
-    assert "a" == root.component
-    assert ["b"] == root.instances
-    assert "c" == root.metric_name
+def test_parse_query_string(mock_query):
+  query = "TS(a, b, c)"
+  root = mock_query.parse_query_string(query)
+  assert "a" == root.component
+  assert ["b"] == root.instances
+  assert "c" == root.metric_name
 
-    query = "TS(a, *, m)"
-    root = self.query.parse_query_string(query)
-    assert "a" == root.component
-    assert [] == root.instances
-    assert "m" == root.metric_name
+  query = "TS(a, *, m)"
+  root = mock_query.parse_query_string(query)
+  assert "a" == root.component
+  assert [] == root.instances
+  assert "m" == root.metric_name
 
-    query = "DEFAULT(0, TS(a, b, c))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Default)
-    assert isinstance(root.constant, float)
-    assert root.constant == 0
-    assert isinstance(root.timeseries, TS)
+  query = "DEFAULT(0, TS(a, b, c))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Default)
+  assert isinstance(root.constant, float)
+  assert root.constant == 0
+  assert isinstance(root.timeseries, TS)
 
-    query = "DEFAULT(0, SUM(TS(a, a, a), TS(b, b, b)))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Default)
-    assert isinstance(root.constant, float)
-    assert isinstance(root.timeseries, Sum)
-    assert 2 == len(root.timeseries.timeSeriesList)
-    assert isinstance(root.timeseries.timeSeriesList[0], TS)
-    assert isinstance(root.timeseries.timeSeriesList[1], TS)
+  query = "DEFAULT(0, SUM(TS(a, a, a), TS(b, b, b)))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Default)
+  assert isinstance(root.constant, float)
+  assert isinstance(root.timeseries, Sum)
+  assert 2 == len(root.timeseries.timeSeriesList)
+  assert isinstance(root.timeseries.timeSeriesList[0], TS)
+  assert isinstance(root.timeseries.timeSeriesList[1], TS)
 
-    query = "MAX(1, TS(a, a, a))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Max)
-    assert isinstance(root.timeSeriesList[0], float)
-    assert isinstance(root.timeSeriesList[1], TS)
+  query = "MAX(1, TS(a, a, a))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Max)
+  assert isinstance(root.timeSeriesList[0], float)
+  assert isinstance(root.timeSeriesList[1], TS)
 
-    query = "PERCENTILE(90, TS(a, a, a))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Percentile)
-    assert isinstance(root.quantile, float)
-    assert isinstance(root.timeSeriesList[0], TS)
+  query = "PERCENTILE(90, TS(a, a, a))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Percentile)
+  assert isinstance(root.quantile, float)
+  assert isinstance(root.timeSeriesList[0], TS)
 
-    query = "PERCENTILE(TS(a, a, a), 90)"
-    with pytest.raises(Exception):
-      self.query.parse_query_string(query)
+  query = "PERCENTILE(TS(a, a, a), 90)"
+  with pytest.raises(Exception):
+    mock_query.parse_query_string(query)
 
-    query = "DIVIDE(TS(a, a, a), TS(b, b, b))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Divide)
-    assert isinstance(root.operand1, TS)
-    assert isinstance(root.operand2, TS)
+  query = "DIVIDE(TS(a, a, a), TS(b, b, b))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Divide)
+  assert isinstance(root.operand1, TS)
+  assert isinstance(root.operand2, TS)
 
     # Dividing by a constant is fine
-    query = "DIVIDE(TS(a, a, a), 90)"
-    self.query.parse_query_string(query)
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Divide)
-    assert isinstance(root.operand1, TS)
-    assert isinstance(root.operand2, float)
+  query = "DIVIDE(TS(a, a, a), 90)"
+  mock_query.parse_query_string(query)
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Divide)
+  assert isinstance(root.operand1, TS)
+  assert isinstance(root.operand2, float)
 
     # Must have two operands
-    query = "DIVIDE(TS(a, a, a))"
-    with pytest.raises(Exception):
-      self.query.parse_query_string(query)
+  query = "DIVIDE(TS(a, a, a))"
+  with pytest.raises(Exception):
+    mock_query.parse_query_string(query)
 
-    query = "MULTIPLY(TS(a, a, a), TS(b, b, b))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Multiply)
-    assert isinstance(root.operand1, TS)
-    assert isinstance(root.operand2, TS)
+  query = "MULTIPLY(TS(a, a, a), TS(b, b, b))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Multiply)
+  assert isinstance(root.operand1, TS)
+  assert isinstance(root.operand2, TS)
 
     # Multiplying with a constant is fine.
-    query = "MULTIPLY(TS(a, a, a), 10)"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Multiply)
-    assert isinstance(root.operand1, TS)
-    assert isinstance(root.operand2, float)
+  query = "MULTIPLY(TS(a, a, a), 10)"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Multiply)
+  assert isinstance(root.operand1, TS)
+  assert isinstance(root.operand2, float)
 
     # Must have two operands
-    query = "MULTIPLY(TS(a, a, a))"
-    with pytest.raises(Exception):
-      self.query.parse_query_string(query)
+  query = "MULTIPLY(TS(a, a, a))"
+  with pytest.raises(Exception):
+    mock_query.parse_query_string(query)
 
-    query = "SUBTRACT(TS(a, a, a), TS(b, b, b))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Subtract)
-    assert isinstance(root.operand1, TS)
-    assert isinstance(root.operand2, TS)
+  query = "SUBTRACT(TS(a, a, a), TS(b, b, b))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Subtract)
+  assert isinstance(root.operand1, TS)
+  assert isinstance(root.operand2, TS)
 
     # Multiplying with a constant is fine.
-    query = "SUBTRACT(TS(a, a, a), 10)"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Subtract)
-    assert isinstance(root.operand1, TS)
-    assert isinstance(root.operand2, float)
+  query = "SUBTRACT(TS(a, a, a), 10)"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Subtract)
+  assert isinstance(root.operand1, TS)
+  assert isinstance(root.operand2, float)
 
     # Must have two operands
-    query = "SUBTRACT(TS(a, a, a))"
-    with pytest.raises(Exception):
-      self.query.parse_query_string(query)
+  query = "SUBTRACT(TS(a, a, a))"
+  with pytest.raises(Exception):
+    mock_query.parse_query_string(query)
 
-    query = "RATE(TS(a, a, a))"
-    root = self.query.parse_query_string(query)
-    assert isinstance(root, Rate)
-    assert isinstance(root.timeSeries, TS)
+  query = "RATE(TS(a, a, a))"
+  root = mock_query.parse_query_string(query)
+  assert isinstance(root, Rate)
+  assert isinstance(root.timeSeries, TS)
 
     # Must have one operand only
-    query = "RATE(TS(a, a, a), TS(b, b, b))"
-    with pytest.raises(Exception):
-      self.query.parse_query_string(query)
+  query = "RATE(TS(a, a, a), TS(b, b, b))"
+  with pytest.raises(Exception):
+    mock_query.parse_query_string(query)
--- heron/tools/tracker/tests/python/tracker_unittest.py
+++ heron/tools/tracker/tests/python/tracker_unittest.py
@@ -17,215 +17,196 @@
 ''' tracker_unittest.py '''
 
 # pylint: disable=missing-docstring, attribute-defined-outside-init
-import unittest
-
 from functools import partial
 from unittest.mock import call, patch, Mock
 
 import heron.proto.execution_state_pb2 as protoEState
+
 from heron.statemgrs.src.python import statemanagerfactory
 from heron.tools.tracker.src.python.topology import Topology
 from heron.tools.tracker.src.python.tracker import Tracker
 
+import pytest
+
+# just for convenience in testing
 Topology = partial(Topology, tracker_config={})
 
-class TrackerTest(unittest.TestCase):
-  def setUp(self):
-    mock_config = Mock()
-    mock_config.validate.return_value = True
-    self.tracker = Tracker(mock_config)
+@pytest.fixture
+def tracker():
+  mock_config = Mock()
+  mock_config.validate.return_value = True
+  return Tracker(mock_config)
 
-  # pylint: disable=unused-argument
-  @patch.object(Tracker, 'get_stmgr_topologies')
-  @patch.object(Tracker, 'remove_topology')
-  @patch.object(Tracker, 'add_new_topology')
-  @patch.object(statemanagerfactory, 'get_all_state_managers')
-  def test_first_synch_topologies(
-      self, mock_get_all_state_managers,
-      mock_add_new_topology, mock_remove_topology,
-      mock_get_topologies_for_state_location):
-    mock_state_manager_1 = Mock()
-    mock_state_manager_1.name = 'mock_name1'
+@pytest.fixture
+def mock_tracker(tracker):
+  # this wouldn't be so ugly with Python3.9+
+  with patch.object(Tracker, 'get_stmgr_topologies'), patch.object(Tracker, 'remove_topology'), patch.object(Tracker, 'add_new_topology'), patch.object(statemanagerfactory, 'get_all_state_managers'):
+    yield tracker
 
-    mock_state_manager_2 = Mock()
-    mock_state_manager_2.name = 'mock_name2'
+# pylint: disable=unused-argument
+def test_first_synch_topologies(mock_tracker):
+  mock_state_manager_1 = Mock()
+  mock_state_manager_1.name = 'mock_name1'
 
-    watches = {}
-    mock_get_all_state_managers.return_value = [mock_state_manager_1, mock_state_manager_2]
+  mock_state_manager_2 = Mock()
+  mock_state_manager_2.name = 'mock_name2'
 
-    mock_get_topologies_for_state_location.return_value = []
-    def side_effect1(on_topologies_watch):
-      watches["1"] = on_topologies_watch
-      on_topologies_watch(['top_name1', 'top_name2'])
-    mock_state_manager_1.get_topologies = side_effect1
+  watches = {}
+  statemanagerfactory.get_all_state_managers.return_value = [mock_state_manager_1, mock_state_manager_2]
 
-    def side_effect2(on_topologies_watch):
-      watches["2"] = on_topologies_watch
-      on_topologies_watch(['top_name3', 'top_name4'])
-    mock_state_manager_2.get_topologies = side_effect2
+  mock_tracker.get_stmgr_topologies.return_value = []
+  def side_effect1(on_topologies_watch):
+    watches["1"] = on_topologies_watch
+    on_topologies_watch(['top_name1', 'top_name2'])
+  mock_state_manager_1.get_topologies = side_effect1
 
-    self.tracker.synch_topologies()
-    mock_get_topologies_for_state_location.assert_has_calls(
-        [call("mock_name2"),
-         call("mock_name1")],
-        any_order=True)
-    mock_add_new_topology.assert_has_calls([call(mock_state_manager_1, 'top_name1'),
-                                            call(mock_state_manager_1, 'top_name2'),
-                                            call(mock_state_manager_2, 'top_name3'),
-                                            call(mock_state_manager_2, 'top_name4')],
-                                           any_order=True)
+  def side_effect2(on_topologies_watch):
+    watches["2"] = on_topologies_watch
+    on_topologies_watch(['top_name3', 'top_name4'])
+  mock_state_manager_2.get_topologies = side_effect2
 
-  @patch.object(Tracker, 'get_stmgr_topologies')
-  @patch.object(Tracker, 'remove_topology')
-  @patch.object(Tracker, 'add_new_topology')
-  @patch.object(statemanagerfactory, 'get_all_state_managers')
-  def test_synch_topologies_leading_with_add_and_remove_topologies(
-      self, mock_get_all_state_managers,
-      mock_add_new_topology, mock_remove_topology,
-      mock_get_topologies_for_state_location):
-    mock_state_manager_1 = Mock()
-    mock_state_manager_1.name = 'mock_name1'
+  mock_tracker.synch_topologies()
+  mock_tracker.get_stmgr_topologies.assert_has_calls(
+      [call("mock_name2"),
+       call("mock_name1")],
+      any_order=True)
+  mock_tracker.add_new_topology.assert_has_calls([call(mock_state_manager_1, 'top_name1'),
+                                          call(mock_state_manager_1, 'top_name2'),
+                                          call(mock_state_manager_2, 'top_name3'),
+                                          call(mock_state_manager_2, 'top_name4')],
+                                         any_order=True)
 
-    mock_state_manager_2 = Mock()
-    mock_state_manager_2.name = 'mock_name2'
+def test_synch_topologies_leading_with_add_and_remove_topologies(mock_tracker):
+  mock_state_manager_1 = Mock()
+  mock_state_manager_1.name = 'mock_name1'
 
-    watches = {}
-    mock_get_all_state_managers.return_value = [mock_state_manager_1, mock_state_manager_2]
-    mock_get_topologies_for_state_location.return_value = []
+  mock_state_manager_2 = Mock()
+  mock_state_manager_2.name = 'mock_name2'
 
-    def side_effect1(on_topologies_watch):
-      watches["1"] = on_topologies_watch
-      on_topologies_watch(['top_name1', 'top_name2'])
-    mock_state_manager_1.get_topologies = side_effect1
+  watches = {}
+  statemanagerfactory.get_all_state_managers.return_value = [mock_state_manager_1, mock_state_manager_2]
+  mock_tracker.get_stmgr_topologies.return_value = []
 
-    def side_effect2(on_topologies_watch):
-      watches["2"] = on_topologies_watch
-      on_topologies_watch(['top_name3', 'top_name4'])
-    mock_state_manager_2.get_topologies = side_effect2
+  def side_effect1(on_topologies_watch):
+    watches["1"] = on_topologies_watch
+    on_topologies_watch(['top_name1', 'top_name2'])
+  mock_state_manager_1.get_topologies = side_effect1
 
-    self.tracker.synch_topologies()
-    mock_get_topologies_for_state_location.assert_has_calls(
-        [call("mock_name2"),
-         call("mock_name1")],
-        any_order=True)
-    mock_add_new_topology.assert_has_calls([call(mock_state_manager_1, 'top_name1'),
-                                            call(mock_state_manager_1, 'top_name2'),
-                                            call(mock_state_manager_2, 'top_name3'),
-                                            call(mock_state_manager_2, 'top_name4')],
-                                           any_order=True)
-    assert 4 == mock_add_new_topology.call_count
-    assert 0 == mock_remove_topology.call_count
-    mock_get_topologies_for_state_location.reset_mock()
-    mock_add_new_topology.reset_mock()
-    mock_remove_topology.reset_mock()
+  def side_effect2(on_topologies_watch):
+    watches["2"] = on_topologies_watch
+    on_topologies_watch(['top_name3', 'top_name4'])
+  mock_state_manager_2.get_topologies = side_effect2
 
-    def get_topologies_for_state_location_side_effect(name):
-      if name == 'mock_name1':
-        return [Topology('top_name1', 'mock_name1'),
-                Topology('top_name2', 'mock_name1')]
-      if name == 'mock_name2':
-        return [Topology('top_name3', 'mock_name2'),
-                Topology('top_name4', 'mock_name2')]
-      return []
+  mock_tracker.synch_topologies()
+  mock_tracker.get_stmgr_topologies.assert_has_calls(
+      [call("mock_name2"),
+       call("mock_name1")],
+      any_order=True)
+  mock_tracker.add_new_topology.assert_has_calls([call(mock_state_manager_1, 'top_name1'),
+                                          call(mock_state_manager_1, 'top_name2'),
+                                          call(mock_state_manager_2, 'top_name3'),
+                                          call(mock_state_manager_2, 'top_name4')],
+                                         any_order=True)
+  assert 4 == mock_tracker.add_new_topology.call_count
+  assert 0 == mock_tracker.remove_topology.call_count
+  mock_tracker.get_stmgr_topologies.reset_mock()
+  mock_tracker.add_new_topology.reset_mock()
+  mock_tracker.remove_topology.reset_mock()
 
-    # pylint: disable=line-too-long
-    mock_get_topologies_for_state_location.side_effect = get_topologies_for_state_location_side_effect
-
-    watches["1"](['top_name1', 'top_name3'])
-    watches["2"](['top_name5', 'top_name6'])
-    mock_add_new_topology.assert_has_calls([call(mock_state_manager_1, 'top_name3'),
-                                            call(mock_state_manager_2, 'top_name5'),
-                                            call(mock_state_manager_2, 'top_name6')],
-                                           any_order=True)
-    mock_remove_topology.assert_has_calls([call('top_name2', 'mock_name1'),
-                                           call('top_name3', 'mock_name2'),
-                                           call('top_name4', 'mock_name2')],
-                                          any_order=False)
-    assert 3 == mock_add_new_topology.call_count
-    assert 3 == mock_remove_topology.call_count
-
-  def fill_tracker_topologies(self):
-
-    def create_mock_execution_state(cluster, role, environ):
-      estate = protoEState.ExecutionState()
-      estate.cluster = cluster
-      estate.role = role
-      estate.environ = environ
-      return estate
-
-    self.topology1 = Topology('top_name1', 'mock_name1')
-    self.topology1.execution_state = create_mock_execution_state('cluster1', 'mark', 'env1')
-
-    self.topology2 = Topology('top_name2', 'mock_name1')
-    self.topology2.execution_state = create_mock_execution_state('cluster1', 'bob', 'env1')
-
-    self.topology3 = Topology('top_name3', 'mock_name1')
-    self.topology3.execution_state = create_mock_execution_state('cluster1', 'tom', 'env2')
-
-    self.topology4 = Topology('top_name4', 'mock_name2')
-    self.topology4.execution_state = create_mock_execution_state('cluster2', 'x', 'env1')
-
-    self.topology5 = Topology('top_name5', 'mock_name2')
-    self.topology5.execution_state = create_mock_execution_state('cluster2', 'x', 'env2')
-    self.tracker.topologies = [
-        self.topology1,
-        self.topology2,
-        self.topology3,
-        self.topology4,
-        self.topology5]
+  def get_topologies_for_state_location_side_effect(name):
+    if name == 'mock_name1':
+      return [Topology('top_name1', 'mock_name1'),
+              Topology('top_name2', 'mock_name1')]
+    if name == 'mock_name2':
+      return [Topology('top_name3', 'mock_name2'),
+              Topology('top_name4', 'mock_name2')]
+    return []
 
   # pylint: disable=line-too-long
-  def test_get_topology_by_cluster_environ_and_name(self):
-    self.fill_tracker_topologies()
-    assert self.topology1 == self.tracker.get_topology('cluster1', 'mark', 'env1', 'top_name1')
-    assert self.topology1 == self.tracker.get_topology('cluster1', None, 'env1', 'top_name1')
-    assert self.topology2 == self.tracker.get_topology('cluster1', 'bob', 'env1', 'top_name2')
-    assert self.topology2 == self.tracker.get_topology('cluster1', None, 'env1', 'top_name2')
-    assert self.topology3 == self.tracker.get_topology('cluster1', 'tom', 'env2', 'top_name3')
-    assert self.topology3 == self.tracker.get_topology('cluster1', None, 'env2', 'top_name3')
-    assert self.topology4 == self.tracker.get_topology('cluster2', None, 'env1', 'top_name4')
-    assert self.topology5 == self.tracker.get_topology('cluster2', None, 'env2', 'top_name5')
+  mock_tracker.get_stmgr_topologies.side_effect = get_topologies_for_state_location_side_effect
 
-  def test_get_topolies_for_state_location(self):
-    self.fill_tracker_topologies()
-    self.assertCountEqual(
-        [self.topology1, self.topology2, self.topology3],
-        self.tracker.get_stmgr_topologies('mock_name1'))
-    self.assertCountEqual(
-        [self.topology4, self.topology5],
-        self.tracker.get_stmgr_topologies('mock_name2'))
+  watches["1"](['top_name1', 'top_name3'])
+  watches["2"](['top_name5', 'top_name6'])
+  mock_tracker.add_new_topology.assert_has_calls([call(mock_state_manager_1, 'top_name3'),
+                                          call(mock_state_manager_2, 'top_name5'),
+                                          call(mock_state_manager_2, 'top_name6')],
+                                         any_order=True)
+  mock_tracker.remove_topology.assert_has_calls([call('top_name2', 'mock_name1'),
+                                         call('top_name3', 'mock_name2'),
+                                         call('top_name4', 'mock_name2')],
+                                        any_order=False)
+  assert 3 == mock_tracker.add_new_topology.call_count
+  assert 3 == mock_tracker.remove_topology.call_count
 
-  def test_add_new_topology(self):
-    self.assertCountEqual([], self.tracker.topologies)
-    mock_state_manager_1 = Mock()
-    mock_state_manager_1.name = 'mock_name1'
+@pytest.fixture
+def topologies(tracker):
 
-    self.tracker.add_new_topology(mock_state_manager_1, 'top_name1')
-    self.assertCountEqual(
-        ['top_name1'],
-        [t.name for t in self.tracker.topologies])
+  def create_mock_execution_state(cluster, role, environ):
+    estate = protoEState.ExecutionState()
+    estate.cluster = cluster
+    estate.role = role
+    estate.environ = environ
+    return estate
 
-    self.tracker.add_new_topology(mock_state_manager_1, 'top_name2')
-    self.assertCountEqual(
-        ['top_name1', 'top_name2'],
-        [t.name for t in self.tracker.topologies])
+  topology1 = Topology('top_name1', 'mock_name1')
+  topology1.execution_state = create_mock_execution_state('cluster1', 'mark', 'env1')
 
-    assert 2 == mock_state_manager_1.get_pplan.call_count
-    assert 2 == mock_state_manager_1.get_execution_state.call_count
-    assert 2 == mock_state_manager_1.get_tmanager.call_count
+  topology2 = Topology('top_name2', 'mock_name1')
+  topology2.execution_state = create_mock_execution_state('cluster1', 'bob', 'env1')
 
-  def test_remove_topology(self):
-    self.fill_tracker_topologies()
-    self.tracker.remove_topology('top_name1', 'mock_name1')
-    self.assertCountEqual([self.topology2, self.topology3, self.topology4, self.topology5],
-                          self.tracker.topologies)
-    self.tracker.remove_topology('top_name2', 'mock_name1')
-    self.assertCountEqual([self.topology3, self.topology4, self.topology5],
-                          self.tracker.topologies)
-    # Removing one that is not there should not have any affect
-    self.tracker.remove_topology('top_name8', 'mock_name1')
-    self.assertCountEqual([self.topology3, self.topology4, self.topology5],
-                          self.tracker.topologies)
-    self.tracker.remove_topology('top_name4', 'mock_name2')
-    self.assertCountEqual([self.topology3, self.topology5],
-                          self.tracker.topologies)
+  topology3 = Topology('top_name3', 'mock_name1')
+  topology3.execution_state = create_mock_execution_state('cluster1', 'tom', 'env2')
+
+  topology4 = Topology('top_name4', 'mock_name2')
+  topology4.execution_state = create_mock_execution_state('cluster2', 'x', 'env1')
+
+  topology5 = Topology('top_name5', 'mock_name2')
+  topology5.execution_state = create_mock_execution_state('cluster2', 'x', 'env2')
+  tracker.topologies = [
+      topology1,
+      topology2,
+      topology3,
+      topology4,
+      topology5]
+  return tracker.topologies[:]
+
+# pylint: disable=line-too-long
+def test_get_topology_by_cluster_environ_and_name(tracker, topologies):
+  assert topologies[0] == tracker.get_topology('cluster1', 'mark', 'env1', 'top_name1')
+  assert topologies[0] == tracker.get_topology('cluster1', None, 'env1', 'top_name1')
+  assert topologies[1] == tracker.get_topology('cluster1', 'bob', 'env1', 'top_name2')
+  assert topologies[1] == tracker.get_topology('cluster1', None, 'env1', 'top_name2')
+  assert topologies[2] == tracker.get_topology('cluster1', 'tom', 'env2', 'top_name3')
+  assert topologies[2] == tracker.get_topology('cluster1', None, 'env2', 'top_name3')
+  assert topologies[3] == tracker.get_topology('cluster2', None, 'env1', 'top_name4')
+  assert topologies[4] == tracker.get_topology('cluster2', None, 'env2', 'top_name5')
+
+def test_get_topolies_for_state_location(tracker, topologies):
+  assert [topologies[0], topologies[1], topologies[2]] == tracker.get_stmgr_topologies('mock_name1')
+  assert [topologies[3], topologies[4]] == tracker.get_stmgr_topologies('mock_name2')
+
+def test_add_new_topology(tracker):
+  assert [] == tracker.topologies
+  mock_state_manager_1 = Mock()
+  mock_state_manager_1.name = 'mock_name1'
+
+  tracker.add_new_topology(mock_state_manager_1, 'top_name1')
+  assert ['top_name1'] == [t.name for t in tracker.topologies]
+
+  tracker.add_new_topology(mock_state_manager_1, 'top_name2')
+  assert ['top_name1', 'top_name2'] == [t.name for t in tracker.topologies]
+
+  assert 2 == mock_state_manager_1.get_pplan.call_count
+  assert 2 == mock_state_manager_1.get_execution_state.call_count
+  assert 2 == mock_state_manager_1.get_tmanager.call_count
+
+def test_remove_topology(tracker, topologies):
+  tracker.remove_topology('top_name1', 'mock_name1')
+  assert [topologies[1], topologies[2], topologies[3], topologies[4]] == tracker.topologies
+  tracker.remove_topology('top_name2', 'mock_name1')
+  assert [topologies[2], topologies[3], topologies[4]] == tracker.topologies
+  # Removing one that is not there should not have any affect
+  tracker.remove_topology('top_name8', 'mock_name1')
+  assert [topologies[2], topologies[3], topologies[4]] == tracker.topologies
+  tracker.remove_topology('top_name4', 'mock_name2')
+  assert [topologies[2], topologies[4]] == tracker.topologies
