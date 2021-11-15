78264446e25fc474e96962f20d9494fbda5c5683
Saad Ur Rahman <saad.ur.rahman@gmail.com>
2021-11-15 13:43:04 -0500

    [Tests] <KubernetesContext>
    Added checks for `claimName`.

--- heron/schedulers/tests/java/org/apache/heron/scheduler/kubernetes/KubernetesContextTest.java
+++ heron/schedulers/tests/java/org/apache/heron/scheduler/kubernetes/KubernetesContextTest.java
@@ -89,20 +89,26 @@ public class KubernetesContextTest {
   public void testGetVolumeClaimTemplates() {
     final String volumeNameOne = "volume-name-one";
     final String volumeNameTwo = "volume-name-two";
+    final String claimName = "OnDeMaNd";
     final String keyPattern = KubernetesContext.KUBERNETES_VOLUME_CLAIM_PREFIX + "%s.%s";
 
-    final String storageClassField = VolumeClaimTemplateConfigKeys.storageClassName.toString();
-    final String pathField = VolumeClaimTemplateConfigKeys.path.toString();
+    final String storageClassField = VolumeClaimTemplateConfigKeys.storageClassName.name();
+    final String pathField = VolumeClaimTemplateConfigKeys.path.name();
+    final String claimNameField = VolumeClaimTemplateConfigKeys.claimName.name();
     final String expectedStorageClass = "expected-storage-class";
     final String storageClassKeyOne = String.format(keyPattern, volumeNameOne, storageClassField);
     final String storageClassKeyTwo = String.format(keyPattern, volumeNameTwo, storageClassField);
     final String expectedPath = "/path/for/volume/expected";
     final String pathKeyOne = String.format(keyPattern, volumeNameOne, pathField);
     final String pathKeyTwo = String.format(keyPattern, volumeNameTwo, pathField);
+    final String claimNameKeyOne = String.format(keyPattern, volumeNameOne, claimNameField);
+    final String claimNameKeyTwo = String.format(keyPattern, volumeNameTwo, claimNameField);
 
     final Config configPVC = Config.newBuilder()
         .put(pathKeyOne, expectedPath)
         .put(pathKeyTwo, expectedPath)
+        .put(claimNameKeyOne, claimName)
+        .put(claimNameKeyTwo, claimName)
         .put(storageClassKeyOne, expectedStorageClass)
         .put(storageClassKeyTwo, expectedStorageClass)
         .build();
@@ -110,9 +116,10 @@ public class KubernetesContextTest {
     final List<String> expectedKeys = Arrays.asList(volumeNameOne, volumeNameTwo);
     final List<VolumeClaimTemplateConfigKeys> expectedOptionsKeys =
         Arrays.asList(VolumeClaimTemplateConfigKeys.path,
-            VolumeClaimTemplateConfigKeys.storageClassName);
+            VolumeClaimTemplateConfigKeys.storageClassName,
+            VolumeClaimTemplateConfigKeys.claimName);
     final List<String> expectedOptionsValues =
-        Arrays.asList(expectedPath, expectedStorageClass);
+        Arrays.asList(expectedPath, expectedStorageClass, claimName);
 
     // List of provided PVC options.
     final Map<String, Map<VolumeClaimTemplateConfigKeys, String>> mapOfPVC =
@@ -164,12 +171,19 @@ public class KubernetesContextTest {
     testCases.add(new TestTuple<>("Invalid Volume Name should trigger exception",
         configInvalidVolumeName, "lowercase RFC-1123"));
 
+    // Invalid Claim Name.
+    final Config configInvalidClaimName = Config.newBuilder()
+        .put(String.format(keyPattern, volumeNameValid, "claimName"), failureValue)
+        .build();
+    testCases.add(new TestTuple<>("Invalid Claim Name should trigger exception",
+        configInvalidClaimName, "Option `claimName`"));
+
     // Invalid Storage Class Name.
     final Config configInvalidStorageClassName = Config.newBuilder()
         .put(String.format(keyPattern, volumeNameValid, "storageClassName"), failureValue)
         .build();
     testCases.add(new TestTuple<>("Invalid Storage Class Name should trigger exception",
-        configInvalidStorageClassName, "Storage Class name"));
+        configInvalidStorageClassName, "Option `storageClassName`"));
 
     // Testing loop.
     for (TestTuple<Config, String> testCase : testCases) {
