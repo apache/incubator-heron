5499ad4a87d694b7506267af180469eff3e4ca54
Saad Ur Rahman <saad.ur.rahman@gmail.com>
2021-12-06 21:16:57 -0500

    [Kubernetes] <Context>
    <getVolumeHostPath> collects and validates Host Path CLI options.

--- heron/schedulers/src/java/org/apache/heron/scheduler/kubernetes/KubernetesContext.java
+++ heron/schedulers/src/java/org/apache/heron/scheduler/kubernetes/KubernetesContext.java
@@ -415,6 +415,43 @@ public final class KubernetesContext extends Context {
     return volumes;
   }
 
+  /**
+   * Collects parameters form the <code>CLI</code> and validates options for <code>Host Path</code>s.
+   * @param config Contains the configuration options collected from the <code>CLI</code>.
+   * @param isExecutor Flag used to collect CLI commands for the <code>Executor</code> and <code>Manager</code>.
+   * @return A mapping between <code>Volumes</code> and their configuration <code>key-value</code> pairs.
+   * Will return an empty list if there are no Volume Claim Templates to be generated.
+   */
+  public static Map<String, Map<KubernetesConstants.VolumeConfigKeys, String>>
+      getVolumeHostPath(Config config, boolean isExecutor) {
+    final Map<String, Map<KubernetesConstants.VolumeConfigKeys, String>> volumes =
+        getVolumeConfigs(config, KubernetesContext.KUBERNETES_VOLUME_HOSTPATH_PREFIX, isExecutor);
+
+    for (Map.Entry<String, Map<KubernetesConstants.VolumeConfigKeys, String>> volume
+        : volumes.entrySet()) {
+      final String type = volume.getValue().get(KubernetesConstants.VolumeConfigKeys.type);
+      if (type != null && !KubernetesConstants.VALID_VOLUME_HOSTPATH_TYPES.contains(type)) {
+        throw new TopologySubmissionException(String.format("Volume `%s`: Host Path"
+            + " `type` of '%s' is invalid.", volume.getKey(), type));
+      }
+
+      for (Map.Entry<KubernetesConstants.VolumeConfigKeys, String> volumeConfig
+          : volume.getValue().entrySet()) {
+        final KubernetesConstants.VolumeConfigKeys key = volumeConfig.getKey();
+
+        switch (key) {
+          case type: case path: case subPath:
+            break;
+          default:
+            throw new TopologySubmissionException(String.format("Volume `%s`: Invalid Host Path"
+                + " type option for '%s'", volume.getKey(), key));
+        }
+      }
+    }
+
+    return volumes;
+  }
+
   static Set<String> getConfigKeys(Config config, String keyPrefix) {
     Set<String> annotations = new HashSet<>();
     for (String s : config.getKeySet()) {
