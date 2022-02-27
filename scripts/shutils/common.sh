6fd5e2f75945b37c2d054c376def1bb330cce242
Nicholas Nezis <nicholas.nezis@gmail.com>
2022-02-27 15:32:25 -0500

    Remove function not used anywhere

--- scripts/shutils/common.sh
+++ scripts/shutils/common.sh
@@ -90,19 +90,6 @@ function print_timer_summary {
   done
 }
 
-# Check the ci environment is valid
-function ci_environ {
-  environ=$1
-  if [[ $environ =~ travis ]]; then
-    echo "travis"
-  elif [[ $environ =~ applatix ]]; then
-    echo "applatix"
-  else
-    echo "$environ ci not supported"
-    exit 1
-  fi
-}
-
 function pathadd {
   if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
     PATH="${PATH:+"$PATH:"}$1"
