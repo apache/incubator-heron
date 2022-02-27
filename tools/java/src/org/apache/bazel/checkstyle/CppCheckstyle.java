0c8aea233e1377ddcd058b14f88d94fb761bb475
Nicholas Nezis <nicholas.nezis@gmail.com>
2022-02-27 16:22:27 -0500

    More cleanup and documentation update

--- tools/java/src/org/apache/bazel/checkstyle/CppCheckstyle.java
+++ tools/java/src/org/apache/bazel/checkstyle/CppCheckstyle.java
@@ -43,7 +43,7 @@ import org.apache.commons.cli.ParseException;
  * Usage: java org.apache.bazel.checkstyle.CppCheckstyle -f &lt;extra_action_file&gt; -c &lt;cpplint_file&gt;
  * <p>
  * To test:
- * $ bazel build --config=darwin --experimental_action_listener=tools/cpp:compile_cpp heron/stmgr/src/cpp:grouping-cxx
+ * $ bazel build --experimental_action_listener=tools/cpp:compile_cpp heron/stmgr/src/cpp:grouping-cxx
  */
 public final class CppCheckstyle {
   public static final Logger LOG = Logger.getLogger(CppCheckstyle.class.getName());
